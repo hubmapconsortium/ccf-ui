69af906fc4cff0112934d92ebbf6621323fb8d52
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-03-28 14:37:35 -0400

    Change the routes

--- src/app/views/main/main.module.ts
+++ src/app/views/main/main.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { MainComponent } from './main.component';
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule
+  ],
+  exports: [MainComponent]
 })
 export class MainModule { }
