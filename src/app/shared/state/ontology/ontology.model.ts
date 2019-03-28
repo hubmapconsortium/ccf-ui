56adc82f5b8bc268c293ae185aff16cd7f3310f3
axdanbol <daniel_bolin@yahoo.com>
2019-03-28 11:03:45 -0400

    Added documentation and tests to ontology state

--- src/app/shared/state/ontology/ontology.model.ts
+++ src/app/shared/state/ontology/ontology.model.ts
@@ -1,11 +1,39 @@
+/**
+ * A node in the ontology tree.
+ */
 export interface OntologyNode {
+  /**
+   * The unique identifier of this node.
+   */
   id: string;
-  parent: string;
+
+  /**
+   * The identifier of the parent node. `undefined` if this is the root node.
+   */
+  parent: string | undefined;
+
+  /**
+   * An array of child identifiers.
+   */
   children: string[];
 }
 
+/**
+ * A flat representation of an ontology tree.
+ */
 export interface OntologyStateModel {
+  /**
+   * Identifier of the root node.
+   */
   root: string;
+
+  /**
+   * Identifiers of all nodes in the hash table.
+   */
   ids: string[];
+
+  /**
+   * Hash table of nodes.
+   */
   nodes: { [id: string]: OntologyNode };
 }
--- src/app/shared/state/ontology/ontology.state.ts
+++ src/app/shared/state/ontology/ontology.state.ts
@@ -1,7 +1,11 @@
-import { NgxsOnInit, State, StateContext } from '@ngxs/store';
+import { NgxsOnInit, Selector, State, StateContext } from '@ngxs/store';
+import { values as loValues } from 'lodash';
 
-import { OntologyStateModel } from './ontology.model';
+import { OntologyNode, OntologyStateModel } from './ontology.model';
 
+/**
+ * Ontology tree state.
+ */
 @State<OntologyStateModel>({
   name: 'ontology',
   defaults: {
@@ -11,6 +15,17 @@ import { OntologyStateModel } from './ontology.model';
   }
 })
 export class OntologyState implements NgxsOnInit {
+  /**
+   * Selects all nodes in the ontology tree.
+   *
+   * @param state The ontology tree state.
+   * @returns All ontology nodes.
+   */
+  @Selector()
+  static nodes(state: OntologyStateModel): OntologyNode[] {
+    return loValues(state.nodes);
+  }
+
   ngxsOnInit(ctx: StateContext<OntologyStateModel>) {
     // TODO load ontology
   }
