4ae24216cff10b92b752c8d16993e7cfedab23c2
axdanbol <daniel_bolin@yahoo.com>
2019-04-01 13:42:33 -0400

    Changed a grid getter to a property that is recalculated on changes to reduce number of  times it is called

--- src/app/components/tissues-browser-grid/tissues-browser-grid.component.ts
+++ src/app/components/tissues-browser-grid/tissues-browser-grid.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'ccf-tissues-browser-grid',
@@ -6,15 +6,25 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./tissues-browser-grid.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TissuesBrowserGridComponent {
+export class TissuesBrowserGridComponent implements OnInit, OnChanges {
   static readonly defaultNumColumns = 6;
 
   @Input() items: any[]; // FIXME: Correct typings
   @Input() numColumns: number;
 
-  get itemWidthPercentage(): string {
+  itemWidthPercentage: string;
+
+  ngOnInit() {
+    this.updateItemWidth();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ('numColumns' in changes) { this.updateItemWidth(); }
+  }
+
+  private updateItemWidth(): void {
     const percentage = 100 / (this.numColumns || TissuesBrowserGridComponent.defaultNumColumns);
     const truncated = percentage.toFixed(3).slice(0, -1); // Truncate number to 2 decimal places without rounding.
-    return truncated + '%';
+    this.itemWidthPercentage = truncated + '%';
   }
 }
