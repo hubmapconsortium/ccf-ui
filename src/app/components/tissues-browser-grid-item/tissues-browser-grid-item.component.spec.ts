a44df3a6bc078a0adfdc99072d0f25012ceb7721
axdanbol <daniel_bolin@yahoo.com>
2019-04-03 14:14:29 -0400

    Fully implemented grid item with docs + tests

--- src/app/components/tissues-browser-grid-item/tissues-browser-grid-item.component.spec.ts
+++ src/app/components/tissues-browser-grid-item/tissues-browser-grid-item.component.spec.ts
@@ -1,25 +1,105 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MockRender } from 'ng-mocks';
 
+import { NavigationService } from '../../shared/services/navigation/navigation.service';
+import { OntologyNode } from '../../shared/state/ontology/ontology.model';
 import { TissuesBrowserGridItemComponent } from './tissues-browser-grid-item.component';
 
 describe('TissuesBrowserGridItemComponent', () => {
   let component: TissuesBrowserGridItemComponent;
-  let fixture: ComponentFixture<TissuesBrowserGridItemComponent>;
+  let element: DebugElement;
+  let fixture: ComponentFixture<{ item: OntologyNode }>;
+  let item: OntologyNode;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [ TissuesBrowserGridItemComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [TissuesBrowserGridItemComponent],
+      providers: [
+        { provide: NavigationService, useValue: { navigateToTissue: () => undefined } }
+      ]
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TissuesBrowserGridItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await TestBed.compileComponents();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    fixture = MockRender(`
+      <ccf-tissues-browser-grid-item [item]="item">
+      </ccf-tissues-browser-grid-item>
+    `, {
+      item: {
+        id: 'test',
+        tileUrl: 'anUrlButNotReally',
+        description: 'abcdef'
+      } as OntologyNode
+    });
+
+    element = fixture.debugElement.query(By.directive(TissuesBrowserGridItemComponent));
+    component = element.componentInstance;
+    item = fixture.componentInstance.item;
+  });
+
+  describe('component', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    describe('tileUrl', () => {
+      it('is the same as the item\'s tileUrl', () => {
+        expect(component.tileUrl).toEqual(item.tileUrl);
+      });
+    });
+
+    describe('description', () => {
+      it('is the same as the item\'s description', () => {
+        expect(component.description).toEqual(item.description);
+      });
+
+      it('has a default', () => {
+        fixture.componentInstance.item = { id: 'foo' } as any;
+        expect(component.description).toBeTruthy();
+      });
+    });
+
+    describe('navigateToTissue()', () => {
+      let spy: jasmine.Spy;
+
+      beforeEach(() => {
+        const service = TestBed.get(NavigationService);
+        spy = spyOn(service, 'navigateToTissue');
+      });
+
+      beforeEach(() => {
+        component.navigateToTissue();
+      });
+
+      it('calls the service to navigate', () => {
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('dom', () => {
+    describe('tile', () => {
+      let tile: DebugElement;
+
+      beforeEach(() => {
+        tile = element.query(By.css('.tile'));
+      });
+
+      it('exists', () => {
+        expect(tile).toBeTruthy();
+      });
+
+      it('has the src attribute set', () => {
+        expect(tile.attributes['src']).toBeTruthy();
+      });
+
+      it('has an alt attribute set', () => {
+        expect(tile.attributes['alt']).toBeTruthy();
+      });
+    });
   });
 });
--- src/app/components/tissues-browser-grid-item/tissues-browser-grid-item.component.ts
+++ src/app/components/tissues-browser-grid-item/tissues-browser-grid-item.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
+import { NavigationService } from '../../shared/services/navigation/navigation.service';
+import { OntologyNode } from '../../shared/state/ontology/ontology.model';
+
+/**
+ * Encapsulates the content of a single tissues browser grid item.
+ */
 @Component({
   selector: 'ccf-tissues-browser-grid-item',
   templateUrl: './tissues-browser-grid-item.component.html',
@@ -7,5 +13,36 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TissuesBrowserGridItemComponent {
-  @Input() item: any; // FIXME: Correct typings
+  /**
+   * The item for which to display a tile.
+   */
+  @Input() item: OntologyNode;
+
+  /**
+   * Gets the tile's url.
+   */
+  get tileUrl(): string {
+    return this.item.tileUrl || '../../../assets/grid-tissue.png'; // FIXME: Second part is temporary (used for testing)
+  }
+
+  /**
+   * Alternate textual description of the tile.
+   */
+  get description(): string {
+    return this.item.description || `Tissue sample id: ${this.item.id}`;
+  }
+
+  /**
+   * Creates an instance of tissues browser grid item component.
+   *
+   * @param navigator The navigation service.
+   */
+  constructor(private navigator: NavigationService) { }
+
+  /**
+   * Navigates to the tissue associated with this item.
+   */
+  navigateToTissue(): void {
+    this.navigator.navigateToTissue(this.item);
+  }
 }
