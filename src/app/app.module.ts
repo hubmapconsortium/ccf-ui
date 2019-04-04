f2573c6aa9fb795fa2adfed009c94068c506339b
kalaraja <shekhar957@gmail.com>
2019-04-04 16:38:38 -0400

    use feedback icon for tissue - temporary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
 import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 import { NgxsModule } from '@ngxs/store';
 
@@ -20,7 +21,8 @@ const rootStates = [
     AppRoutingModule,
     MainModule,
     NgxsModule.forRoot(rootStates, { developmentMode: !environment.production }),
-    NgxsRouterPluginModule.forRoot()
+    NgxsRouterPluginModule.forRoot(),
+    NgxsDispatchPluginModule.forRoot()
   ],
   declarations: [
     AppComponent
--- src/app/views/home/home-routing.module.ts
+++ src/app/views/home/home-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 
-
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     component: HomeComponent
   }
 ];
