e714bc755d5ebbf1618749c24bd84efa7d325e89
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-04-04 14:19:33 -0400

    Unit tests and code comments.

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -1,25 +1,44 @@
+import { Provider } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatIconModule } from '@angular/material';
+import { AboutModalService } from 'src/app/shared/services/about-modal.service';
 
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
+  let aboutModalService: AboutModalService;
+  const mockedAboutModalService = {
+    closeAbout: (): void => undefined
+  };
 
   beforeEach(async(() => {
+    const mockProviders: Provider[] = [
+      { provide: AboutModalService, useValue: mockedAboutModalService }
+    ];
     TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [ AboutComponent ],
+      providers: mockProviders,
+      imports: [ MatIconModule, MatDialogModule ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AboutComponent);
+    aboutModalService  = TestBed.get(AboutModalService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the modal', () => {
+    spyOn(aboutModalService, 'closeAbout');
+    component.close();
+    expect(aboutModalService.closeAbout).toHaveBeenCalled();
   });
 });
--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AboutModalService } from 'src/app/shared/services/about-modal.service';
 
+/**
+ * Component for about modal, that gives brief description of the application.
+ */
 @Component({
   selector: 'ccf-about',
   templateUrl: './about.component.html',
@@ -8,8 +11,15 @@ import { AboutModalService } from 'src/app/shared/services/about-modal.service';
 })
 export class AboutComponent {
 
+  /**
+   * Creates an instance of about component.
+   * @param aboutModalService Modal service responsible for opening and closing of the modal.
+   */
   constructor(private readonly aboutModalService: AboutModalService) { }
 
+  /**
+   * Closes about modal
+   */
   close() {
     this.aboutModalService.closeAbout();
   }
