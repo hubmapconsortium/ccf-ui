b08c8828dc38d419e9eacd6ca2a5779d488b7eea
kalaraja <shekhar957@gmail.com>
2019-04-04 14:35:27 -0400

    improved basic styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,17 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'ccf-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ccf-ui');
-  });
-
-  xit('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ccf-ui!');
-  });
 });
