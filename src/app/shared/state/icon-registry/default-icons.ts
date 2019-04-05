21163acd80e8d0c4e6ea2de1f4799a777ff4394d
axdanbol <daniel_bolin@yahoo.com>
2019-04-05 13:05:50 -0400

    Added state for registering icons

---
+++ src/app/shared/state/icon-registry/default-icons.ts
@@ -0,0 +1,21 @@
+import { IconDefinition } from './icon-registry.model';
+
+/**
+ * Icon definition where url and html are strings instead of SafeResourceUrl or SafeHtml.
+ */
+export type DefaultIconDefinition = Exclude<IconDefinition, 'url' | 'html'> & {
+  url?: string,
+  html?: string
+};
+
+/**
+ * Icons that should be registered at startup.
+ */
+export const defaultIcons: DefaultIconDefinition[] = [
+  { name: 'male', namespace: 'gender', url: '../../assets/icons/gender-male.svg' },
+  { name: 'female', namespace: 'gender', url: '../../assets/icons/gender-female.svg' },
+  { name: 'male-female', namespace: 'gender', url: '../../assets/icons/gender-male-female.svg' },
+  { name: 'unknown', namespace: 'gender', url: '../../assets/icons/gender-male-female.svg' }
+
+  // Add additional icons that should be loaded on startup here!
+];
