9af5ce43958f310979c09cf463e1e8654e69bbfe
kalaraja <shekhar957@gmail.com>
2019-04-05 11:57:45 -0400

    added jsdoc comments

--- src/app/components/leftbar/leftbar.component.ts
+++ src/app/components/leftbar/leftbar.component.ts
@@ -8,7 +8,11 @@ import { Observable } from 'rxjs';
 })
 export class LeftbarComponent {
 
+  /**
+   * Output sends an drawer toggle event as an observable to main.component.ts
+   */
   @Output() drawerToggle: Observable<boolean>;
+
   private readonly _drawerToggleEventEmitter = new EventEmitter<boolean>();
   sidenavExpanded = true;
 
@@ -16,6 +20,11 @@ export class LeftbarComponent {
     this.drawerToggle = this._drawerToggleEventEmitter.asObservable();
   }
 
+
+  /**
+   * Emits toggle event
+   * clicking on this will change the event emitter of the observable and also sidenavExpanded state
+   */
   emitToggleEvent() {
     this.sidenavExpanded = !this.sidenavExpanded;
     this._drawerToggleEventEmitter.next(this.sidenavExpanded);
