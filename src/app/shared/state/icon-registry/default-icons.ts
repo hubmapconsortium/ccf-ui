8ae69719cbe736fa3675c083c937af3d032ee6b0
axdanbol <daniel_bolin@yahoo.com>
2019-04-08 11:20:12 -0400

    Moved icons and fixed paths

--- src/app/shared/state/icon-registry/default-icons.ts
+++ src/app/shared/state/icon-registry/default-icons.ts
@@ -12,10 +12,12 @@ export type DefaultIconDefinition = Exclude<IconDefinition, 'url' | 'html'> & {
  * Icons that should be registered at startup.
  */
 export const defaultIcons: DefaultIconDefinition[] = [
-  { name: 'male', namespace: 'gender', url: '../../assets/icons/gender-male.svg' },
-  { name: 'female', namespace: 'gender', url: '../../assets/icons/gender-female.svg' },
-  { name: 'male-female', namespace: 'gender', url: '../../assets/icons/gender-male-female.svg' },
-  { name: 'unknown', namespace: 'gender', url: '../../assets/icons/gender-male-female.svg' }
+  { name: 'male', namespace: 'gender', url: '/assets/icons/gender/male.svg' },
+  { name: 'female', namespace: 'gender', url: '/assets/icons/gender/female.svg' },
+  { name: 'male-female', namespace: 'gender', url: '/assets/icons/gender/male-female.svg' },
+  { name: 'unknown', namespace: 'gender', url: '/assets/icons/gender/male-female.svg' },
+
+  { name: 'search', namespace: 'toolbar', url: '/assets/icons/toolbar/search.svg' }
 
   // Add additional icons that should be loaded on startup here!
 ];
