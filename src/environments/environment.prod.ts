7d648d92135c81093861bbf7a1a6215bdd135213
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-04-18 15:41:17 -0400

    fixed the build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  localDatabaseUrl: 'https://hubmapconsortium.github.io/ccf-ui-sampledata/v1/data.yml'
+  localDatabaseUrl: 'https://hubmapconsortium.github.io/ccf-ui-sampledata/v1/data.yml',
+  ccfAssetUrl: 'https://hubmapconsortium.github.io/ccf-ui-sampledata/ccf'
 };
