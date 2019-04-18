8f8cb15775f92d5a82ba56683ffe39209c0f5484
Gurjaspal <gurjaspal.bedi@gmail.com>
2019-04-18 15:44:10 -0400

    Fixed the URL so that it also works on Netlify.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   localDatabaseUrl: 'https://hubmapconsortium.github.io/ccf-ui-sampledata/v1/data.yml',
-  ccfAssetUrl: 'https://hubmapconsortium.github.io/ccf-ui-sampledata/ccf'
+  ccfAssetUrl: '/ccf-ui-sampledata/ccf'
 };
