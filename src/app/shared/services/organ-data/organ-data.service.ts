e15840552f91c7d8594a3844b5fd7dae4996fe97
axdanbol <daniel_bolin@yahoo.com>
2019-05-02 14:25:55 -0400

    Removed memory leaks and incorrect code

--- src/app/shared/services/organ-data/organ-data.service.ts
+++ src/app/shared/services/organ-data/organ-data.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { map, pluck, switchMap } from 'rxjs/operators';
 
 import { environment } from '../../../../environments/environment';
-import { TissueSample, Patient, TissueSlice, TissueImage } from '../../state/database/database.models';
 import { NavigationState } from '../../state/navigation/navigation.state';
 import { LocalDatabaseService } from '../database/local/local-database.service';
 
+export interface CountMetadata {
+  patients: number;
+  tissueSamples: number;
+  tissueSlices: number;
+  tissueImages: number;
+  cells: number;
+}
+
 /**
- * Injectable organ data service talks to localdatabase.
+ * Injectable organ data service talks to local database.
  */
 @Injectable()
 export class OrganDataService {
-
   /**
-   * Creates an instance of organ data service.
-   * @param localDatabase to get irgan data.
+   * Base path to images of tissues - TODO - this will come from a json file eventually
    */
-  constructor(
-    private readonly localDatabase: LocalDatabaseService
-  ) { }
-
-  /**
-   * Path to images of tissues - TODO - this will come from a json file eventually
-   */
-  private readonly pathToImages = environment.ccfAssetUrl + '/organ';
+  private readonly basePath = environment.ccfAssetUrl + '/organ';
 
   /**
    * Emits the currently active organ.
@@ -34,60 +32,25 @@ export class OrganDataService {
   private activeOrgan: Observable<{ id: string }>; // FIXME: Update with proper organ object
 
   /**
-   * Gets active organ.
-   * @returns active organ Observable.
+   * Emits the path for currently active organ's image.
    */
-  getActiveOrgan(): Observable<{ id: string }> {
-    return this.activeOrgan;
-  }
+  readonly organImagePath = this.activeOrgan.pipe(
+    pluck('id'),
+    map(id => id && `${this.basePath}/${id}/organ.svg#organ`)
+  );
 
   /**
-   * Gets organ source path
-   * @returns Observable of organ source path
+   * Emits the overlay objects for currently active organ.
    */
-  getOrganSourcePath(): Observable<string> {
-    return this.activeOrgan.pipe(
-      pluck('id'),
-      map(id => id && `${this.pathToImages}/${id}/`)
-    );
-  }
+  readonly organOverlays = this.activeOrgan.pipe(
+    pluck('id'),
+    switchMap(id => this.localDatabase.getTissueSamples(sample => sample.patient.anatomicalLocations[0] === id))
+  );
 
   /**
-   * Gets all tissue samples
-   * @returns all tissue samples' observable.
+   * Creates an instance of organ data service.
+   *
+   * @param localDatabase Database from which organ and related data is fetched.
    */
-  getAllTissueSamples(organId: string): Observable<TissueSample[]> {
-    return this.localDatabase.getTissueSamples((item: TissueSample) =>  item.patient.anatomicalLocations[0] === organId);
-  }
-
-  getCounts(organId: string): CountMetadata {
-    const countMetadata: CountMetadata = {
-      patients: 0,
-      tissueSamples: 0,
-      tissueSlices: 0,
-      tissueImages: 0,
-      cells: 0, // TODO: populate this with number of cells
-    };
-    this.localDatabase.getPatients((item: Patient) => item.anatomicalLocations[0] === organId)
-    .toPromise().then(d => {
-      countMetadata.patients = d.length;
-    });
-    this.localDatabase.getTissueSlices((item: TissueSlice) => item.sample.patient.anatomicalLocations[0] === organId)
-    .toPromise().then(d => {
-      countMetadata.tissueSlices = d.length;
-    });
-    this.localDatabase.getTissueImages((item: TissueImage) => item.slice.sample.patient.anatomicalLocations[0] === organId)
-    .toPromise().then(d => {
-      countMetadata.tissueImages = d.length;
-    });
-    return countMetadata;
-  }
-}
-
-export interface CountMetadata {
-  patients: number;
-  tissueSamples: number;
-  tissueSlices: number;
-  tissueImages: number;
-  cells: number;
+  constructor(private readonly localDatabase: LocalDatabaseService) { }
 }
--- src/app/views/organ/organ.component.ts
+++ src/app/views/organ/organ.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { CountMetadata, OrganDataService } from 'src/app/shared/services/organ-data/organ-data.service';
-import { TissueSample } from 'src/app/shared/state/database/database.models';
+import { Component } from '@angular/core';
+
+import { OrganDataService } from '../../shared/services/organ-data/organ-data.service';
 
 /**
  * Component organ view component.
@@ -11,69 +11,16 @@ import { TissueSample } from 'src/app/shared/state/database/database.models';
   styleUrls: ['./organ.component.scss']
 })
 
-export class OrganComponent implements OnInit {
+export class OrganComponent {
   /**
-   * Organ id of organ component
+   * Metadata that is shown on the right hand side table.
    */
-  organId: string;
-  /**
-   * Count metadata of organ component
-   */
-  countMetadata: CountMetadata;
-  /**
-   * Tissue samples of organ component used to display overlays on organ.
-   */
-  tissueSamples: TissueSample[];
-  /**
-   * Tissue sample metadata of organ component displays metadata on hover of overlay.
-   */
-  tissueSampleMetadata: { [label: string]: string }[];
-  /**
-   * Organ image path of organ component displays corresponding image deictated by naviagation path.
-   */
-  organImagePath: string;
+  activeMetadata: [{ [label: string]: string }] | [] = [];
+
   /**
    * Creates an instance of organ component.
-   * @param organService gets metadata and organ path from the service.
+   *
+   * @param organService Service used to fetch organ related data for display.
    */
-  constructor(public readonly organService: OrganDataService) { }
-
-  /**
-   * on init listen to changes in browser navigation path and render organ stuff accordingly.
-   */
-  ngOnInit() {
-    this.getOrganImageSourcePath();
-    this.organService.getActiveOrgan().subscribe(d => {
-      this.organId = d.id;
-      this.getTissueSamples(d.id);
-    });
-  }
-
-  /**
-   * Gets organ image source path displays organ image.
-   */
-  getOrganImageSourcePath() {
-    this.organService.getOrganSourcePath().subscribe(image => {
-      this.organImagePath = image;
-    });
-  }
-
-  /**
-   * Gets tissue samples gets list of tissues samples.
-   */
-  getTissueSamples(organId: string) {
-    this.organService.getAllTissueSamples(organId).subscribe(d => {
-      this.tissueSamples = d;
-    });
-  }
-
-  /**
-   * on overlay, display metadata.
-   * @param tissueSampleId id of tissue sample on which hovered.
-   */
-  onTissueSampleMouseenter(tissueSampleMetadata: { [label: string]: string }) {
-    this.tissueSampleMetadata = [tissueSampleMetadata];
-    this.countMetadata = this.organService.getCounts(this.organId);
-    this.countMetadata.tissueSamples = this.tissueSamples ? this.tissueSamples.length : 0;
-  }
+  constructor(public readonly dataService: OrganDataService) { }
 }
