51f6ead8a78bd70cfd864c74f038cd00492792ea
Gagandeep Singh <singhgag@indiana.edu>
2019-05-07 15:10:05 -0400

    modified mat-input border-radius styles to match mockup + added boilerplate autocomplete

--- src/app/components/ontology-search/ontology-search.component.ts
+++ src/app/components/ontology-search/ontology-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'ccf-ontology-search',
@@ -7,12 +9,22 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./ontology-search.component.scss']
 })
 export class OntologySearchComponent implements OnInit {
-  myControl = new FormControl();
+  formControl = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions$: Observable<string[]>;
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredOptions$ = this.formControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
   }
 
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
