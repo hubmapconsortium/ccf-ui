650fe425df15b156664cd930f4c24b638ba55574
axdanbol <daniel_bolin@yahoo.com>
2019-05-09 13:24:51 -0400

    Added full documentation to ontology tree component

--- src/app/components/ontology-tree/ontology-tree.component.ts
+++ src/app/components/ontology-tree/ontology-tree.component.ts
@@ -6,8 +6,19 @@ import { Observable } from 'rxjs';
 
 import { FlatNode, NodeLike } from './shared/flat-node';
 
+/**
+ * Getter function for 'level' on a flat node.
+ */
 const getLevel = property<FlatNode<any>, number>('level');
+
+/**
+ * Getter function for 'expandable' on a flat node.
+ */
 const isExpandable = property<FlatNode<any>, boolean>('expandable');
+
+/**
+ * Getter function for 'children' on a node like object.
+ */
 const getChildren = property<NodeLike<any>, Observable<any[]> | any[]>('children');
 
 @Component({
@@ -17,24 +28,61 @@ const getChildren = property<NodeLike<any>, Observable<any[]> | any[]>('children
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OntologyTreeComponent<T extends NodeLike<T>> {
+  /**
+   * The node like objects to display in the tree.
+   */
   @Input()
   set nodes(nodes: T | T[]) {
     this.dataSource.data = isArray(nodes) ? nodes : [nodes];
   }
 
+  /**
+   * Emits an event whenever a node has been selected.
+   */
   @Output() nodeSelected = new EventEmitter<T>();
+
+  /**
+   * Emits an event whenever a node is deselected.
+   */
   @Output() nodeDeselected = new EventEmitter<T>();
 
-  readonly indent = '1.5rem';
+  /**
+   * Indentation of each level in the tree.
+   */
+  readonly indent: number | string = '1.5rem';
 
+  /**
+   * Tree controller.
+   */
   readonly control = new FlatTreeControl<FlatNode<T>>(getLevel, isExpandable);
+
+  /**
+   * Node flattener.
+   */
   readonly flattener = new MatTreeFlattener<T, FlatNode<T>>(FlatNode.create, getLevel, isExpandable, getChildren);
+
+  /**
+   * Data source of flat nodes.
+   */
   readonly dataSource = new MatTreeFlatDataSource(this.control, this.flattener);
 
+  /**
+   * Currently selected node.
+   */
   private selectedNode?: FlatNode<T> = undefined;
 
+  /**
+   * Creates an instance of ontology tree component.
+   *
+   * @param cdr The change detector.
+   */
   constructor(private cdr: ChangeDetectorRef) { }
 
+  /**
+   * Expands the tree to show a node and sets the currect selection to that node.
+   *
+   * @param node The node to expand to and select.
+   */
   expandAndSelect(node: T): void {
     const { cdr, control } = this;
 
@@ -51,6 +99,7 @@ export class OntologyTreeComponent<T extends NodeLike<T>> {
     const flatNode = control.dataNodes.find(flat => flat.original === node);
 
     // Expand nodes
+    control.collapseAll();
     for (const flat of parentFlatNodes) { control.expand(flat); }
 
     // Select the node
@@ -61,22 +110,33 @@ export class OntologyTreeComponent<T extends NodeLike<T>> {
   }
 
   // Internal
-  getDisabled(type: 'inner' | 'leaf'): boolean {
-    return type === 'leaf' ? true : undefined;
-  }
-
-  getAriaLabel(type: 'inner' | 'leaf', node: FlatNode<T>): string {
-    return type === 'inner' ? `toggle ${node.name}` : undefined;
-  }
-
+  /**
+   * Determines whether a node can be expanded.
+   *
+   * @param node The node to test.
+   * @returns True if the node has children.
+   */
   isInnerNode(_index: number, node: FlatNode<T>): boolean {
     return node.expandable;
   }
 
+  /**
+   * Determines whether a node is currently selected.
+   * Only a single node can be selected at any time.
+   *
+   * @param node  The node to test.
+   * @returns True if the node is the currently selected node.
+   */
   isSelected(node: FlatNode<T>): boolean {
     return node === this.selectedNode;
   }
 
+  /**
+   * Sets a node to be the currently selected node.
+   * Deselects the previously selected node.
+   *
+   * @param node The node to select.
+   */
   select(node: FlatNode<T>): void {
     const { nodeDeselected, nodeSelected, selectedNode } = this;
     if (node === selectedNode) {
--- src/app/components/ontology-tree/shared/flat-node.ts
+++ src/app/components/ontology-tree/shared/flat-node.ts
@@ -1,20 +1,57 @@
 import { isArrayLikeObject } from 'lodash';
 import { isObservable, Observable } from 'rxjs';
 
+/**
+ * An object which has a tree node like structure with a label.
+ */
 export interface NodeLike<T extends NodeLike<T>> {
-  name: string;
+  /**
+   * The display name of this node.
+   */
+  label: string;
+
+  /**
+   * Parent node - undefined if this is a root node.
+   */
   parent?: T;
+
+  /**
+   * Child nodes - undefined if this is a leaf node.
+   */
   children?: Observable<T[]> | T[];
 }
 
+/**
+ * A flat tree node.
+ */
 export class FlatNode<T extends NodeLike<T>> {
-  get name(): string { return this.original.name; }
+  /**
+   * Same as the original node's label.
+   */
+  get label(): string { return this.original.label; }
+
+  /**
+   * Indicates whether this node has children.
+   */
   expandable: boolean;
 
+  /**
+   * Creates a flat node from a node like object and a level in the tree.
+   *
+   * @param original The original node like object.
+   * @param level The level of the new flat node in the tree.
+   * @returns The newly created flat node.
+   */
   static create<T extends NodeLike<T>>(original: T, level: number): FlatNode<T> {
     return new FlatNode(original, level);
   }
 
+  /**
+   * Creates an instance of flat node.
+   *
+   * @param original The original node like object.
+   * @param level The level of the new flat node in the tree.
+   */
   constructor(readonly original: T, readonly level: number) {
     const { children } = original;
     if (isObservable(children)) {
