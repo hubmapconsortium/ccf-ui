100d9e3a60379b521c05459311aa80b57d1d86e1
axdanbol <daniel_bolin@yahoo.com>
2019-05-10 13:06:14 -0400

    Added tests for ontology service

--- src/app/shared/services/ontology/ontology.service.spec.ts
+++ src/app/shared/services/ontology/ontology.service.spec.ts
@@ -1,8 +1,11 @@
 import { Component, NgModule, Type } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { Shallow } from 'shallow-render';
+import { keyBy } from 'lodash';
 
 import { OntologyService } from './ontology.service';
+import { OntologyNode } from '../../state/ontology/ontology.model';
+import { take } from 'rxjs/operators';
 
 @Component({ selector: 'ccf-test', template: '' })
 class TestComponent { }
@@ -25,5 +28,39 @@ describe('OntologyService', () => {
     service = get(OntologyService);
   });
 
-  // TODO tests
+  describe('rootNode', () => {
+    const node = { id: 'root' } as OntologyNode;
+    let valuePromise: Promise<OntologyNode>;
+
+    beforeEach(() => {
+      store.reset({ ontology: { root: node.id, nodes: { [node.id]: node } } });
+      valuePromise = service.rootNode.pipe(take(1)).toPromise();
+    });
+
+    it('emits the root node of the tree', async () => {
+      expect(await valuePromise).toEqual(node);
+    });
+  });
+
+  describe('getChildren(node)', () => {
+    const parent = { id: 'parent', children: ['child1', 'child2'] } as OntologyNode;
+    const child1 = { id: 'child1' } as OntologyNode;
+    const child2 = { id: 'child2' } as OntologyNode;
+    const nodes = keyBy([parent, child1, child2], 'id');
+    let result: OntologyNode[];
+
+    beforeEach(() => {
+      store.reset({ ontology: { nodes } });
+      result = service.getChildren(parent);
+    });
+
+    it('fetches the child nodes', () => {
+      expect(result).toEqual(jasmine.arrayContaining([child1, child2]));
+    });
+
+    it('can be called without \'this\'', () => {
+      const getChildren = service.getChildren;
+      expect(() => getChildren(parent)).not.toThrow();
+    });
+  });
 });
