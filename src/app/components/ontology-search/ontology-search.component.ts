8632974b633f0baaa24f1e8f6ca0ffe6044359e4
Gagandeep Singh <singhgag@indiana.edu>
2019-05-13 17:12:59 -0400

    added documentation + updated sort order and logic for sort by index

--- src/app/components/ontology-search/ontology-search.component.ts
+++ src/app/components/ontology-search/ontology-search.component.ts
@@ -6,40 +6,66 @@ import { map, startWith } from 'rxjs/operators';
 
 import { OntologySearchService, SearchResult } from '../../shared/services/ontology-search/ontology-search.service';
 
+/**
+ * Ontology Search Component responsible for searching the ontology
+ */
 @Component({
   selector: 'ccf-ontology-search',
   templateUrl: './ontology-search.component.html',
   styleUrls: ['./ontology-search.component.scss']
 })
 export class OntologySearchComponent implements OnInit {
+  /**
+   * Output event-emitter which emits the id of the OntologyNode whose label was
+   * selected by the user in the search-results
+   */
   @Output() selected = new EventEmitter<string>();
+  /**
+   * Instance of FormControl - tracks the value and validation status of an individual form control
+   */
   formControl = new FormControl();
-  filteredOptions$: Observable<SearchResult[]>;
+  /**
+   * Observable which provides the filtered search results
+   */
+  filteredResults$: Observable<SearchResult[]>;
 
+  /**
+   * Creates an instance of ontology search component.
+   * @param searchService instance of OntologySearchService which provides all the search functionality
+   */
   constructor(private searchService: OntologySearchService) { }
 
+  /**
+   * on-init lifecycle hook for this component -
+   * gets the searched value from the view, sends it to the filter function in the OntologyService,
+   * and gets the search results from the service
+   */
   ngOnInit() {
-    this.filteredOptions$ = this.formControl.valueChanges.pipe(
+    this.filteredResults$ = this.formControl.valueChanges.pipe(
       startWith(''),
       map(value => this.searchService.filter(value)
-        .sort((entry1, entry2) => entry1.index - entry2.index)
-        .sort(entry1 => {
-          if (entry1.displayLabel.join().includes('(')) {
-            return 1;
-          } else {
-            return -1;
-          }
-        })
+        .sort((entry1, entry2) => entry1.index - entry2.index) // first sort by substring match index
+        .sort((entry1, entry2) => entry2.displayLabel.join().localeCompare(entry1.displayLabel.join())) // then sort lexically
+        .sort(entry1 => entry1.displayLabel.join().includes('(') ? 1 : -1) // then sort by if it was found in the label or synonym-labels
       )
     );
   }
 
+  /**
+   * Callback function triggered when the user selects a value from search results
+   * @param event instance of MatAutocompleteSelectedEvent provides the selected value
+   */
   onSelect(event: MatAutocompleteSelectedEvent): void {
     if (event && event.option && event.option.value) {
       this.selected.emit(event.option.value.node.id);
     }
   }
 
+  /**
+   * A formatter function to enable different display and selected value
+   * @param option a search result entry
+   * @returns a part of the search result entry to be displayed as a display value
+   */
   displayFormatter(option: SearchResult): string {
     if (option) {
       return option.displayLabel.join();
--- src/app/shared/services/ontology-search/ontology-search.service.ts
+++ src/app/shared/services/ontology-search/ontology-search.service.ts
@@ -5,11 +5,18 @@ import { Observable } from 'rxjs';
 import { OntologyNode } from '../../state/ontology/ontology.model';
 import { OntologyState } from '../../state/ontology/ontology.state';
 
+/**
+ * Search result interface type for the search results
+ */
 export interface SearchResult {
-  index: number;
-  displayLabel: string[];
-  node: OntologyNode;
+  index: number; // ensures order of search-results
+  displayLabel: string[]; // label to be displayed in the view
+  node: OntologyNode; // instance of OntologyNode, provides data associated with a search result
 }
+
+/**
+ * Injectable OntologySearchService responsible for search result computations
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -19,33 +26,47 @@ export class OntologySearchService {
    */
   @Select(OntologyState.nodes)
   stateObservable: Observable<OntologyNode[]>;
+  /**
+   * An array of all Ontology nodes fetched from the state
+   */
   private ontologyNodes: OntologyNode[] = [];
 
+  /**
+   * Creates an instance of ontology search service,
+   * subscribes to the changes in the ontology state
+   */
   constructor() {
     this.stateObservable.subscribe((ontologyNodes) => {
       this.ontologyNodes = ontologyNodes;
     });
   }
 
+  /**
+   * Searches the ontology with the search-term
+   * @param value the search term
+   * @returns an array of search-results
+   */
   filter(value: string): SearchResult[] {
-    const filterValue = typeof value === 'string' && value.length && value.toLowerCase();
+    const searchValue = typeof value === 'string' && value.length && value.toLowerCase();
     const searchResults = new Map<string, SearchResult>();
 
     if (this.ontologyNodes) {
-      this.ontologyNodes.forEach((node, index) => {
-        const condition = node.label.toLowerCase().includes(filterValue);
+      this.ontologyNodes.forEach((node) => {
+        const condition = node.label.toLowerCase().includes(searchValue);
+
         if (condition && !searchResults.get(node.id)) {
           searchResults.set(node.id, {
-            index: index,
-            displayLabel: this.formatLabel(node.label, filterValue),
+            index: this.getIndexOfMatch(node.label, searchValue),
+            displayLabel: this.formatLabel(node.label, searchValue),
             node: node
           });
         } else {
-          const match = node.synonymLabels.find((label) => label.toLowerCase().includes(filterValue));
+          const match = node.synonymLabels.find((label) => label.toLowerCase().includes(searchValue));
+
           if (match && !searchResults.get(node.id)) {
             searchResults.set(node.id, {
-              index: index,
-              displayLabel: this.formatLabel(node.label + ' (' + match + ')', filterValue),
+              index: this.getIndexOfMatch(node.label + ' (' + match + ')', searchValue),
+              displayLabel: this.formatLabel(node.label + ' (' + match + ')', searchValue),
               node: node
             });
           }
@@ -56,13 +77,23 @@ export class OntologySearchService {
     return Array.from(searchResults.values());
   }
 
-  formatLabel(label: string, filterValue: string): string[] {
-    const indexOfMatch = label.toLowerCase().indexOf(filterValue);
-    return [
-      label.substr(0, indexOfMatch),
-      label.substr(indexOfMatch, filterValue.length),
-      label.substr(indexOfMatch + filterValue.length, label.length)
-    ];
+
+  getIndexOfMatch(label: string, searchValue: string) {
+    return label.toLowerCase().indexOf(searchValue);
   }
 
+  /**
+   * Formats label based on where the search-term was found in the OntologyNode
+   * @param label label or first synonym-label of OntologyNode which has the search-term
+   * @param searchValue search-term
+   * @returns an array in the form of [prefix, search-term, suffix]
+   */
+  formatLabel(label: string, searchValue: string): string[] {
+    const index  = this.getIndexOfMatch(label, searchValue);
+    return [
+      label.substr(0, index),
+      label.substr(index, searchValue.length),
+      label.substr(index + searchValue.length, label.length)
+    ];
+  }
 }
