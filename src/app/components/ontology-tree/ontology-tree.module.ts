120bedf8ed7a1fc3c9fb8dbe109f5bad9301c4d8
axdanbol <daniel_bolin@yahoo.com>
2019-05-15 14:12:27 -0400

    Added tooltips to the tree ui

--- src/app/components/ontology-tree/ontology-tree.module.ts
+++ src/app/components/ontology-tree/ontology-tree.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule, MatTreeModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatTooltipModule, MatTreeModule } from '@angular/material';
 
 import { OntologyTreeComponent } from './ontology-tree.component';
 
@@ -9,6 +9,7 @@ import { OntologyTreeComponent } from './ontology-tree.component';
     CommonModule,
     MatButtonModule,
     MatIconModule,
+    MatTooltipModule,
     MatTreeModule
   ],
   declarations: [OntologyTreeComponent],
