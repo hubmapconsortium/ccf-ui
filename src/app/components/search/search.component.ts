1dcb3b0e4de07de4e0d2e454b7282ea5ee183276
axdanbol <daniel_bolin@yahoo.com>
2019-05-17 13:53:27 -0400

    Removed tmc prefixes without affecting the state

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { OntologyNode } from 'src/app/shared/state/ontology/ontology.model';
+import { OntologyState } from 'src/app/shared/state/ontology/ontology.state';
 
 import { OntologyService } from '../../shared/services/ontology/ontology.service';
 import { SearchService } from '../../shared/services/search/search.service';
 import { OntologyTreeComponent } from '../ontology-tree/ontology-tree.component';
-import { OntologyNode } from 'src/app/shared/state/ontology/ontology.model';
-import { Tree } from '@angular/router/src/utils/tree';
-import { Store } from '@ngxs/store';
-import { OntologyState } from 'src/app/shared/state/ontology/ontology.state';
 
 /**
  * Container component for search filters.
@@ -27,7 +26,7 @@ export class SearchComponent {
   */
   filterCategories = new Map([
     ['Technologies', ['IMS', 'MxIF', 'AF', 'PAS', 'IHC']],
-    ['TMCs', ['Vanderbilt', 'UCSD', 'Stanford', 'Florida', 'CalTech']]
+    ['TMCs', ['TMC-Vanderbilt', 'TMC-UCSD', 'TMC-Stanford', 'TMC-Florida', 'TMC-CalTech']]
   ]);
 
   /**
@@ -35,6 +34,7 @@ export class SearchComponent {
    *
    * @param ontology The service used to interact with the ontology.
    * @param search Service used to update the search state.
+   * @param store The global state.
    */
   constructor(
     readonly ontology: OntologyService,
