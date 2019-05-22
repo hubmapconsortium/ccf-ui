b1d288292f0c10c945475f34b3b72d9aafdfa2ce
axdanbol <daniel_bolin@yahoo.com>
2019-05-22 11:11:55 -0400

    Intermediate commit (overlays mostly done)

--- src/app/views/tissue/tissue.component.ts
+++ src/app/views/tissue/tissue.component.ts
@@ -1,13 +1,20 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, ElementRef, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
-import { capitalize as loCapitalize } from 'lodash';
-import openSeaDragon from 'openseadragon';
+import 'svg-overlay';
+
+import { AfterViewInit, Component, ElementRef, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { capitalize } from 'lodash';
+import { Viewer } from 'openseadragon';
 import { Observable, Subscription } from 'rxjs';
-import { mergeAll, mergeMap, take } from 'rxjs/operators';
-import { TissueOverlay } from 'src/app/shared/state/database/database.models';
-import { svgOverlay } from 'svg-overlay';
+import { map, share } from 'rxjs/operators';
 
 import { TissueDataService } from '../../shared/services/tissue-data/tissue-data.service';
+import { TissueOverlay } from '../../shared/state/database/database.models';
+
+const enum OverlayVisibility {
+  // Note: These are used with bit operations. Do NOT change.
+  Hidden = 0,
+  Visible = 1,
+  AlwaysVisible = 2
+}
 
 /**
  * Component for viewing an individual tissue image
@@ -17,100 +24,69 @@ import { TissueDataService } from '../../shared/services/tissue-data/tissue-data
   templateUrl: './tissue.component.html',
   styleUrls: ['./tissue.component.scss']
 })
-export class TissueComponent implements OnInit, OnDestroy {
-  /**
-   * Tissue source path subscription for the observable returned from the data-service with tissue-source-path
-   */
-  private tissueSourcePathSubscription: Subscription;
-  /**
-   * Tissue metadata subscription for the observable returned from the data-service with tissue-metadata
-   */
-  private tissueMetadataSubscription: Subscription;
-  /**
-   * openseadragon viewer reference
-   */
-  private viewer: any;
+export class TissueComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('viewerEl') viewerEl: ElementRef;
+  @ViewChild('overlayEl') overlayEl: ElementRef;
 
-  private overlaysSubsription: Subscription;
+  organName$ = this.dataService.getOrganName().pipe(map(capitalize));
+  metadata$ = this.dataService.getMetadata().pipe(map(data => [data]));
+  tissueOverlay$: Observable<TissueOverlay[]> = this.dataService.getTissueOverlays().pipe(share());
 
-  private tissueOverlays$: Observable<TissueOverlay[]>;
-  /**
-   * Tissue metadata
-   */
-  tissueMetadata: { [label: string]: string }[];
-  /**
-   * Organ name
-   */
-  organName: string;
-  /**
-   * Reference to 'tissueView' element view-child
-   */
-  @ViewChild('tissueView') tissueView: ElementRef;
+  viewer: any;
+  overlayWidth = 0;
+  overlayHeight = 0;
+  overlayViewBox = '0 0 0 0';
 
-  /**
-   * Creates an instance of tissue component.
-   * @param tissueDataService instance of TissueDataService
-   */
-  constructor(private readonly tissueDataService: TissueDataService,
-    private readonly renderer: Renderer2,
-    private readonly http: HttpClient) {
-      console.log(svgOverlay);
-  }
+  overlayVisibility = new WeakMap<TissueOverlay, OverlayVisibility>();
 
-  /**
-   * on init lifecycle hook for this component - instantiates a subscription object for the
-   * observable with tissue source path from the data service
-   */
-  ngOnInit() {
-    this.tissueOverlays$ = this.tissueDataService.getTissueOverlays();
-    this.tissueSourcePathSubscription = this.tissueDataService.getTissueSourcePath()
-      .subscribe(path => this.launchTissueViewer(path));
-    this.tissueMetadataSubscription = this.tissueDataService.getMetadata()
-    // enclosed metadata inside array becasue @Input of metadata component expects array.
-      .subscribe(metadata => { this.tissueMetadata = [metadata]; });
-    this.tissueDataService.getOrganName().subscribe((name) => this.organName = loCapitalize(name));
-  }
+  private tileSourceSubscription: Subscription;
 
-  private getHeaders(): HttpHeaders {
-    const headers = new HttpHeaders();
-    headers.set('Accept', 'image/svg+xml');
-    return headers;
-  }
 
-  private setOverlays(): void {
-    const headers = this.getHeaders();
-    const result = this.tissueOverlays$.pipe(mergeAll(), mergeMap(overlay => this.http.get(
-      overlay.overlayUrl, { headers, responseType : 'text' })));
-    const svgOverlays  = this.viewer.svgOverlay();
-    const el = this.renderer.selectRootElement(svgOverlays.node());
-    const parser = new DOMParser();
-    this.overlaysSubsription = result.pipe(take(1)).subscribe((overlaySvg) => {
-      const doc = parser.parseFromString(overlaySvg, 'image/svg+xml');
-      // const doc = parser.parseFromString('<rect></rect>', 'image/svg+xml');
-      // doc.documentElement.setAttribute('style', 'color:red; border: 1px solid blue;');
-      // Trying different values to see if overlay is displayed on the screen.
-      doc.documentElement.setAttribute('x', '0.5');
-      doc.documentElement.setAttribute('y', '0.5');
-      doc.documentElement.setAttribute('height', '100');
-      doc.documentElement.setAttribute('width', '100');
-      el.append(doc.documentElement);
+  constructor(
+    private dataService: TissueDataService,
+    private renderer: Renderer2
+  ) { }
+
+  ngAfterViewInit() {
+    // This hook doesn't like changes! Do the work at a later cycle.
+    setTimeout(() => {
+      this.initializeViewer();
+      this.tileSourceSubscription = this.dataService
+        .getTissueSourcePath()
+        .subscribe(path => this.viewer.open(path));
     });
-
   }
 
-  /**
-   * Launches tissue viewer
-   * @param tissueSourcePath tissue-source-path string returned from the data service
-   */
-  launchTissueViewer(tissueSourcePath: string): void {
-    if (this.viewer) {
-      this.viewer.destroy();
-    }
+  ngOnDestroy() {
+    this.tileSourceSubscription.unsubscribe();
+    this.viewer.destroy();
+  }
 
-    this.viewer = openSeaDragon({
-      element: this.tissueView.nativeElement,
+  displayState(cond: any): 'none' | undefined {
+    return !!cond ? undefined : 'none';
+  }
+
+  isVisible(overlay: TissueOverlay): boolean {
+    const visibility = this.overlayVisibility.get(overlay) || OverlayVisibility.Hidden;
+    // return visibility !== 0;
+    return true; // For testing
+  }
+
+  setVisibility(overlay: TissueOverlay, visibility: 'on' | 'off', always?: 'always'): void {
+    const current = this.overlayVisibility.get(overlay) || OverlayVisibility.Hidden;
+    const bits = always ? OverlayVisibility.AlwaysVisible : OverlayVisibility.Visible;
+    // tslint:disable-next-line:no-bitwise
+    const updated = visibility === 'on' ? current | bits : current & ~bits;
+
+    this.overlayVisibility.set(overlay, updated);
+  }
+
+  private initializeViewer(): void {
+    const { renderer, overlayEl, viewerEl } = this;
+    // Initialize OpenSeadragon Viewer
+    const viewer = this.viewer = new Viewer({
+      element: viewerEl.nativeElement,
       prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/images/',
-      tileSources: tissueSourcePath,
       showNavigator: true,
       navigatorPosition: 'ABSOLUTE',
       navigatorTop: '2.5rem',
@@ -121,31 +97,23 @@ export class TissueComponent implements OnInit, OnDestroy {
       visibilityRatio: 1
     });
 
-    this.setOverlays();
+    // Add tissue overlay element
+    const overlay = viewer.svgOverlay();
+    renderer.appendChild(overlay.node(), overlayEl.nativeElement);
+
+    // Setup event handling
+    viewer.addHandler('open', () => this.setOverlaySize());
   }
 
-  /**
-   * on destroy lifecycle hook of this component - unsubscribes to subscriptions
-   */
-  ngOnDestroy() {
-    if (this.tissueSourcePathSubscription) {
-      this.tissueSourcePathSubscription.unsubscribe();
-    }
+  private setOverlaySize(): void {
+    const { viewer: { viewport, world } } = this;
+    const { x, y, width, height } = viewport.getBounds();
+    // There should only be a single image loaded
+    // There might be a better way to get this!
+    const { source: { dimensions: { x: imageWidth, y: imageHeight } } } = world.getItemAt(0);
 
-    if (this.tissueMetadataSubscription) {
-      this.tissueMetadataSubscription.unsubscribe();
-    }
-
-    if (this.overlaysSubsription) {
-      this.overlaysSubsription.unsubscribe();
-    }
-
-    if (this.viewer) {
-      this.viewer.destroy();
-    }
-  }
-
-  showTissueOverlay(overlay: TissueOverlay) {
-    // Called when you hover over the buttons that display labels of different tissue overlays.
+    this.overlayWidth = 2 * x + width;
+    this.overlayHeight = 2 * y + height;
+    this.overlayViewBox = `0 0 ${imageWidth} ${imageHeight}`;
   }
 }
