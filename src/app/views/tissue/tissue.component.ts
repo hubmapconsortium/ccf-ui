bc7e8bcd0da50b828017eb83e1eeefa4a908ba0b
axdanbol <daniel_bolin@yahoo.com>
2019-05-22 17:34:45 -0400

    Tweaked tissue overlay + added overlay buttons + docs

--- src/app/views/tissue/tissue.component.ts
+++ src/app/views/tissue/tissue.component.ts
@@ -9,8 +9,10 @@ import { map, share } from 'rxjs/operators';
 import { TissueDataService } from '../../shared/services/tissue-data/tissue-data.service';
 import { TissueOverlay } from '../../shared/state/database/database.models';
 
+/**
+ * Bit flags indicating the current visibility status of an overlay.
+ */
 const enum OverlayVisibility {
-  // Note: These are used with bit operations. Do NOT change.
   Hidden = 0,
   Visible = 1,
   AlwaysVisible = 2
@@ -25,28 +27,77 @@ const enum OverlayVisibility {
   styleUrls: ['./tissue.component.scss']
 })
 export class TissueComponent implements AfterViewInit, OnDestroy {
+  /**
+   * Element on to which Open Seadragon is mounted.
+   */
   @ViewChild('viewerEl') viewerEl: ElementRef;
+
+  /**
+   * Top svg element of the overlays. Will be moved into Open Seadragon's view.
+   */
   @ViewChild('overlayEl') overlayEl: ElementRef;
 
+  /**
+   * Observable emitting the name of the organ for the current tissue.
+   */
   organName$ = this.dataService.getOrganName().pipe(map(capitalize));
+
+  /**
+   * Observable emitting the metadata for the current tissue.
+   */
   metadata$ = this.dataService.getMetadata().pipe(map(data => [data]));
+
+  /**
+   * Observable emitting the overlays for the current tissue.
+   */
   tissueOverlay$: Observable<TissueOverlay[]> = this.dataService.getTissueOverlays().pipe(share());
 
+  /**
+   * The Open Seadragon Viewer instance.
+   * Refer to https://openseadragon.github.io/ for documentation.
+   */
   viewer: any;
+
+  /**
+   * Overlay width in viewport coordinates (0 <= width <= 1).
+   */
   overlayWidth = 0;
+
+  /**
+   * Overlay height in viewport coordinates (0 <= height <= 1).
+   */
   overlayHeight = 0;
+
+  /**
+   * Overlay view box in original image coordinates.
+   */
   overlayViewBox = '0 0 0 0';
 
-  overlayVisibility = new WeakMap<TissueOverlay, OverlayVisibility>();
+  /**
+   * Mapping containing the current visibility state for each overlay.
+   */
+  private overlayVisibility = new WeakMap<TissueOverlay, OverlayVisibility>();
 
+  /**
+   * Subscription for the tile source url.
+   */
   private tileSourceSubscription: Subscription;
 
-
+  /**
+   * Creates an instance of tissue component.
+   *
+   * @param dataService The service used to fetch the tissue information.
+   * @param renderer Used to interact with Open Seadragon's html elements.
+   */
   constructor(
     private dataService: TissueDataService,
     private renderer: Renderer2
   ) { }
 
+  /**
+   * Angular's AfterViewInit hook.
+   * Initializes the Open Seadragon viewer.
+   */
   ngAfterViewInit() {
     // This hook doesn't like changes! Do the work at a later cycle.
     setTimeout(() => {
@@ -57,30 +108,65 @@ export class TissueComponent implements AfterViewInit, OnDestroy {
     });
   }
 
+  /**
+   * Angular's OnDestroy hook.
+   * Destroys the Open Seadragon viewer and cleans up subscriptions.
+   */
   ngOnDestroy() {
     this.tileSourceSubscription.unsubscribe();
     this.viewer.destroy();
   }
 
+  /**
+   * Utility function for setting the `display` attribute on an element.
+   * Returns `none` when the provided condition is falsy.
+   *
+   * @param cond The condition on which to display/hide an element.
+   * @returns `none` if `cond` is falsy else `undefined`
+   */
   displayState(cond: any): 'none' | undefined {
     return !!cond ? undefined : 'none';
   }
 
+  /**
+   * Determines whether an overlay is visible.
+   *
+   * @param overlay The overlay to test.
+   * @returns `true` if the visibility is `Visible` or `AlwaysVisible`, else `false`.
+   */
   isVisible(overlay: TissueOverlay): boolean {
     const visibility = this.overlayVisibility.get(overlay) || OverlayVisibility.Hidden;
-    // return visibility !== 0;
-    return true; // For testing
+    return visibility !== OverlayVisibility.Hidden;
   }
 
-  setVisibility(overlay: TissueOverlay, visibility: 'on' | 'off', always?: 'always'): void {
+  /**
+   * Sets, unsets, or toggles the visibility for an overlay.
+   *
+   * @param overlay The overlay on which to set the visibility.
+   * @param visibility Whether to set, unset, or toggle the visibility.
+   * @param [always] If present the always visible flag will be updated instead of the regular visibility flag.
+   */
+  setVisibility(overlay: TissueOverlay, visibility: 'on' | 'off' | 'toggle', always?: 'always'): void {
     const current = this.overlayVisibility.get(overlay) || OverlayVisibility.Hidden;
     const bits = always ? OverlayVisibility.AlwaysVisible : OverlayVisibility.Visible;
-    // tslint:disable-next-line:no-bitwise
-    const updated = visibility === 'on' ? current | bits : current & ~bits;
+    let updated = current;
+
+    // tslint:disable:no-bitwise
+    if (visibility === 'on') {
+      updated |= bits;
+    } else if (visibility === 'off') {
+      updated &= ~bits;
+    } else {
+      updated ^= bits;
+    }
+    // tslint:enable:no-bitwise
 
     this.overlayVisibility.set(overlay, updated);
   }
 
+  /**
+   * Initializes the Open Seadragon Viewer.
+   */
   private initializeViewer(): void {
     const { renderer, overlayEl, viewerEl } = this;
     // Initialize OpenSeadragon Viewer
@@ -105,6 +191,9 @@ export class TissueComponent implements AfterViewInit, OnDestroy {
     viewer.addHandler('open', () => this.setOverlaySize());
   }
 
+  /**
+   * Sets the size and view box on the top svg overlay element.
+   */
   private setOverlaySize(): void {
     const { viewer: { viewport, world } } = this;
     const { x, y, width, height } = viewport.getBounds();
