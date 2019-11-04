253549b53a82fc733279d078b4bd0bb3c172467a
axdanbol <daniel_bolin@yahoo.com>
2019-11-04 14:41:30 -0500

    Updated packages + switched to gltf model loader

--- src/app/app-render-init.module.ts
+++ src/app/app-render-init.module.ts
@@ -1,29 +1,29 @@
 import { InjectionToken, NgModule } from '@angular/core';
 import { registerLoaders } from '@loaders.gl/core';
-import { OBJLoader } from '@loaders.gl/obj';
+import { GLTFScenegraphLoader } from '@luma.gl/addons';
 import { Style } from 'mapbox-gl';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 export const BLANK_MAPBOX_STYLE = new InjectionToken<Style>('Blank Mapbox style', {
   providedIn: 'root',
   factory: () => ({
-    'version': 8,
-    'name': 'Blank',
-    'center': [0, 0],
-    'zoom': 0,
-    'sources': {},
-    'sprite': 'https://cdn.jsdelivr.net/gh/lukasmartinelli/osm-liberty@gh-pages/sprites/osm-liberty',
-    'glyphs': 'https://cdn.jsdelivr.net/gh/orangemug/font-glyphs@gh-pages/glyphs/{fontstack}/{range}.pbf',
-    'layers': [
+    id: 'blank',
+    version: 8,
+    name: 'Blank',
+    center: [0, 0],
+    zoom: 0,
+    sources: {},
+    sprite: 'https://cdn.jsdelivr.net/gh/lukasmartinelli/osm-liberty@gh-pages/sprites/osm-liberty',
+    glyphs: 'https://cdn.jsdelivr.net/gh/orangemug/font-glyphs@gh-pages/glyphs/{fontstack}/{range}.pbf',
+    layers: [
       {
-        'id': 'background',
-        'type': 'background',
-        'paint': {
+        id: 'background',
+        type: 'background',
+        paint: {
           'background-color': 'rgba(255,255,255,1)'
         }
       }
-    ],
-    'id': 'blank'
+    ]
   })
 });
 
@@ -33,6 +33,6 @@ export const BLANK_MAPBOX_STYLE = new InjectionToken<Style>('Blank Mapbox style'
 })
 export class AppRenderInitModule {
   constructor() {
-    registerLoaders([OBJLoader]);
+    registerLoaders([GLTFScenegraphLoader]);
   }
 }
