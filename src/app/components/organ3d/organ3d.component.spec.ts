bdd5a735790bd0b66bc2fd741c3e6123331a3995
axdanbol <daniel_bolin@yahoo.com>
2019-11-08 13:39:54 -0500

    Added more tests

--- src/app/components/organ3d/organ3d.component.spec.ts
+++ src/app/components/organ3d/organ3d.component.spec.ts
@@ -1,12 +1,15 @@
 import { DebugElement, Type } from '@angular/core';
+import { Map } from 'mapbox-gl';
+import { MapComponent } from 'ngx-mapbox-gl';
 import { Shallow } from 'shallow-render';
+import { Scene } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import { Organ3dComponent } from './organ3d.component';
 import { Organ3dModule } from './organ3d.module';
-import { NgxMapboxGLModule, MapComponent } from 'ngx-mapbox-gl';
-import { Map } from 'mapbox-gl';
 
 describe('Organ3dComponent', () => {
+  const base = 'test.url';
   let component: Organ3dComponent;
   let find: (cssOrDirective: string | Type<any>) => DebugElement & DebugElement[];
   let get: <T>(type: Type<T>) => T;
@@ -14,22 +17,54 @@ describe('Organ3dComponent', () => {
 
   beforeEach(async () => {
     shallow = new Shallow(Organ3dComponent, Organ3dModule);
-    ({ instance: component, get, find } = await shallow.render({ bind: { base: 'test.url' } }));
+    ({ instance: component, get, find } = await shallow.render({ bind: { base } }));
   });
 
   describe('component', () => {
+    let map: Map;
+    let scene: Scene;
+    let loadSpy: jasmine.Spy;
+
+    function setLoadValue(value: any): void {
+      loadSpy.and.callFake((_url, done) => done(value));
+    }
+
+    function setErrorValue(value: any): void {
+      loadSpy.and.callFake((_url, _done, _progress, error) => error(value));
+    }
+
+    beforeEach(() => {
+      map = jasmine.createSpyObj('Map', ['addLayer']);
+      scene = new Scene();
+      loadSpy = spyOn(GLTFLoader.prototype, 'load');
+    });
+
     it('exists', () => {
       expect(component).toBeTruthy();
     });
 
-    describe('onMapLoad(map)', () => {
-      let map: Map;
-
+    describe('ngOnChange(changes)', () => {
       beforeEach(() => {
-        map = jasmine.createSpyObj('Map', ['addLayer']);
         component.onMapLoad(map);
+        component['baseScene'] = scene;
+        loadSpy.calls.reset();
       });
 
+      it('loads a new base model if changed', () => {
+        component.ngOnChanges({ base: {} as any });
+
+        expect(loadSpy).toHaveBeenCalledTimes(1);
+      });
+
+      it('updates base rotation if changed', () => {
+        const spy = spyOn(component as any, 'setPositionScaleRotation').and.callThrough();
+        component.ngOnChanges({ baseRotation: {} as any });
+
+        expect(spy).toHaveBeenCalled();
+      });
+    });
+
+    describe('onMapLoad(map)', () => {
       it('is called on mgl-map\'s load event', () => {
         const mglMap = find(MapComponent);
         const spy = spyOn(component, 'onMapLoad');
@@ -39,7 +74,15 @@ describe('Organ3dComponent', () => {
       });
 
       it('adds layers to the map', () => {
+        component.onMapLoad(map);
         expect(map.addLayer).toHaveBeenCalled();
+      });
+
+      it('loads the base model', () => {
+        setLoadValue({ scene });
+        component.onMapLoad(map);
+
+        expect(loadSpy).toHaveBeenCalled();
       });
     });
   });
