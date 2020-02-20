45878208c9475a8416d909be341c354f80b202ab
axdanbol <daniel_bolin@yahoo.com>
2020-02-20 11:49:13 -0500

    Implemented header/logo bar structure (WIP)

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 
 
 @Component({
   selector: 'ccf-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HeaderComponent {
+  @Output() logoClicked = new EventEmitter<void>();
+  @Output() downloadClicked = new EventEmitter<void>();
+  @Output() infoClicked = new EventEmitter<void>();
 }
--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './header.component';
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatToolbarModule],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
 })
