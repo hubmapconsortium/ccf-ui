04dac123fff92be346928fe7281e0019e40ce670
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-03-06 10:38:51 -0500

    updates to filters data bindings

--- src/app/modules/filters/filters-content/filters-content.component.ts
+++ src/app/modules/filters/filters-content/filters-content.component.ts
@@ -1,29 +1,21 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'ccf-filters-content',
   templateUrl: './filters-content.component.html',
   styleUrls: ['./filters-content.component.scss']
 })
-export class FiltersContentComponent implements OnInit {
-  filters: Record<string, unknown> = {};
+export class FiltersContentComponent {
+  @Input() filters: Record<string, unknown> = { tmc: [], technologies: []};
+  @Output() filtersChange = new EventEmitter<Record<string, unknown>>();
   @Output() applyFilters = new EventEmitter<Record<string, unknown>>();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  updateTechnologies(event: string[]): void {
-    this.filters = {...this.filters, technologies: event};
-  }
-
-  updateTMC(event: string[]): void {
-    this.filters = {...this.filters, tmc: event};
+  updateFilter(value: unknown, key: string): void {
+    this.filters = {...this.filters, [key] : value};
+    this.filtersChange.emit(this.filters);
   }
 
   applyButtonClick(): void {
     this.applyFilters.emit(this.filters);
   }
-
 }
