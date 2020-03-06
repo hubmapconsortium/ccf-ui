53857c0064b6ad1ad356dadd05913215a8bd09b0
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-03-06 10:59:46 -0500

    Add dropdown menu to filters-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
-import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
-import { DrawerModule } from "./shared/components/drawer/drawer.module";
-import { DropdownModule } from "./shared/components/dropdown/dropdown.module";
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DrawerModule } from './shared/components/drawer/drawer.module';
+import { DropdownModule } from './shared/components/dropdown/dropdown.module';
 
-import { FiltersPopoverComponent } from "./modules/filters/filters-popover/filters-popover.component";
-import { FiltersContentComponent } from "./modules/filters/filters-content/filters-content.component";
-import { CheckboxComponent } from "./shared/components/checkbox/checkbox.component";
+import { FiltersPopoverComponent } from './modules/filters/filters-popover/filters-popover.component';
+import { FiltersContentComponent } from './modules/filters/filters-content/filters-content.component';
+import { CheckboxComponent } from './shared/components/checkbox/checkbox.component';
 
 @NgModule({
   imports: [
