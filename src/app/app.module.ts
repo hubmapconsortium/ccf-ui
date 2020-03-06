02bf194d8ffbb2bfeec068afe6e6ce9deb57672b
axdanbol <daniel_bolin@yahoo.com>
2020-03-06 13:49:07 -0500

    Added missing component modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FiltersPopoverModule } from './modules/filters/filters-popover/filters-popover.module';
 import { DrawerModule } from './shared/components/drawer/drawer.module';
-import { DropdownModule } from './shared/components/dropdown/dropdown.module';
-
-import { FiltersPopoverComponent } from './modules/filters/filters-popover/filters-popover.component';
-import { FiltersContentComponent } from './modules/filters/filters-content/filters-content.component';
-import { CheckboxComponent } from './shared/components/checkbox/checkbox.component';
 
 @NgModule({
   imports: [
@@ -21,17 +13,9 @@ import { CheckboxComponent } from './shared/components/checkbox/checkbox.compone
     BrowserAnimationsModule,
     CoreModule,
     DrawerModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    MatIconModule,
-    DropdownModule
+    FiltersPopoverModule
   ],
-  declarations: [
-    AppComponent,
-    FiltersPopoverComponent,
-    FiltersContentComponent,
-    CheckboxComponent
-  ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/shared/components/dropdown/dropdown.module.ts
+++ src/app/shared/components/dropdown/dropdown.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
 import { DropdownComponent } from './dropdown.component';
 
+
 @NgModule({
-  imports: [
-    CommonModule,
-    MatSelectModule,
-  ],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule],
   declarations: [DropdownComponent],
   exports: [DropdownComponent]
 })
