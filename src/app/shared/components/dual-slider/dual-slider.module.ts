ba00d3af53139fec301b19b42571b858fe27596f
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-03-12 11:40:37 -0400

    Adjust formatting

--- src/app/shared/components/dual-slider/dual-slider.module.ts
+++ src/app/shared/components/dual-slider/dual-slider.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { Ng5SliderModule } from 'ng5-slider';
