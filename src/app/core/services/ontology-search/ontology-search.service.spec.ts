99e37f16a201c6af8fcf128669816417321966e0
Gurjaspal <gurjaspal.bedi@gmail.com>
2020-03-18 15:37:32 -0400

    build fix.

--- src/app/core/services/ontology-search/ontology-search.service.spec.ts
+++ src/app/core/services/ontology-search/ontology-search.service.spec.ts
@@ -1,16 +1,2 @@
-import { TestBed } from '@angular/core/testing';
-
-import { OntologySearchService } from './ontology-search.service';
-
 describe('OntologySearchService', () => {
-  // let service: OntologySearchService;
-
-  // beforeEach(() => {
-  //   TestBed.configureTestingModule({});
-  //   service = TestBed.inject(OntologySearchService);
-  // });
-
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
 });
