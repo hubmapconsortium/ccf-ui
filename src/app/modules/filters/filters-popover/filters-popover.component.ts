28dac6fba11b7b7977dbd88fcb17ea30a8151a79
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-03-18 12:33:49 -0400

    implementing Daniel's refactoring suggestion

--- src/app/modules/filters/filters-popover/filters-popover.component.ts
+++ src/app/modules/filters/filters-popover/filters-popover.component.ts
@@ -14,32 +14,11 @@ export class FiltersPopoverComponent {
 
   toggleFilterVisible(): void {
     this.filtersBoxVisible = !this.filtersBoxVisible;
-
-    if (this.filtersBoxVisible) {
-      this.displayFilters();
-    } else {
-      this.hideFilters();
-    }
-  }
-
-  displayFilters(): void {
-    this.popupContainerWidth = '35em';
-    this.popupBodyOpacity = '100%';
-    this.popupBodyTransitionDelay = '.3s';
-    this.popupBodyTransitionDuration = '.3s';
-  }
-
-  hideFilters(): void {
-    this.popupContainerWidth = '0em';
-    this.popupBodyOpacity = '0%';
-    this.popupBodyTransitionDelay = '0s';
-    this.popupBodyTransitionDuration = '0s';
   }
 
   applyFilters(filters: Record<string, unknown>) {
     // To be hooked up later to the real filter call.
     console.log('Filter box. Filters: ', filters);
     this.filtersBoxVisible = false;
-    this.hideFilters();
   }
 }
