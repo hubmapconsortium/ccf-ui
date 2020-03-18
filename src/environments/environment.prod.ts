e9f0c4a22340be1d506e67225fcf611bd6dbe42c
Gurjaspal <gurjaspal.bedi@gmail.com>
2020-03-18 10:52:48 -0400

    fixed issue where prod build was failing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  ontologyUrl: 'https://hubmapconsortium.github.io/hubmap-ontology/ccf-partonomy.jsonld',
+  organNodes: [
+    'http://purl.obolibrary.org/obo/UBERON_0000948', // Heart
+    'http://purl.obolibrary.org/obo/UBERON_0002113', // Kidney
+  ]
 };
