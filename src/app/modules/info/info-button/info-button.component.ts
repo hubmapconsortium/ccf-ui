1323288019b36adbf9de96ce1c3deca1ce3368f5
Gurjaspal <gurjaspal.bedi@gmail.com>
2020-03-20 11:00:32 -0400

    Fix where clicking on info button was taking user to download page.

--- src/app/modules/info/info-button/info-button.component.ts
+++ src/app/modules/info/info-button/info-button.component.ts
@@ -3,6 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { InfoDialogComponent } from '../info-dialog/info-dialog.component';
 
 
+/**
+ * Info button component: Information icon displays project details when clicked.
+ */
 @Component({
   selector: 'ccf-info-button',
   templateUrl: './info-button.component.html',
@@ -12,11 +15,14 @@ export class InfoButtonComponent {
 
   constructor(private dialog: MatDialog) {}
 
+
+  /**
+   * Opens the info dialogue with the project details
+   */
   launchInfoDialog(): void {
     this.dialog.open(InfoDialogComponent, {
       width: '60em',
       data: {}
     });
   }
-
 }
