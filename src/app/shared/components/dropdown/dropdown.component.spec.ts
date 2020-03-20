a698303108c70867cd3c31a8c6bf9fe2ad3ac3a8
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-03-20 13:44:03 -0400

    Tests for dropdown component

--- src/app/shared/components/dropdown/dropdown.component.spec.ts
+++ src/app/shared/components/dropdown/dropdown.component.spec.ts
@@ -1,25 +1,51 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSelect, MatSelectChange } from '@angular/material/select';
+import { Shallow } from 'shallow-render';
 import { DropdownComponent } from './dropdown.component';
+import { DropdownModule } from './dropdown.module';
+
 
 describe('DropdownComponent', () => {
-  // let component: DropdownComponent;
-  // let fixture: ComponentFixture<DropdownComponent>;
+  let shallow: Shallow<DropdownComponent>;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ DropdownComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
+  beforeEach(() => {
+    shallow = new Shallow(DropdownComponent, DropdownModule);
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(DropdownComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  it('should provide the current value to mat-select', async () => {
+    const { findComponent } = await shallow.render({ bind: { selection: 'one' } });
+    const matSelect = findComponent(MatSelect);
+    expect(matSelect.value).toBe('one');
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should open options if clicked', async () => {
+    const { findComponent, instance } = await shallow.render();
+    const matSelect = findComponent(MatSelect);
+    const spy = spyOn(instance, 'toggleOptions');
+    matSelect.openedChange.emit();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should change options if clicked', async () => {
+    const { findComponent, instance } = await shallow.render();
+    const matSelect = findComponent(MatSelect);
+    const spy = spyOn(instance, 'selectionChanged');
+    matSelect.selectionChange.emit({ value: 'two' } as MatSelectChange);
+    expect(spy).toHaveBeenCalledWith('two');
+  });
+
+  it('changes invisible to visible when toggled', async () => {
+    const { instance } = await shallow.render();
+    instance.optionsVisible = 'invisible';
+    instance.toggleOptions();
+    expect(instance.optionsVisible).toBe('visible');
+    instance.toggleOptions();
+    expect(instance.optionsVisible).toBe('invisible');
+  });
+
+  it('updates the current selection when selected', async () => {
+    const { instance, outputs } = await shallow.render();
+    instance.selectionChanged('two');
+    expect(instance.selection).toBe('two');
+    expect(outputs.selectionChange.emit).toHaveBeenCalledWith('two');
+  });
 });
