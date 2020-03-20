291516c8a1fd5b662c9453ae485e9b163f93ede2
axdanbol <daniel_bolin@yahoo.com>
2020-03-20 15:55:19 -0400

    Added a test + fixed a couple small linting errors

--- src/app/shared/components/checkbox/checkbox.component.spec.ts
+++ src/app/shared/components/checkbox/checkbox.component.spec.ts
@@ -4,6 +4,7 @@ import { Shallow } from 'shallow-render';
 import { CheckboxComponent } from './checkbox.component';
 import { CheckboxModule } from './checkbox.module';
 
+
 describe('FiltersCheckboxComponent', () => {
   let shallow: Shallow<CheckboxComponent>;
 
@@ -12,13 +13,14 @@ describe('FiltersCheckboxComponent', () => {
   });
 
   it('should set the label to value passed in', async () => {
-    const { find } = await shallow.render({ bind: { label: 'TestLabel', options: ['Test1'] } });
+    const { find } = await shallow.render({ bind: { label: 'TestLabel' } });
+    const label = find('.filter-label').nativeElement as HTMLElement;
 
-    expect(find('.filter-label').nativeElement.textContent).toBe('TestLabel');
+    expect(label.textContent).toBe('TestLabel');
   });
 
   it('should trigger filterOnChange when an option is changed', async () => {
-    const { findComponent, instance } = await shallow.render({ bind: { label: 'Test', options: ['Test1'] } });
+    const { findComponent, instance } = await shallow.render({ bind: { options: ['Test1'] } });
     const spy = spyOn(instance, 'filterOnChange');
     const checkbox = findComponent(MatCheckbox);
 
@@ -27,17 +29,23 @@ describe('FiltersCheckboxComponent', () => {
   });
 
   it('should add an item to the selection array when checked', async () => {
-    const { instance } = await shallow.render({ bind: { label: 'Test', options: ['Test1', 'Test2'] } });
+    const { instance } = await shallow.render();
 
-    instance.filterOnChange({ 'checked': true } as MatCheckboxChange, 'Test1')
+    instance.filterOnChange({ checked: true } as MatCheckboxChange, 'Test1');
     expect(instance.selection).toContain('Test1');
   });
 
   it('should remove an item from the selection array when unchecked', async () => {
-    const { instance } = await shallow.render({ bind: { label: 'Test', options: ['Test1', 'Test2'] } });
-    instance.selection = ['Test1'];
+    const { instance } = await shallow.render({ bind: { selection: ['Test1'] } });
 
-    instance.filterOnChange({ 'checked': false } as MatCheckboxChange, 'Test1');
+    instance.filterOnChange({ checked: false } as MatCheckboxChange, 'Test1');
     expect(instance.selection).not.toContain('Test1');
+  });
+
+  it('should emit the new selection when changed', async () => {
+    const { instance, outputs } = await shallow.render();
+
+    instance.filterOnChange({ checked: true } as MatCheckboxChange, 'Test1');
+    expect(outputs.selectionChange.emit).toHaveBeenCalledWith(['Test1']);
   });
 });
