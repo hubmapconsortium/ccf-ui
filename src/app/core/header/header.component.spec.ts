a18fa633b94f46088a0656e59c8e4f21312b5d78
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-03-23 15:29:17 -0400

    Fix sonar errors

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -5,19 +5,20 @@ import { HeaderModule } from './header.module';
 
 describe('HeaderComponent', () => {
   let shallow: Shallow<HeaderComponent>;
+  const filterClass = '.filter-labels';
 
   beforeEach(() => {
     shallow = new Shallow(HeaderComponent, HeaderModule);
   });
 
   it('should emit downloadClicked when download button is clicked', async () => {
-    const { find, instance, outputs } = await shallow.render();
+    const { find, outputs } = await shallow.render();
     find('.download').triggerEventHandler('click', {});
     expect(outputs.downloadClicked.emit).toHaveBeenCalled();
   });
 
   it('should emit logoClicked when logo is clicked', async () => {
-    const { find, instance, outputs } = await shallow.render();
+    const { find, outputs } = await shallow.render();
     find('.logo').triggerEventHandler('click', {});
     expect(outputs.logoClicked.emit).toHaveBeenCalled();
   });
@@ -25,21 +26,21 @@ describe('HeaderComponent', () => {
   it('should display the current sex', async () => {
     const { find } = await shallow
       .render({bind: {filters: { sex: 'Both', ageRange: [1, 110], BMIRange: [13, 83] }}});
-    const label = find('.filter-labels').nativeElement as HTMLElement;
+    const label = find(filterClass).nativeElement as HTMLElement;
     expect(label.textContent).toContain('Sex: Both');
   });
 
   it('should display the current age range', async () => {
     const { find } = await shallow
       .render({bind: {filters: { sex: 'Both', ageRange: [1, 110], BMIRange: [13, 83] }}});
-    const label = find('.filter-labels').nativeElement as HTMLElement;
+    const label = find(filterClass).nativeElement as HTMLElement;
     expect(label.textContent).toContain('Age: 1-110');
   });
 
   it('should display the current BMI range', async () => {
     const { find } = await shallow
       .render({bind: {filters: { sex: 'Both', ageRange: [1, 110], BMIRange: [13, 83] }}});
-    const label = find('.filter-labels').nativeElement as HTMLElement;
+    const label = find(filterClass).nativeElement as HTMLElement;
     expect(label.textContent).toContain('BMI: 13-83');
   });
 });
