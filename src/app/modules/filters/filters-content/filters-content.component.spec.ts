deb3727b11c2fe3b798e4cfaacd14531c3cb1a33
adphillips2017 <adjp2013@gmail.com>
2020-03-23 11:39:24 -0400

    tests + documentation for filters-content

--- src/app/modules/filters/filters-content/filters-content.component.spec.ts
+++ src/app/modules/filters/filters-content/filters-content.component.spec.ts
@@ -1,25 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Shallow } from 'shallow-render';
 
 import { FiltersContentComponent } from './filters-content.component';
+import { FiltersContentModule } from './filters-content.module';
 
 describe('FiltersContentComponent', () => {
-  // let component: FiltersContentComponent;
-  // let fixture: ComponentFixture<FiltersContentComponent>;
+  let shallow: Shallow<FiltersContentComponent>;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ FiltersContentComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
+  beforeEach(() => {
+    shallow = new Shallow(FiltersContentComponent, FiltersContentModule);
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(FiltersContentComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  it('should update the filter object with the passed in filter and value', async () => {
+    const { instance } = await shallow.render({ bind: {} });
+    instance.filters = {};
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+    instance.updateFilter('value', 'test');
+    expect(instance.filters['test']).toBe('value');
+  });
+
+  it('should emit the filter change when updateFilter is called', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.updateFilter('value', 'test');
+
+    expect(outputs.filtersChange).toHaveBeenCalled();
+  });
+
+  it('should call applyButtonClick when the apply button is clicked', async () => {
+    const { find, instance } = await shallow.render({ bind: {} });
+    const spy = spyOn(instance, 'applyButtonClick');
+    const button = find('.outline-button');
+
+    button.triggerEventHandler('click', {});
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should emit the filters when applyButtonClick is called', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.applyButtonClick();
+
+    expect(outputs.applyFilters.emit).toHaveBeenCalled();
+  });
 });
