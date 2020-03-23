dcacf57daa148de37ab53813fbba630df87ee975
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-03-23 14:06:06 -0400

    Class documentation

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,6 +1,10 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 
 
+/**
+ * Header which is always displayed on the site; contains current filter info,
+ * a link to download data, and a logo which resets the page when clicked.
+ */
 @Component({
   selector: 'ccf-header',
   templateUrl: './header.component.html',
