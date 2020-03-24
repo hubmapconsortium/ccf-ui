169efce1b45c08ccfd181c6422781eed200ef120
adphillips2017 <adjp2013@gmail.com>
2020-03-24 11:08:49 -0400

    documentation for the checkbox component

--- src/app/shared/components/checkbox/checkbox.component.ts
+++ src/app/shared/components/checkbox/checkbox.component.ts
@@ -1,21 +1,44 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 
+/**
+ * Creates a labeled group of checkboxes and emits a current list of selections whenever a selection changes.
+ */
 @Component({
   selector: 'ccf-checkbox',
   templateUrl: './checkbox.component.html',
   styleUrls: ['./checkbox.component.scss']
 })
 export class CheckboxComponent {
+  /**
+   * The label that describes the overall question the checkbox is asking
+   */
   @Input() label: string;
+
+  /**
+   * Used to generate the individual checkboxes and their individual labels
+   */
   @Input() options: string[];
+
+  /**
+   * A list of the checkboxes the user has checked. To be updated any time a checkbox changes.
+   */
   @Input() selection: string[] = [];
+
+  /**
+   * Any time a checkbox changes we emit that value so the parent component has that information
+   */
   @Output() selectionChange = new EventEmitter<string[]>();
 
+  /**
+   * This method captures checkbox events and decides whether to add or remove a filter selection based on the checked property
+   * @param event Event object from the checkbox that contains the boolean property 'checked'
+   * @param option Tells us which option was checked or unchecked
+   */
   filterOnChange(event: MatCheckboxChange, option: string): void {
     const checked = event.checked;
 
-    if(checked) {
+    if (checked) {
       this.selection.push(option);
     } else {
       this.selection = this.selection.filter(selection => selection !== option);
