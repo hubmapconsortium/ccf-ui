c8a06f543ed4b7feeea5e70a3cb055092ab917d4
Gurjaspal <gurjaspal.bedi@gmail.com>
2020-03-26 12:48:14 -0400

    ontology selection tests.

--- src/app/modules/ontology-exploration/ontology-selection/ontology-selection.component.spec.ts
+++ src/app/modules/ontology-exploration/ontology-selection/ontology-selection.component.spec.ts
@@ -1,25 +1,41 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Store } from '@ngxs/store';
+import { Shallow } from 'shallow-render';
+import { OntologyNode } from 'src/app/core/models/ontology-node';
+import { OntologyState } from 'src/app/core/store/ontology/ontology.state';
+import { SearchState } from 'src/app/core/store/search/search.state';
 
+import { OntologyTreeComponent } from '../ontology-tree/ontology-tree.component';
 import { OntologySelectionComponent } from './ontology-selection.component';
+import { OntologySelectionModule } from './ontology-selection.module';
 
 describe('OntologySelectionComponent', () => {
-  // let component: OntologySelectionComponent;
-  // let fixture: ComponentFixture<OntologySelectionComponent>;
+  let shallow: Shallow<OntologySelectionComponent>;
+  let mockStore: jasmine.SpyObj<Store>;
+  let mockTreeComponent: jasmine.SpyObj<OntologyTreeComponent>;
+  const ontologyNode = {
+    label: 'label'
+  } as OntologyNode;
 
-  // beforeEach(async(() => {
-  //   TestBed.configureTestingModule({
-  //     declarations: [ OntologySelectionComponent ]
-  //   })
-  //   .compileComponents();
-  // }));
+  beforeEach(() => {
+    mockStore = jasmine.createSpyObj<Store>(['selectSnapshot']);
+    mockStore.selectSnapshot.and.returnValue({node: ontologyNode });
+    mockTreeComponent = jasmine.createSpyObj<OntologyTreeComponent>(['expandAndSelect']);
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(OntologySelectionComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+    shallow = new Shallow(OntologySelectionComponent, OntologySelectionModule)
+    .mock(Store, mockStore)
+    .mock(OntologyTreeComponent, mockTreeComponent)
+    .provide([
+      { provide: SearchState, useValue: {} },
+      { provide: HttpClient, useValue: {} },
+      { provide: OntologyState, useValue: {} }
+    ]);
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should expand the selected node', async () => {
+    const { instance } = await shallow.render();
+    instance.tree = mockTreeComponent;
+    instance.selected(ontologyNode);
+    expect(mockTreeComponent.expandAndSelect).toHaveBeenCalled();
+  });
 });
