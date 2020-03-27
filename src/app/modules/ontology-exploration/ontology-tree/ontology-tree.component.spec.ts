23fffcd5e3b29190b05304603036831c1c56dce6
axdanbol <daniel_bolin@yahoo.com>
2020-03-27 14:37:03 -0400

    Added a couple of tests

--- src/app/modules/ontology-exploration/ontology-tree/ontology-tree.component.spec.ts
+++ src/app/modules/ontology-exploration/ontology-tree/ontology-tree.component.spec.ts
@@ -12,9 +12,10 @@ function fromPartial<T>(partial: RecursivePartial<T>): T {
 
 
 describe('OntologyTreeComponent', () => {
-  let shallow: Shallow<OntologyTreeComponent>;
   const node1 = new FlatNode(fromPartial<OntologyNode>({ label: 'label', children:['child1', 'child2'] }), 1);
   const node2 = new FlatNode(fromPartial<OntologyNode>({ label: 'label2', children:[]  }), 1);
+
+  let shallow: Shallow<OntologyTreeComponent>;
 
   beforeEach(() => {
     shallow = new Shallow(OntologyTreeComponent, OntologyTreeModule);
@@ -40,5 +41,22 @@ describe('OntologyTreeComponent', () => {
   it('should check if the node can be expanded 2', async () => {
     const { instance } = await shallow.render();
     expect(instance.isInnerNode(1, node2)).toBeFalse();
+  });
+
+  it('should set the current selection when a different node is selected', async () => {
+    const { instance, outputs } = await shallow.render();
+
+    instance.select(node1);
+    expect(instance.selectedNode).toBe(node1);
+    expect(outputs.nodeSelected.emit).toHaveBeenCalledWith(node1.original);
+  });
+
+  it('should clear the current selection when the same node is selected', async () => {
+    const { instance, outputs } = await shallow.render();
+
+    instance.selectedNode = node1;
+    instance.select(node1);
+    expect(instance.selectedNode).toBeUndefined();
+    expect(outputs.nodeSelected.emit).toHaveBeenCalledWith(undefined);
   });
 });
