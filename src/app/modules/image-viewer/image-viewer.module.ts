2667eb8a35d5b60dcdc5f1d3fe7e7884243993b4
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-04-21 11:31:33 -0400

    Rename component to Image Viewer

---
+++ src/app/modules/image-viewer/image-viewer.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ImageViewerComponent } from './image-viewer.component';
+
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ImageViewerComponent],
+  exports: [ImageViewerComponent]
+})
+export class ImageViewerModule { }
