f1322e4cc3e1c00fe861627f33f5f155c73d2eb4
Edward Lu <eddd.lu@gmail.com>
2020-04-28 13:37:43 -0400

    add documentation

--- src/app/modules/image-viewer/image-viewer-content/image-viewer-content.component.ts
+++ src/app/modules/image-viewer/image-viewer-content/image-viewer-content.component.ts
@@ -1,5 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 
+/**
+ * Content for the image viewer sidebar
+ */
 @Component({
   selector: 'ccf-image-viewer-content',
   templateUrl: './image-viewer-content.component.html',
@@ -7,8 +10,14 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class ImageViewerContentComponent {
 
+  /**
+   * Emits closeViewer on close button click
+   */
   @Output() closeViewer = new EventEmitter();
 
+  /**
+   * Hides the viewer
+   */
   close() {
     this.closeViewer.emit();
   }
--- src/app/modules/image-viewer/image-viewer-popover/image-viewer-popover.component.ts
+++ src/app/modules/image-viewer/image-viewer-popover/image-viewer-popover.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * Popup that displays detailed information on a selected image along with viewing options
+ */
 @Component({
   selector: 'ccf-image-viewer-popover',
   templateUrl: './image-viewer-popover.component.html',
@@ -7,12 +10,21 @@ import { Component } from '@angular/core';
 })
 export class ImageViewerPopoverComponent {
 
+  /**
+   * Whether or not the image viewer is visible
+   */
   viewerVisible = false;
 
+  /**
+   * Controls visibility of the viewer
+   */
   toggleViewerVisible(): void {
     this.viewerVisible = !this.viewerVisible;
   }
 
+  /**
+   * Returns viewer to closed state
+   */
   closeViewer(): void {
     this.viewerVisible = false;
   }
