a35b4cea392717f3ebf2b09fcbcd25f302c11a4c
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-04-29 11:51:53 -0400

    Remove unused imports

--- src/app/modules/image-viewer/image-viewer-content/image-viewer-content.component.spec.ts
+++ src/app/modules/image-viewer/image-viewer-content/image-viewer-content.component.spec.ts
@@ -2,7 +2,6 @@ import { Shallow } from 'shallow-render';
 
 import { ImageViewerContentComponent } from './image-viewer-content.component';
 import { ImageViewerContentModule } from './image-viewer-content.module';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ImageViewerPopoverComponent', () => {
   let shallow: Shallow<ImageViewerContentComponent>;
