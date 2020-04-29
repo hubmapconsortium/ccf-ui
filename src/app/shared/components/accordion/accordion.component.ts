544f1b95e6fd53b20b3e1074dcac31a3b8729eb1
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-04-29 18:07:24 -0400

    Add accordion options

---
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ccf-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.scss']
+})
+export class AccordionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
