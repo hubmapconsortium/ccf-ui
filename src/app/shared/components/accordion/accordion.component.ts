74d599db290cfda5e02026595f1e76807945476f
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-04-30 17:42:34 -0400

    Add about display

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ccf-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss']
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent {
 
-  constructor() { }
+  @Input() metadata: Record<string, unknown> =
+    {
+      'Patient Number': 64354,
+      'Procedure ID': 66598,
+      Date: '1/30/2019',
+      Age: 38,
+      Gender: 'Female',
+      Race: 'White',
+      Height: '165.1 cm',
+      Weight: '115.2 kg',
+      BMI: 42.3,
+      Comorbidities: 'Obesity',
+      'Type of Procedure': 'Total Nephrectomy',
+      'Indications for Procedure': 'Renal tumor',
+      Laterality: 'Lest Tissue Type: kidney',
+      'Dimensions (mm)': 'L: 19 x W: 13 x H: 7',
+      'Anatomical Landmark': 'Lower Pole',
+      'Distance from Tumor': '7 cm Sample',
+      Processing: 'Frozen',
+      'Method of Freezing': 'Dry ice/Isopentane Slurry',
+      'Embedding Media': 'CMC'
+    };
 
-  ngOnInit(): void {
-  }
+  labels = Object.keys(this.metadata);
+  values = Object.values(this.metadata);
 
+
+
+  // labels = Object.keys(this.data);
+  // values = Object.values(this.data);
 }
