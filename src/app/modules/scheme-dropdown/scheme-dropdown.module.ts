ab86c5e02e801e7ed9e60927ba61c49171021984
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-05-14 14:36:19 -0400

    Add dropdown menu

--- src/app/modules/scheme-dropdown/scheme-dropdown.module.ts
+++ src/app/modules/scheme-dropdown/scheme-dropdown.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SchemeDropdownComponent } from './scheme-dropdown.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 
+import { SchemeDropdownComponent } from './scheme-dropdown.component';
 
 @NgModule({
   declarations: [SchemeDropdownComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule
   ],
   exports: [SchemeDropdownComponent]
 })
