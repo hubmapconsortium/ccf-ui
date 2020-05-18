e4a7ee3392d80a07a1945f57ee0deddd73f657c1
axdanbol <daniel_bolin@yahoo.com>
2020-05-18 12:37:07 -0400

    Removed angular generated classes

--- projects/ccf-image-viewer/src/public-api.ts
+++ projects/ccf-image-viewer/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of ccf-image-viewer
  */
-
-export * from './lib/ccf-image-viewer.service';
-export * from './lib/ccf-image-viewer.component';
-export * from './lib/ccf-image-viewer.module';
