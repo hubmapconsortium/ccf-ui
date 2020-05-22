0c553e22ecf2b2b34f4c85260c10e8122b05712a
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-05-22 10:24:42 -0400

    Remove code smell

--- src/app/modules/color-scheme/color-scheme-contents/color-scheme-contents.component.spec.ts
+++ src/app/modules/color-scheme/color-scheme-contents/color-scheme-contents.component.spec.ts
@@ -1,6 +1,6 @@
 import { Shallow } from 'shallow-render';
 
-import { ColorScheme, DEFAULT_COLOR_SCHEMES } from '../color-schemes';
+import { DEFAULT_COLOR_SCHEMES } from '../color-schemes';
 import { ColorSchemeContentsComponent } from './color-scheme-contents.component';
 import { ColorSchemeContentsModule } from './color-scheme-contents.module';
 
