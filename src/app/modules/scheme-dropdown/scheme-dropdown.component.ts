237fe40cc54433ff4ef891061f919d17710089bb
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-05-26 14:32:09 -0400

    Save changes

--- src/app/modules/scheme-dropdown/scheme-dropdown.component.ts
+++ src/app/modules/scheme-dropdown/scheme-dropdown.component.ts
@@ -10,15 +10,23 @@ export class SchemeDropdownComponent implements OnInit {
 
   @Input() schemeOptions: ColorScheme[] = DEFAULT_COLOR_SCHEMES;
   @Input() colorScheme: ColorScheme = DEFAULT_COLOR_SCHEMES[0];
+  closed: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.colorScheme);
     this.schemeOptions = this.schemeOptions.filter(scheme => scheme.type === 'discrete');
   }
 
-  schemeChanged(n: number) {
-    this.colorScheme = this.schemeOptions[n];
+  schemeChanged(scheme: ColorScheme) {
+    if (this.colorScheme === scheme) {
+      this.closed = true;
+    } else {
+      this.colorScheme = scheme;
+      this.closed = false;
+
+    }
   }
 
 }
