28f503cf5bf120264041eaba5a94ac323e98ffcf
axdanbol <daniel_bolin@yahoo.com>
2020-05-29 13:42:21 -0400

    Added non-angular image-viewer

---
+++ projects/ccf-image-viewer/src/lib/image-viewer.ts
@@ -0,0 +1,137 @@
+import { Deck } from '@deck.gl/core';
+import { createOMETiffLoader, DetailView, OverviewView } from '@hubmap/vitessce-image-viewer';
+
+export interface LayerSpec {
+  url: string;
+  offsets?: number[];
+}
+
+export interface ImageViewerProps {
+  id: string;
+  canvas: string | HTMLCanvasElement;
+  width: number;
+  height: number;
+  layers: LayerSpec[];
+}
+
+export class ImageViewer {
+  private loaders: Promise<unknown[]>;
+  private views: any[] = [];
+  private states: {[id: string]: unknown} = {};
+  private deck?: Deck;
+
+  constructor(private props: ImageViewerProps) {
+    this.loaders = this.createLoaders(props.layers);
+    this.loaders.then(loaders => {
+      this.views = this.createViews(loaders);
+      this.states = this.createState(view => ({ viewState: view.initialViewState }));
+      this.deck = this.createDeck(loaders);
+    });
+
+    console.log(this);
+  }
+
+  async getMetadata(): Promise<unknown> {
+    const loaders = await this.loaders;
+    const channels = loaders.reduce((names: string[], loader) => {
+      return names.concat((loader as any).channelNames);
+    }, []);
+
+    return {
+      channels
+    };
+  }
+
+  destroy(): void {
+    this.deck?.finalize();
+    this.loaders = Promise.resolve([]);
+    this.views = [];
+    this.states = {};
+    this.deck = undefined;
+  }
+
+  private createLoaders(layers: LayerSpec[]): Promise<unknown[]> {
+    const loaders = layers.map(createOMETiffLoader);
+    return Promise.all(loaders);
+  }
+
+  private createViews(loaders: unknown[]): unknown[] {
+    const initialViewState = {
+      target: [25000, 10000, 0],
+      zoom: -6,
+      width: this.props.width,
+      height: this.props.height
+    };
+
+    const detailView = new DetailView({
+      initialViewState: {
+        ...initialViewState,
+        id: 'detail'
+      }
+    });
+
+    const overviewView = new OverviewView({
+      initialViewState: {
+        ...initialViewState,
+        id: 'overview'
+      },
+      loader: loaders[0], // FIXME
+      detailWidth: 200,
+      detailHeight: 200,
+      margin: 25,
+      scale: 0.15,
+      position: 'bottom-left'
+    });
+
+    return [detailView, overviewView];
+  }
+
+  private createState(argGen: (view: any) => any): {[id: string]: unknown} {
+    return this.views.reduce((state, view) => {
+      state[view.id] = view.filterViewState(argGen(view));
+      return state;
+    }, {});
+  }
+
+  private createLayers(layerProps: any[]): unknown[] {
+    return this.views.map((view, i) => view.getLayers({
+      viewStates: this.states,
+      props: {
+        ...layerProps[i]
+      }
+    }));
+  }
+
+  private createDeck(loaders: unknown[]): Deck {
+    const loader: any = loaders[0]; // FIX
+    const channelCount = loader.channelNames.length;
+    const layerConfig = {
+      loader,
+      sliderValues: [],
+      colorValues: [],
+      channelIsOn: Array(channelCount).fill(true),
+      loaderSelection: loader.channelNames.map(name => ({ channel: name })),
+      colormap: undefined
+    };
+
+    return new Deck({
+      id: this.props.id,
+      canvas: this.props.canvas,
+      layers: this.createLayers(Array(this.views.length).fill(layerConfig)),
+      views: this.views.map(view => view.getDeckGlView()),
+      viewState: this.states,
+      onViewStateChange: this.onViewStateChange.bind(this),
+      glOptions: {
+        webgl2: true
+      }
+    } as any);
+  }
+
+  private onViewStateChange({ viewId, viewState, oldViewState }): void {
+    this.states = this.createState(view => ({
+      viewState: { ...viewState, id: viewId },
+      oldViewState,
+      currentViewState: this.states[view.id]
+    }));
+  }
+}
--- projects/ccf-image-viewer/src/public-api.ts
+++ projects/ccf-image-viewer/src/public-api.ts
@@ -1,3 +1,9 @@
+// Hack to support deck.gl and other typings
+// tslint:disable-next-line: no-reference
+/// <reference types="node" />
+
 /*
  * Public API Surface of ccf-image-viewer
  */
+
+export { ImageViewer, ImageViewerProps, LayerSpec } from './lib/image-viewer';
