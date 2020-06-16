51609310314533ba776529bfb536b8ca0c75049a
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-06-16 14:21:33 -0400

    Add borders to light color icons

--- src/app/core/models/image-viewer-layer.ts
+++ src/app/core/models/image-viewer-layer.ts
@@ -39,4 +39,37 @@ export class ImageViewerLayer {
 
     return gradient;
   }
+
+  isLight(): boolean {
+    const color = this.color;
+    let r = '0';
+    let g = '0';
+    let b = '0';
+
+    // 3 digits
+    if (color.length === 4) {
+      r = '0x' + color[1] + color[1];
+      g = '0x' + color[2] + color[2];
+      b = '0x' + color[3] + color[3];
+
+    // 6 digits
+    } else if (color.length === 7) {
+      r = '0x' + color[1] + color[2];
+      g = '0x' + color[3] + color[4];
+      b = '0x' + color[5] + color[6];
+    }
+
+    const hsp = Math.sqrt(
+      0.299 * (r * r) +
+      0.587 * (g * g) +
+      0.114 * (b * b)
+    );
+
+    if (hsp>225) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
