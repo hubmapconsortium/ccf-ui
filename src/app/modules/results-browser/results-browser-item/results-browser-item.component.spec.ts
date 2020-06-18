71d393b0a2b2ff7ab2a09cf02fa016cfe476a753
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-06-18 14:57:25 -0400

    adding tests

--- src/app/modules/results-browser/results-browser-item/results-browser-item.component.spec.ts
+++ src/app/modules/results-browser/results-browser-item/results-browser-item.component.spec.ts
@@ -13,30 +13,69 @@ describe('ResultsBrowserItemComponent', () => {
   });
 
   it('should show a placeholder div when no thumbnail image url is present', async () => {
-    const testListResult: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test'};
-    const { find } = await shallow.render({ bind: { data: testListResult }});
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test'};
+    const { find } = await shallow.render({ bind: { data }});
 
     expect(find('.result-image-placeholder')).toHaveFoundOne();
   });
 
   it('should not show a placeholder div when a thumbnail image url is present', async () => {
-    const testListResult: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', thumbnailUrl: 'test.com'};
-    const { find } = await shallow.render({ bind: { data: testListResult }});
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', thumbnailUrl: 'test.com'};
+    const { find } = await shallow.render({ bind: { data }});
 
     expect(find('.result-image-placeholder')).not.toHaveFoundOne();
   });
 
   it('should show a grayed out icon when no download url is present', async () => {
-    const testListResult: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test' };
-    const { find } = await shallow.render({ bind: { data: testListResult }});
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test' };
+    const { find } = await shallow.render({ bind: { data }});
 
     expect(find('.no-download')).toHaveFoundOne();
   });
 
   it('should show a non-grayed out icon when download url is present', async () => {
-    const testListResult: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
-    const { find } = await shallow.render({ bind: { data: testListResult }});
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
+    const { find } = await shallow.render({ bind: { data }});
 
     expect(find('.download')).toHaveFoundOne();
+  });
+
+  it('should trigger the openImageViewer emit when a result with resultType image_viewer is clicked', async () => {
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
+    const { instance, outputs } = await shallow.render({ bind: { data }});
+    data.resultType = 'image_viewer';
+
+    instance.openResult();
+    expect(outputs.openImageViewer.emit).toHaveBeenCalled();
+  });
+
+  it('should trigger the openImageViewer emit when a result with no resultType is clicked', async () => {
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
+    const { instance, outputs } = await shallow.render({ bind: { data }});
+
+    instance.openResult();
+    expect(outputs.openImageViewer.emit).toHaveBeenCalled();
+  });
+
+  it('should call window.open for a new tab when a result with external_link resultType is clicked', async () => {
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
+    const { instance, outputs } = await shallow.render({ bind: { data }});
+    data.resultUrl = 'testresult.com';
+    data.resultType = 'external_link';
+    const spy = spyOn(window, 'open');
+
+    instance.openResult();
+    expect(spy).toHaveBeenCalledWith(data.resultUrl, '_blank');
+  });
+
+  it('should call window.open in the same tab when a result with local_link resultType is clicked', async () => {
+    const data: ListResult = { '@id': '123', '@type': 'ListResult', id: '123', label: 'test', downloadUrl: 'test.com' };
+    const { instance, outputs } = await shallow.render({ bind: { data }});
+    data.resultUrl = 'testresult.com';
+    data.resultType = 'local_link';
+    const spy = spyOn(window, 'open');
+
+    instance.openResult();
+    expect(spy).toHaveBeenCalledWith(data.resultUrl, '_self');
   });
 });
--- src/app/modules/results-browser/results-browser/results-browser.component.spec.ts
+++ src/app/modules/results-browser/results-browser/results-browser.component.spec.ts
@@ -28,17 +28,6 @@ describe('ResultsBrowserComponent', () => {
       .dontMock(ScrollingModule);
   });
 
-  it('should emit the result when one is clicked on', async () => {
-    const testData: ListResult[] = [{ '@id': '123', '@type': 'ListResult', id: '123', label: 'test' }];
-    const testAggregate: AggregateResult[] = [{ label: 'test', count: 2 }];
-    const testBind = { data: testData, aggregateData: testAggregate, resultLabel: 'test' };
-    const { find, outputs } = await shallow.render({ bind: testBind });
-    const result = find('.result-browser-item');
-
-    result.triggerEventHandler('click', {});
-    expect(outputs.resultClicked.emit).toHaveBeenCalled();
-  });
-
   it('should re-run the gradient display logic when the dataLoading Input variable changes to false', async () => {
     const { instance } = await shallow.render();
     const spy = spyOn(instance.virtualScroll, 'measureScrollOffset');
