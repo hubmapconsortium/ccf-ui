9f51ba6a68fafb3dcb274bbaa3e95d7500da4288
axdanbol <daniel_bolin@yahoo.com>
2020-08-04 11:35:56 -0400

    Create rui app project

---
+++ projects/ccf-rui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ccf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ccf-rui';
+}
