8022c0655850c4e61c6f5d21db90ca57e56dc125
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-04 10:05:19 -0400

    Add component functions

--- src/app/core/block-size-input/block-size-input.component.ts
+++ src/app/core/block-size-input/block-size-input.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'ccf-block-size-input',
   templateUrl: './block-size-input.component.html',
   styleUrls: ['./block-size-input.component.scss']
 })
-export class BlockSizeInputComponent implements OnInit {
+export class BlockSizeInputComponent {
+
+  values: Record<string, unknown> = {width: 10, height: 10, depth: 10};
+  @Output() valuesChange = new EventEmitter<Record<string, unknown>>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  updateBlockSize(value: unknown, key: string): void {
+    this.values = { ...this.values, [key]: value };
+    console.log(this.values)
+    this.valuesChange.emit(this.values);
   }
-
 }
