b6ef31ef99a88bf35553780ed2329d5455f116e0
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-04 15:23:48 -0400

    Inputs and styling

--- src/app/core/block-size-input/block-size-input.component.ts
+++ src/app/core/block-size-input/block-size-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'ccf-block-size-input',
@@ -7,13 +7,21 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class BlockSizeInputComponent {
 
-  values: Record<string, unknown> = {width: 10, height: 10, depth: 10};
+  values: Record<string, unknown> = {width: '10', height: '10', depth: '10'};
   @Output() valuesChange = new EventEmitter<Record<string, unknown>>();
 
   constructor() { }
 
-  updateBlockSize(value: unknown, key: string): void {
-    this.values = { ...this.values, [key]: value };
+  updateBlockSize(input: InputEvent, key: string): void {
+    const inputTarget = input.target as HTMLInputElement;
+    const inputValue = inputTarget.value;
+    this.values = { ...this.values, [key]: inputValue };
+    console.log(this.values)
+    this.valuesChange.emit(this.values);
+  }
+
+  refreshBlockSize(): void {
+    this.values = {width: '10', height: '10', depth: '10'};
     console.log(this.values)
     this.valuesChange.emit(this.values);
   }
