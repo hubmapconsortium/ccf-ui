41615d41d367d201502089fe1b4d0087dd8f95f6
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-08-05 11:10:33 -0400

    basic component set up with styling.

--- projects/ccf-rui/src/app/shared/components/rotation-slider/rotation-slider.module.ts
+++ projects/ccf-rui/src/app/shared/components/rotation-slider/rotation-slider.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RotationSliderComponent } from './rotation-slider.component';
 
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [RotationSliderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [RotationSliderComponent]
 })
