0d9ac85ee769efe111797e6fa7a189558185c9be
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-08-07 15:57:11 -0400

    put rotation slider back in app.component

--- projects/ccf-rui/src/app/app.module.ts
+++ projects/ccf-rui/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { StageNavModule } from './modules/stage-nav/stage-nav.module';
+import { RotationSliderModule } from './shared/components/rotation-slider/rotation-slider.module';
 import { DEFAULT_THEME } from './core/services/theming/theming.service';
 
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, StageNavModule],
+  imports: [BrowserModule, CoreModule, RotationSliderModule, StageNavModule],
   declarations: [AppComponent],
   providers: [{provide: DEFAULT_THEME, useValue: 'light-theme'}],
   bootstrap: [AppComponent]
