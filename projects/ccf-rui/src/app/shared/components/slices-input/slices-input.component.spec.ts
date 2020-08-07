809e0f04141d7e27fdf084757ae8cb616c15d214
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-07 13:31:25 -0400

    Fix tests

---
+++ projects/ccf-rui/src/app/shared/components/slices-input/slices-input.component.spec.ts
@@ -0,0 +1,29 @@
+import { Shallow } from 'shallow-render';
+
+import { SlicesInputComponent } from './slices-input.component';
+import { SlicesInputModule } from './slices-input.module';
+
+describe('SlicesInputComponent', () => {
+  let shallow: Shallow<SlicesInputComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(SlicesInputComponent, SlicesInputModule);
+  });
+
+  it('should update the values object with the passed in value', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.slicesData = {thickness: '', numSlices: ''};
+    const mockEvent = { target: {value: 20} } as unknown as InputEvent;
+    instance.updateSlicesData(mockEvent, 'thickness');
+    expect(instance.slicesData.thickness).toBe(20);
+    expect(outputs.valuesChange.emit).toHaveBeenCalled();
+  });
+
+  it('should change all values to defaults when refreshSlices is called', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.slicesData = {thickness: 100, numSlices: 50};
+    instance.refreshSlices();
+    expect(instance.slicesData).toEqual({thickness: '', numSlices: ''});
+    expect(outputs.valuesChange.emit).toHaveBeenCalled();
+  });
+});
