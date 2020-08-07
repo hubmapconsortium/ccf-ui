fbadae35c6a261070692d8ad7f9cfee585d901bd
Edward Lu <eddd.lu@gmail.com>
2020-08-07 11:38:40 -0400

    add block-size-input component files

---
+++ projects/ccf-rui/src/app/shared/block-size-input/block-size-input.component.spec.ts
@@ -0,0 +1,29 @@
+import { Shallow } from 'shallow-render';
+
+import { BlockSizeInputComponent } from './block-size-input.component';
+import { BlockSizeInputModule } from './block-size-input.module';
+
+describe('BlockSizeInputComponent', () => {
+  let shallow: Shallow<BlockSizeInputComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(BlockSizeInputComponent, BlockSizeInputModule);
+  });
+
+  it('should update the values object with the passed in value', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.values = {};
+    const mockEvent = { target: {value: '5'} } as unknown as InputEvent;
+    instance.updateBlockSize(mockEvent, 'depth');
+    expect(instance.values.depth).toBe('5');
+    expect(outputs.valuesChange.emit).toHaveBeenCalled();
+  });
+
+  it('should change all values to defaults when refreshBlockSize is called', async () => {
+    const { instance, outputs } = await shallow.render({ bind: {} });
+    instance.values = {};
+    instance.refreshBlockSize();
+    expect(instance.values).toEqual({width: '10', height: '10', depth: '10'});
+    expect(outputs.valuesChange.emit).toHaveBeenCalled();
+  });
+});
---
+++ projects/ccf-rui/src/app/shared/block-size-input/block-size-input.component.ts
@@ -0,0 +1,64 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+export interface TissueData {
+  width: number;
+  height: number;
+  depth: number;
+  thickness: any;
+  numSlices: any;
+}
+
+/**
+ * Component for entering dimensions of the tissue block
+ */
+@Component({
+  selector: 'ccf-block-size-input',
+  templateUrl: './block-size-input.component.html',
+  styleUrls: ['./block-size-input.component.scss']
+})
+export class BlockSizeInputComponent {
+
+  /**
+   * Values of block dimensions to be emitted
+   */
+  tissueData: TissueData = {
+    width: 10,
+    height: 10,
+    depth: 10,
+    thickness: '',
+    numSlices: ''
+  };
+
+  /**
+   * Emitter for values
+   */
+  @Output() valuesChange = new EventEmitter<TissueData>();
+
+  /**
+   * Updates values when an input changes
+   * @param input InputEvent from the input element which contains the new value
+   * @param key Name of the dimension to be updated
+   */
+  updateTissueData(input: InputEvent, key: string): void {
+    const inputTarget = input.target as HTMLInputElement;
+    const inputValue = inputTarget.value;
+    this.tissueData = { ...this.tissueData, [key]: inputValue };
+    console.log(this.tissueData);
+    this.valuesChange.emit(this.tissueData);
+  }
+
+  /**
+   * Refreshes all block size values to 10
+   */
+  refreshBlockSize(): void {
+    this.tissueData = { ...this.tissueData, width: 10, height: 10, depth: 10};
+    console.log(this.tissueData);
+    this.valuesChange.emit(this.tissueData);
+  }
+
+  refreshSlices(): void {
+    this.tissueData = { ...this.tissueData, thickness: '', numSlices: ''};
+    console.log(this.tissueData);
+    this.valuesChange.emit(this.tissueData);
+  }
+}
