661c26e5366a8a4793dbcd55a4751236ad1f9502
axdanbol <daniel_bolin@yahoo.com>
2020-08-10 02:28:48 -0400

    Added header scaffold

--- projects/ccf-rui/src/app/app.module.ts
+++ projects/ccf-rui/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { DEFAULT_THEME } from './core/services/theming/theming.service';
+import { HeaderModule } from './modules/header/header.module';
 import { LeftSidebarModule } from './modules/left-sidebar/left-sidebar.module';
 import { RightSidebarModule } from './modules/right-sidebar/right-sidebar.module';
 import { DrawerModule } from './shared/components/drawer/drawer.module';
 
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, DrawerModule, LeftSidebarModule, RightSidebarModule],
+  imports: [BrowserModule, CoreModule, DrawerModule, LeftSidebarModule, RightSidebarModule, HeaderModule],
   declarations: [AppComponent],
   providers: [{provide: DEFAULT_THEME, useValue: 'light-theme'}],
   bootstrap: [AppComponent]
