92521885996e33fe4e676cc0c4afd8ab956f6415
axdanbol <daniel_bolin@yahoo.com>
2020-08-11 10:20:50 -0400

    Renamed ccf-components to ccf-shared

--- projects/ccf-shared/karma.conf.js
+++ projects/ccf-shared/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ccf-components'),
+      dir: require('path').join(__dirname, '../../coverage/ccf-shared'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
       thresholds: {
---
+++ projects/ccf-shared/src/lib/placeholder-tests.spec.ts
@@ -0,0 +1,6 @@
+
+describe('placeholder', () => {
+  it('simply passes', () => {
+    expect(true).toBeTruthy();
+  });
+});
