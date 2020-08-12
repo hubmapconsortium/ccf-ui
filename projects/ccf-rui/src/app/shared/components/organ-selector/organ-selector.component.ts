8fc6b0a32c2fc4d4c1810d834ead98814a14f297
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-12 17:46:02 -0400

    Refactoring

--- projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
+++ projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
@@ -1,35 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface OrganInfo {
+  name: string;
+  url?: string;
+  leftRight: boolean;
+  genderToggle: boolean;
+}
 
 @Component({
   selector: 'ccf-organ-selector',
   templateUrl: './organ-selector.component.html',
   styleUrls: ['./organ-selector.component.scss']
 })
-export class OrganSelectorComponent implements OnInit {
+export class OrganSelectorComponent {
 
-  moveLeft = false;
-  moveRight = false;
+  organList: OrganInfo[] = [
+    {name: 'A', leftRight: true, genderToggle: true},
+    {name: 'B', leftRight: true, genderToggle: false},
+    {name: 'C', leftRight: false, genderToggle: true},
+    {name: 'D', leftRight: false, genderToggle: false},
+    {name: 'E', leftRight: false, genderToggle: true},
+    {name: 'F', leftRight: true, genderToggle: false}
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  shiftLeft(): void {
-    this.moveLeft = true;
+  shift(dir: string, step: number): void {
     const element = document.getElementsByClassName('carousel-item-list')[0] as HTMLElement;
-    const val=(parseInt(element.style.left, 10) || 0) - 40;
-    element.style.left =  val+'px';
-    // element.style.transform = 'translate(-40px,0)';
-    element.style.transition = 'all .5s ease-in-out';
-    console.log(element.style.left)
-  }
-  shiftRight(): void {
-    this.moveRight = true;
-    const element = document.getElementsByClassName('carousel-item-list')[0] as HTMLElement;
-    const val=(parseInt(element.style.left, 10) || 0) + 40;
-    element.style.left =  val+'px';
-    console.log(element.style.left)
+    let val = parseInt(element.style.left, 10) || 0;
+    if (val === 0 && dir === 'right') {
+      return;
+    } else if (-val === step*(this.organList.length - 5) && dir === 'left') {
+      return;
+    }
+    val = dir === 'left' ? val -= step : val += step;
+    element.style.left = val+'px';
   }
 
 }
