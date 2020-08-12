450beaa2193d37fa3b7705550c92b6291ceb36f2
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-12 18:53:10 -0400

    Add selection and styling

--- projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
+++ projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
@@ -7,6 +7,12 @@ export interface OrganInfo {
   genderToggle: boolean;
 }
 
+export interface OrganSettings {
+  name?: string;
+  gender?: string;
+  side?: string;
+}
+
 @Component({
   selector: 'ccf-organ-selector',
   templateUrl: './organ-selector.component.html',
@@ -14,25 +20,46 @@ export interface OrganInfo {
 })
 export class OrganSelectorComponent {
 
+  onLeft = true;
+  onRight = false;
+
   organList: OrganInfo[] = [
     {name: 'A', leftRight: true, genderToggle: true},
     {name: 'B', leftRight: true, genderToggle: false},
     {name: 'C', leftRight: false, genderToggle: true},
     {name: 'D', leftRight: false, genderToggle: false},
     {name: 'E', leftRight: false, genderToggle: true},
-    {name: 'F', leftRight: true, genderToggle: false}
+    {name: 'F', leftRight: true, genderToggle: false},
+    {name: 'G', leftRight: false, genderToggle: false}
   ];
+
+  organSettings: OrganSettings = {
+    name: '',
+    gender: '',
+    side: ''
+  };
 
   shift(dir: string, step: number): void {
     const element = document.getElementsByClassName('carousel-item-list')[0] as HTMLElement;
     let val = parseInt(element.style.left, 10) || 0;
-    if (val === 0 && dir === 'right') {
+    if (val === 0 && dir === 'left') {
       return;
-    } else if (-val === step*(this.organList.length - 5) && dir === 'left') {
+    } else if (val === step*(5 - this.organList.length) && dir === 'right') {
       return;
     }
-    val = dir === 'left' ? val -= step : val += step;
+    val = dir === 'right' ? val -= step : val += step;
     element.style.left = val+'px';
+    this.onLeft = val === 0 ? true : false;
+    this.onRight = val === step*(5 - this.organList.length) ? true : false;
+  }
+
+  selectOrgan(icon: OrganInfo): void {
+    this.organSettings.name = icon.name;
+    console.log(this.organSettings);
+  }
+
+  isSelected(icon: OrganInfo): boolean {
+    return this.organSettings.name === icon.name;
   }
 
 }
