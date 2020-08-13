2594f33c65407bee6ce786b69b1ccc057b241250
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-13 15:38:32 -0400

    Add icons and adjust styling

--- projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
+++ projects/ccf-rui/src/app/shared/components/organ-selector/organ-selector.component.ts
@@ -3,14 +3,6 @@ import { Component } from '@angular/core';
 export interface OrganInfo {
   name: string;
   url?: string;
-  leftRight: boolean;
-  genderToggle: boolean;
-}
-
-export interface OrganSettings {
-  name?: string;
-  gender?: string;
-  side?: string;
 }
 
 @Component({
@@ -24,20 +16,22 @@ export class OrganSelectorComponent {
   onRight = false;
 
   organList: OrganInfo[] = [
-    {name: 'A', leftRight: true, genderToggle: true},
-    {name: 'B', leftRight: true, genderToggle: false},
-    {name: 'C', leftRight: false, genderToggle: true},
-    {name: 'D', leftRight: false, genderToggle: false},
-    {name: 'E', leftRight: false, genderToggle: true},
-    {name: 'F', leftRight: true, genderToggle: false},
-    {name: 'G', leftRight: false, genderToggle: false}
+    {name: 'Bladder', url: '../../../assets/icons-organs_bladder.svg'},
+    {name: 'Brain', url: '../../../assets/icons-organs_brain.svg'},
+    {name: 'Colon', url: '../../../assets/icons-organs_colon.svg'},
+    {name: 'Heart', url: '../../../assets/icons-organs_heart.svg'},
+    {name: 'Kidney', url: '../../../assets/icons-organs_kidney.svg'},
+    {name: 'Liver', url: '../../../assets/icons-organs_liver.svg'},
+    {name: 'Lung', url: '../../../assets/icons-organs_lung.svg'},
+    {name: 'Lymph Nodes', url: '../../../assets/icons-organs_lymph-nodes.svg'},
+    {name: 'Ovaries', url: '../../../assets/icons-organs_ovaries.svg'},
+    {name: 'Small Intestine', url: '../../../assets/icons-organs_small intestine.svg'},
+    {name: 'Spleen', url: '../../../assets/icons-organs_spleen.svg'},
+    {name: 'Stomach', url: '../../../assets/icons-organs_stomach.svg'},
+    {name: 'Thymus', url: '../../../assets/icons-organs_thymus.svg'}
   ];
 
-  organSettings: OrganSettings = {
-    name: '',
-    gender: '',
-    side: ''
-  };
+  selectedOrgan: string;
 
   shift(dir: string, step: number): void {
     const element = document.getElementsByClassName('carousel-item-list')[0] as HTMLElement;
@@ -54,12 +48,12 @@ export class OrganSelectorComponent {
   }
 
   selectOrgan(icon: OrganInfo): void {
-    this.organSettings.name = icon.name;
-    console.log(this.organSettings);
+    this.selectedOrgan = icon.name;
+    console.log(this.selectedOrgan);
   }
 
   isSelected(icon: OrganInfo): boolean {
-    return this.organSettings.name === icon.name;
+    return this.selectedOrgan === icon.name;
   }
 
 }
