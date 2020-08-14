ce2c1652c511ba6e6872591ada802a0297afa430
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-14 16:31:30 -0400

    Create gender selector component

--- projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
+++ projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LeftSidebarComponent } from './left-sidebar.component';
+import { GenderSelectorModule } from '../../shared/components/gender-selector/gender-selector.module';
 
 
 @NgModule({
   declarations: [LeftSidebarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, GenderSelectorModule],
   exports: [LeftSidebarComponent]
 })
 export class LeftSidebarModule { }
---
+++ projects/ccf-rui/src/app/shared/components/gender-selector/gender-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ccf-gender-selector',
+  templateUrl: './gender-selector.component.html',
+  styleUrls: ['./gender-selector.component.scss']
+})
+export class GenderSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
