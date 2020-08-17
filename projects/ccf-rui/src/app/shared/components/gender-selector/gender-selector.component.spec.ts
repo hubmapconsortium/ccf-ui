20f90017ee83a6632ad9cb4e564725039fb90470
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-17 13:07:15 -0400

    Testing and documentation

---
+++ projects/ccf-rui/src/app/shared/components/gender-selector/gender-selector.component.spec.ts
@@ -0,0 +1,19 @@
+import { Shallow } from 'shallow-render';
+
+import { GenderSelectorComponent } from './gender-selector.component';
+import { GenderSelectorModule } from './gender-selector.module';
+
+describe('GenderSelectorComponent', () => {
+  let shallow: Shallow<GenderSelectorComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(GenderSelectorComponent, GenderSelectorModule);
+  });
+
+  it('should emit the gender whenever updateGender is called.', async () => {
+    const { instance, outputs } = await shallow.render({ bind: { isMale: false } });
+    instance.updateGender(true);
+    expect(outputs.genderChanged.emit).toHaveBeenCalledWith('female');
+  });
+
+});
--- projects/ccf-rui/src/app/shared/components/gender-selector/gender-selector.component.ts
+++ projects/ccf-rui/src/app/shared/components/gender-selector/gender-selector.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+/**
+ * Slider for gender selection
+ */
 @Component({
   selector: 'ccf-gender-selector',
   templateUrl: './gender-selector.component.html',
@@ -7,11 +10,25 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class GenderSelectorComponent {
 
+  /**
+   * Disables the silder if not applicable to currently selected organ
+   */
+  @Input() disabled = false;
+
+  /**
+   * Input to determine if 'Male' is selected
+   */
   @Input() isMale = true;
+
+  /**
+   * Emits the currently selected gender
+   */
   @Output() genderChanged = new EventEmitter<string>();
 
-  constructor() { }
-
+  /**
+   * Updates and emits the currently selected gender
+   * @param selection The current toggle state
+   */
   updateGender(selection: boolean): void {
     this.isMale = !selection;
     this.genderChanged.emit(this.isMale ? 'male' : 'female');
