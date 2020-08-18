a33125c7041387503636159cafeb1d135a7356c3
axdanbol <daniel_bolin@yahoo.com>
2020-08-18 12:19:25 -0400

    Updated stage stage and connected to inputs/outputs in right sidebar

--- projects/ccf-rui/src/app/core/store/stage/stage.state.ts
+++ projects/ccf-rui/src/app/core/store/stage/stage.state.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { StateRepository } from '@ngxs-labs/data/decorators';
-import { NgxsDataRepository } from '@ngxs-labs/data/repositories';
+import { DataAction, StateRepository } from '@ngxs-labs/data/decorators';
+import { NgxsImmutableDataRepository } from '@ngxs-labs/data/repositories';
 import { State } from '@ngxs/store';
+import { pluck } from 'rxjs/operators';
 
 
 export interface XYZTriplet<T = number> {
@@ -10,8 +11,15 @@ export interface XYZTriplet<T = number> {
   z: T;
 }
 
+export interface SlicesConfig {
+  thickness: number;
+  numSlices: number;
+}
+
 export interface StageStateModel {
+  blockSize: XYZTriplet;
   rotation: XYZTriplet;
+  slicesConfig: SlicesConfig;
 }
 
 
@@ -19,9 +27,29 @@ export interface StageStateModel {
 @State<StageStateModel>({
   name: 'stage',
   defaults: {
-    rotation: { x: 0, y: 0, z: 0 }
+    blockSize: { x: 10, y: 10, z: 10 },
+    rotation: { x: 0, y: 0, z: 0 },
+    slicesConfig: { thickness: NaN, numSlices: NaN }
   }
 })
 @Injectable()
-export class StageState extends NgxsDataRepository<StageStateModel> {
+export class StageState extends NgxsImmutableDataRepository<StageStateModel> {
+  readonly blockSize$ = this.state$.pipe(pluck('blockSize'));
+  readonly rotation$ = this.state$.pipe(pluck('rotation'));
+  readonly slicesConfig$ = this.state$.pipe(pluck('slicesConfig'));
+
+  @DataAction()
+  updateBlockSize(blockSize: XYZTriplet): void {
+    this.ctx.patchState({ blockSize });
+  }
+
+  @DataAction()
+  updateRotation(rotation: XYZTriplet): void {
+    this.ctx.patchState({ rotation });
+  }
+
+  @DataAction()
+  updateSlicesConfig(slicesConfig: SlicesConfig): void {
+    this.ctx.patchState({ slicesConfig });
+  }
 }
