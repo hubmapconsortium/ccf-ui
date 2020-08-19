91e058b9ebab40c3a84d32657bce9a9324773b7a
axdanbol <daniel_bolin@yahoo.com>
2020-08-19 15:20:23 -0400

    Added tests

---
+++ projects/ccf-rui/src/app/core/store/stage/stage.state.spec.ts
@@ -0,0 +1,80 @@
+import { TestBed } from '@angular/core/testing';
+import { NgxsDataPluginModule } from '@ngxs-labs/data';
+import { NgxsModule, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+import { SlicesConfig, StageState, XYZTriplet } from './stage.state';
+
+
+function nextValue<T>(obs: Observable<T>): Promise<T> {
+  return obs.pipe(take(1)).toPromise();
+}
+
+
+describe('StageState', () => {
+  const initialXYZTriplet: XYZTriplet = { x: 0, y: 0, z: 0 };
+  const initialSlicesConfig: SlicesConfig = { thickness: 0, numSlices: 1 };
+  let state: StageState;
+
+  beforeEach(() => {
+    // NOTE: No need for shallow-render since
+    // the setup is so simple. It would also require
+    // us to create both a fake component and module.
+    TestBed.configureTestingModule({
+      imports: [
+        NgxsDataPluginModule.forRoot(),
+        NgxsModule.forRoot([StageState])
+      ]
+    });
+
+    TestBed.inject(Store).reset({
+      stage: {
+        blockSize: initialXYZTriplet,
+        rotation: initialXYZTriplet,
+        slicesConfig: initialSlicesConfig
+      }
+    });
+
+    state = TestBed.inject(StageState);
+  });
+
+  it('has the latest block size', async () => {
+    const value = await nextValue(state.blockSize$);
+    expect(value).toEqual(initialXYZTriplet);
+  });
+
+  it('has the latest rotation', async () => {
+    const value = await nextValue(state.rotation$);
+    expect(value).toEqual(initialXYZTriplet);
+  });
+
+  it('has the latest slices configuration', async () => {
+    const value = await nextValue(state.slicesConfig$);
+    expect(value).toEqual(initialSlicesConfig);
+  });
+
+  it('updates the block size', async () => {
+    const newBlockSize = { x: 1, y: 2, z: 3};
+    state.updateBlockSize(newBlockSize);
+
+    const value = await nextValue(state.blockSize$);
+    expect(value).toEqual(newBlockSize);
+  });
+
+  it('updates the rotation', async () => {
+    const newRotation = { x: 1, y: 2, z: 3};
+    state.updateRotation(newRotation);
+
+    const value = await nextValue(state.rotation$);
+    expect(value).toEqual(newRotation);
+  });
+
+  it('updates the slices configuration', async () => {
+    const newSlicesConfig = { thickness: 2, numSlices: 3 };
+    state.updateSlicesConfig(newSlicesConfig);
+
+    const value = await nextValue(state.slicesConfig$);
+    expect(value).toEqual(newSlicesConfig);
+  });
+});
--- projects/ccf-shared/src/lib/components/store-debug/store-debug.component.spec.ts
+++ projects/ccf-shared/src/lib/components/store-debug/store-debug.component.spec.ts
@@ -1,25 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { Shallow } from 'shallow-render';
 
 import { StoreDebugComponent } from './store-debug.component';
+import { StoreDebugModule } from './store-debug.module';
+
 
 describe('StoreDebugComponent', () => {
-  let component: StoreDebugComponent;
-  let fixture: ComponentFixture<StoreDebugComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ StoreDebugComponent ]
-    })
-    .compileComponents();
-  }));
+  let shallow: Shallow<StoreDebugComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(StoreDebugComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    shallow = new Shallow(StoreDebugComponent, StoreDebugModule)
+      // NOTE: When importing NgxsModule.forRoot() the returned module
+      // is NOT NgxsModule but the internal NgxsRootModule!
+      // Therefore .dontMock(NgxsModule) will NOT work as expected!
+      // The following is a workaround.
+      .import(NgxsModule)
+      .replaceModule(NgxsModule, NgxsModule.forRoot());
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should turn the state into flat key-value lists', async () => {
+    const { instance, get } = await shallow.render();
+    get(Store).reset({ test: { key: 'value' } });
+
+    expect(instance.data).toEqual([['test', [['key', 'value']]]]);
+  });
+
+  it('excludes state with no data', async () => {
+    const { instance, get } = await shallow.render();
+    get(Store).reset({ test: { } });
+
+    expect(instance.data).toEqual([]);
   });
 });
