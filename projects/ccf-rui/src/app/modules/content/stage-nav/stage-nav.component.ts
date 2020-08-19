2d97fa789b5ba5fa53bf98143140a0cbaa6b1d93
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-19 16:42:01 -0400

    Clean up extra code

--- projects/ccf-rui/src/app/modules/content/stage-nav/stage-nav.component.ts
+++ projects/ccf-rui/src/app/modules/content/stage-nav/stage-nav.component.ts
@@ -25,19 +25,9 @@ export class StageNavComponent {
   @Input() side: Side = 'anterior';
 
   /**
-   * Input that allows toggling of 3D view on / off from outside the component
-   */
-  @Input() view3D = false;
-
-  /**
    * Output that emits whenever the current side selection changes
    */
   @Output() sideChanged = new EventEmitter<Side>();
-
-  /**
-   * Output that emits whenever the 3D view is toggled on / off
-   */
-  @Output() view3DChanged = new EventEmitter<boolean>();
 
   /**
    * Handles the updating of the side selection and calling the event emitter
@@ -46,15 +36,5 @@ export class StageNavComponent {
   updateSide(selection: Side): void {
     this.side = selection;
     this.sideChanged.emit(this.side);
-  }
-
-  /**
-   * Handles updating of the boolean that keeps track of current view
-   * and calling the event emitter.
-   * @param selection 3D (true) or Register (false)
-   */
-  updateView(selection: boolean): void {
-    this.view3D = selection;
-    this.view3DChanged.emit(this.view3D);
   }
 }
