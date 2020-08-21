6a2bf71160fe1d390c50b58420877a38b9fd444d
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-21 14:01:12 -0400

    Add entry selection/highlighting

--- projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.component.ts
+++ projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.component.ts
@@ -3,7 +3,8 @@ import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/co
 export interface ExtractionSite {
   name: string;
   selected: boolean;
-  src?: string;
+  highlighted: boolean;
+  iconSrc: string;
 }
 
 @Component({
@@ -15,12 +16,28 @@ export class ExtractionSitesMenuComponent {
 
   @HostBinding('class') readonly clsName = 'ccf-extraction-sites-menu';
 
-  @Input() extractionSites: ExtractionSite[] = [ {name: 'A', selected: false}, {name: 'B', selected: true}]
-
-  selectedSites: string[];
+  @Input() extractionSites: ExtractionSite[] = [
+    {name: 'A', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'B', selected: true, highlighted: true, iconSrc: 'app:visibility_on'}
+  ];
 
   toggleSite(site: ExtractionSite): void {
     site.selected = !site.selected;
+    if (site.highlighted) {
+      return;
+    } else {
+      this.highlight(site);
+    }
+  }
+
+  highlight(site: ExtractionSite): void {
+    site.highlighted = true;
+    site.iconSrc = site.highlighted ? 'app:visibility_on' : 'app:visibility_off';
+  }
+
+  hoverOut(site: ExtractionSite): void {
+    site.highlighted = site.selected ? true : false;
+    site.iconSrc = site.highlighted ? 'app:visibility_on' : 'app:visibility_off';
   }
 
 }
