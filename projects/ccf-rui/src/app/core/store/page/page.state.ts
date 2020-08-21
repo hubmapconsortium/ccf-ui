aed60e1f97df9cb3e9eecb3590acf1e84483b4c2
axdanbol <daniel_bolin@yahoo.com>
2020-08-21 12:13:49 -0400

    Exposed method to change embedded state on globalThis during development

--- projects/ccf-rui/src/app/core/store/page/page.state.ts
+++ projects/ccf-rui/src/app/core/store/page/page.state.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { StateRepository } from '@ngxs-labs/data/decorators';
-import { NgxsDataRepository } from '@ngxs-labs/data/repositories';
+import { DataAction, StateRepository } from '@ngxs-labs/data/decorators';
+import { NgxsImmutableDataRepository } from '@ngxs-labs/data/repositories';
 import { State } from '@ngxs/store';
 import { pluck } from 'rxjs/operators';
 
+import { environment } from '../../../../environments/environment';
 
 
 export interface PageStateModel {
@@ -21,7 +22,23 @@ export interface PageStateModel {
   }
 })
 @Injectable()
-export class PageState extends NgxsDataRepository<PageStateModel> {
+export class PageState extends NgxsImmutableDataRepository<PageStateModel> {
   readonly embedded$ = this.state$.pipe(pluck('embedded'));
   readonly homeUrl$ = this.state$.pipe(pluck('homeUrl'));
+
+  constructor() {
+    super();
+
+    if (!environment.production && typeof globalThis === 'object') {
+      globalThis.setEmbedded = this.setEmbedded.bind(this);
+    }
+  }
+
+  @DataAction()
+  setEmbedded(embedded: boolean, url?: string): void {
+    this.ctx.patchState({
+      embedded,
+      homeUrl: url ?? this.ctx.getState().homeUrl
+    });
+  }
 }
