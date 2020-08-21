f0a85a39b6f279a6d85482a61a58d27cc9cab30e
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-21 05:49:43 -0400

    Add icons and selection logic

--- projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.component.ts
+++ projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.component.ts
@@ -1,4 +1,10 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+
+export interface ExtractionSite {
+  name: string;
+  selected: boolean;
+  src?: string;
+}
 
 @Component({
   selector: 'ccf-extraction-sites-menu',
@@ -8,5 +14,13 @@ import { Component, HostBinding } from '@angular/core';
 export class ExtractionSitesMenuComponent {
 
   @HostBinding('class') readonly clsName = 'ccf-extraction-sites-menu';
+
+  @Input() extractionSites: ExtractionSite[] = [ {name: 'A', selected: false}, {name: 'B', selected: true}]
+
+  selectedSites: string[];
+
+  toggleSite(site: ExtractionSite): void {
+    site.selected = !site.selected;
+  }
 
 }
--- projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.module.ts
+++ projects/ccf-rui/src/app/shared/components/extraction-sites-menu/extraction-sites-menu.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ExtractionSitesMenuComponent } from './extraction-sites-menu.component';
 
 
 @NgModule({
-  imports: [CommonModule, MatExpansionModule],
+  imports: [CommonModule, MatExpansionModule, MatIconModule],
   declarations: [ExtractionSitesMenuComponent],
   exports: [ExtractionSitesMenuComponent]
 })
