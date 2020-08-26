ff3706d1fb7f34ad7fedeed2c697cd279b748407
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-26 17:51:25 -0400

    Revise menu behavior

--- projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.component.ts
+++ projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.component.ts
@@ -16,22 +16,22 @@ export class LeftSidebarComponent {
   detailsLabels: string[] = ['heart', 'front', 'female'];
 
   extractionSites: VisibilityItem[] = [
-    {name: 'Left atrium, appendage', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Left atrium, PV inflow', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Left ventricle, apex', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Left ventricle, free wall 3cm from apex', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Septum, 3cm from apex including LAD', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Posterior, adjacent to coronary sinus', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Right atrium appendage', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Right atrium, AV(atrioventricular) node', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Right atrium, SA(sinoatrial) node', selected: false, highlighted: false, iconSrc: 'app:visibility_off'},
-    {name: 'Right ventricle, free wall 3cm from apex', selected: false, highlighted: false, iconSrc: 'app:visibility_off'}
+    {name: 'Left atrium, appendage', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Left atrium, PV inflow', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Left ventricle, apex', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Left ventricle, free wall 3cm from apex', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Septum, 3cm from apex including LAD', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Posterior, adjacent to coronary sinus', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Right atrium appendage', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Right atrium, AV(atrioventricular) node', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Right atrium, SA(sinoatrial) node', highlighted: false, iconSrc: 'app:visibility_off'},
+    {name: 'Right ventricle, free wall 3cm from apex', highlighted: false, iconSrc: 'app:visibility_off'}
   ];
 
   anatomicalStructures: VisibilityItem[] = [
-    {name: 'Structure A', selected: false, highlighted: false, iconSrc: 'app:visibility_off', opacity: 20},
-    {name: 'Structure B', selected: false, highlighted: false, iconSrc: 'app:visibility_off', opacity: 20},
-    {name: 'Structure C', selected: false, highlighted: false, iconSrc: 'app:visibility_off', opacity: 20}
+    {name: 'Structure A', highlighted: false, iconSrc: 'app:visibility_off', opacity: 100},
+    {name: 'Structure B', highlighted: false, iconSrc: 'app:visibility_off', opacity: 100},
+    {name: 'Structure C', highlighted: false, iconSrc: 'app:visibility_off', opacity: 100}
   ];
 
   constructor(readonly page: PageState) {}
--- projects/ccf-rui/src/app/shared/components/visibility-menu/visibility-menu.component.ts
+++ projects/ccf-rui/src/app/shared/components/visibility-menu/visibility-menu.component.ts
@@ -11,11 +11,6 @@ export interface VisibilityItem {
   name: string;
 
   /**
-   * Whether the item is currently active
-   */
-  selected: boolean;
-
-  /**
    * Whether the item is currently highlighted
    */
   highlighted: boolean;
@@ -54,30 +49,28 @@ export class VisibilityMenuComponent {
   /**
    * Emits the currently highlighted items
    */
-  @Output() valueChange = new EventEmitter<string[]>();
+  @Output() highlightChange = new EventEmitter<VisibilityItem[]>();
+
+  /**
+   * Emits the currently selected item
+   */
+  @Output() selectionChange = new EventEmitter<VisibilityItem>();
 
   /**
    * Whether opacity value should be displayed
    */
   @Input() opacityOn = false;
 
+  selectedItem: VisibilityItem;
+
   /**
-   * Sets the icon type and emits an array containing the currently highlighted items
+   * Toggles highlight state, sets the icon type, and emits an array containing the currently highlighted items
    * @param item Visibility item
    */
   toggleHighlight(item: VisibilityItem): void {
+    item.highlighted = !item.highlighted;
     item.iconSrc = item.highlighted ? 'app:visibility_on' : 'app:visibility_off';
-    this.valueChange.emit(this.visibilityItems.filter(x => x.highlighted).map(entry => entry.name));
-  }
-
-  /**
-   * Sets highlight status of an item (on mouseout or click)
-   * Sets icon and emits currently highlighted items
-   * @param item Visibility item
-   */
-  setHighlight(item: VisibilityItem): void {
-    item.highlighted = item.selected ? true : false;
-    this.toggleHighlight(item);
+    this.highlightChange.emit(this.visibilityItems.filter(x => x.highlighted));
   }
 
   /**
@@ -85,17 +78,7 @@ export class VisibilityMenuComponent {
    * @param item Visibility item
    */
   toggleSelected(item: VisibilityItem): void {
-    item.selected = !item.selected;
-    this.setHighlight(item);
-  }
-
-  /**
-   * Highlights the item on hover
-   * Sets icon and emits currently highlighted items
-   * @param item Visibility item
-   */
-  hover(item: VisibilityItem): void {
-    item.highlighted = true;
-    this.toggleHighlight(item);
+    this.selectedItem = item;
+    this.selectionChange.emit(item);
   }
 }
