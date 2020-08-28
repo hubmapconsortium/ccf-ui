7d8a251a5dc47b8de071e2c1ccb7496e34d2921c
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-08-28 13:14:29 -0400

    Change to angular slider

--- projects/ccf-rui/src/app/shared/components/opacity-slider/opacity-slider.module.ts
+++ projects/ccf-rui/src/app/shared/components/opacity-slider/opacity-slider.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpacitySliderComponent } from './opacity-slider.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { MatIconModule } from '@angular/material/icon';
 
@@ -8,7 +9,8 @@ import { MatIconModule } from '@angular/material/icon';
   declarations: [OpacitySliderComponent],
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    MatSliderModule
   ],
   exports: [OpacitySliderComponent]
 })
