5ff1f67c11d928db14e0fe7af0572e595190cd4d
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-09-02 08:29:30 -0400

    Pbi 280 review download button (#321)
    * set up for review/download modal mostly done.
    
    * most of the html / css / theming in place, getting close
    
    * review modal set up. tests need updated still
    
    * minor changes
    
    * refactor reviewobject to registrationdata
    
    * changed to correct types, added formatting function, some cleanup
    
    * refactoring of component. test coverage up

---
+++ projects/ccf-rui/src/app/modules/review/review-button/review-button.component.spec.ts
@@ -0,0 +1,49 @@
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { Shallow } from 'shallow-render';
+
+import { ReviewButtonComponent } from './review-button.component';
+import { ReviewButtonModule } from './review-button.module';
+
+describe('ReviewButtonComponent', () => {
+  let shallow: Shallow<ReviewButtonComponent>;
+  let afterClosedObservable: Subject<boolean>;
+
+  beforeEach(() => {
+    const mockDialog = jasmine.createSpyObj<MatDialogRef<unknown, boolean>>('DialogRef', ['afterClosed']);
+    afterClosedObservable = new Subject();
+    mockDialog.afterClosed.and.returnValue(afterClosedObservable);
+
+    shallow = new Shallow(ReviewButtonComponent, ReviewButtonModule)
+      .mock(MatDialog, { open(): MatDialogRef<unknown, boolean> { return mockDialog; } });
+  });
+
+  it('should launch the review dialog', async () => {
+    const { find, instance } = await shallow.render();
+    const spy = spyOn(instance, 'launchReviewModal');
+    find('.review-button').triggerEventHandler('click', '');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('launchReviewModal opens the modal', async () => {
+    const { instance, get } = await shallow.render();
+    instance.launchReviewModal();
+    expect(get(MatDialog).open).toHaveBeenCalled();
+  });
+
+  it('should emit the event when the modal is closed via the download button', async () => {
+    const { instance, outputs } = await shallow.render();
+    instance.launchReviewModal();
+    afterClosedObservable.next(true);
+
+    expect(outputs.registerData.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit the event when the modal is closed not via the download button', async () => {
+    const { instance, outputs } = await shallow.render();
+    instance.launchReviewModal();
+    afterClosedObservable.next(false);
+
+    expect(outputs.registerData.emit).not.toHaveBeenCalled();
+  });
+});
