e42c10c2b0be1716a89847fb2ed7d670bdbfa25e
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-09-02 13:35:49 -0400

    Use lazy rendering for panel contents

--- projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.component.ts
+++ projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.component.ts
@@ -13,8 +13,6 @@ export class LeftSidebarComponent {
   /** HTML class name */
   @HostBinding('class') readonly clsName = 'ccf-left-sidebar';
 
-  load = false;
-
   detailsLabels: string[] = ['heart', 'front', 'female'];
 
   extractionSites: VisibilityItem[] = [
