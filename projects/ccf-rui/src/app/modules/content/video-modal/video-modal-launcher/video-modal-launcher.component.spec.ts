9161b33520671e9e351e47f1c1ac95665ae1a1f2
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-09-08 14:49:22 -0400

    video-modal and launcher tests

--- projects/ccf-rui/src/app/modules/content/video-modal/video-modal-launcher/video-modal-launcher.component.spec.ts
+++ projects/ccf-rui/src/app/modules/content/video-modal/video-modal-launcher/video-modal-launcher.component.spec.ts
@@ -1,5 +1,31 @@
+import { Shallow } from 'shallow-render';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
 import { VideoModalLauncherComponent } from './video-modal-launcher.component';
+import { VideoModalLauncherModule } from './video-modal-launcher.module';
 
 describe('VideoModalLauncherComponent', () => {
+    let shallow: Shallow<VideoModalLauncherComponent>;
+    const mockMatDialog = {
+        open(...args: unknown[]): MatDialogRef<unknown, unknown> {
+            return undefined as unknown as MatDialogRef<unknown, unknown>;
+        }
+    };
 
+    beforeEach(() => {
+        shallow = new Shallow(VideoModalLauncherComponent, VideoModalLauncherModule);
+    });
+
+    it('should launch the video modal', async () => {
+        const { find, instance } = await shallow.mock(MatDialog, mockMatDialog).render();
+        const spy = spyOn(instance, 'launchVideoModal');
+        find('mat-icon').triggerEventHandler('click', '');
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('launchVideoModal opens the modal', async () => {
+        const { instance, get } = await shallow.mock(MatDialog, mockMatDialog).render();
+        instance.launchVideoModal();
+        expect(get(MatDialog).open).toHaveBeenCalled();
+    });
 });
