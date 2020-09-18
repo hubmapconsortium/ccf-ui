19b6b9aaf31105dc24f6622073f7634436a23216
adphillips2017 <adphillips2017@users.noreply.github.com>
2020-09-18 14:48:58 -0400

    body-ui component created

---
+++ projects/ccf-rui/src/app/modules/body-ui/body-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ccf-body-ui',
+  templateUrl: './body-ui.component.html',
+  styleUrls: ['./body-ui.component.scss']
+})
+export class BodyUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
