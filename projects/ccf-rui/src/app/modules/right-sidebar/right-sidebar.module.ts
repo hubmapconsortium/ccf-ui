22fe284155bd92500d20c62cfe06ca7719e51155
axdanbol <daniel_bolin@yahoo.com>
2020-09-28 04:47:47 -0400

    Updated right sidebar

--- projects/ccf-rui/src/app/modules/right-sidebar/right-sidebar.module.ts
+++ projects/ccf-rui/src/app/modules/right-sidebar/right-sidebar.module.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { TextSearchModule } from 'ccf-shared';
 
 import { BlockSizeInputModule } from '../../shared/components/block-size-input/block-size-input.module';
 import { RotationSliderModule } from '../../shared/components/rotation-slider/rotation-slider.module';
@@ -12,14 +10,12 @@ import { ToggleableTooltipModule } from '../../shared/components/toggleable-tool
 import { ReviewButtonModule } from '../review/review-button/review-button.module';
 import { RightSidebarComponent } from './right-sidebar.component';
 
+
 @NgModule({
   imports: [
     CommonModule,
 
     MatExpansionModule,
-    MatIconModule,
-
-    TextSearchModule,
 
     BlockSizeInputModule,
     ReviewButtonModule,
