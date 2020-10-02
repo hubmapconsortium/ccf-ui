9607aa4629348e970c9b8e3b5c6053904abd790d
Edward Lu <36934022+edlu77@users.noreply.github.com>
2020-10-02 15:20:56 -0400

    Make organ attribute optional

--- projects/ccf-rui/src/app/core/models/extraction-set.ts
+++ projects/ccf-rui/src/app/core/models/extraction-set.ts
@@ -8,7 +8,7 @@ export interface ExtractionSet {
   name: string;
 
   /** Organ that the extraction sites belong to */
-  organ: string;
+  organ?: string;
 
   /** Extraction sites belonging to the organ in the extraction set */
   sites: VisibilityItem[];
