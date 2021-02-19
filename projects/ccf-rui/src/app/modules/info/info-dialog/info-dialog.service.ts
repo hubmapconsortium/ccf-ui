fdf21b2604f35b24cde35c7d44d57a3386e45325
Hrishikesh Paul <hrpaul@iu.edu>
2021-02-19 13:17:09 -0500

    chore: added ngx-markdown to package.json and used HttpClient to retrive the documentation

--- projects/ccf-rui/src/app/modules/info/info-dialog/info-dialog.service.ts
+++ projects/ccf-rui/src/app/modules/info/info-dialog/info-dialog.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { DocumentationContent } from '../../../core/models/documentation';
-import * as documentation from 'raw-loader!./documentation.md';
+import { HttpClient } from '@angular/common/http';
+
+const documentationURL = './../../../../assets/documentation.md';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class InfoDialogService {
   /** Subject to send the documentation data to the component when its done processing */
   markdownContent: BehaviorSubject<DocumentationContent[]> = new BehaviorSubject<DocumentationContent[]>([]);
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.readMarkdown();
   }
 
@@ -19,18 +21,21 @@ export class InfoDialogService {
    * Read the markdown file to split it by h1 tags.
    */
   readMarkdown(): void {
-    const markdownContent: DocumentationContent[] = [];
-    const splitByHeaderTag: string[] = documentation.default.split('# ');
-    for (const split of splitByHeaderTag) {
-      if (split.length) {
-        const headerAndContent: string[] = split.split('\n\n');
-        markdownContent.push({
-          title: headerAndContent[0],
-          content: headerAndContent.splice(1).join('\n\n')
-        });
+    this.http.get(documentationURL, {responseType: 'text'}).subscribe((data: string) => {
+      const markdownContent: DocumentationContent[] = [];
+      const splitByHeaderTag: string[] = data.split('# ');
+      for (const split of splitByHeaderTag) {
+        if (split.length) {
+          const headerAndContent: string[] = split.split('\n\n');
+          markdownContent.push({
+            title: headerAndContent[0],
+            content: headerAndContent.splice(1).join('\n\n')
+          });
+        }
       }
-    }
-
-    this.markdownContent.next(markdownContent);
+  
+      this.markdownContent.next(markdownContent);
+    })
+   
   }
 }
