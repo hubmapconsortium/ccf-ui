a8afb7064eba82a31bbc3afeda65034fa9089d90
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-04 12:30:38 -0500

    fix: app module dependency for info button

--- projects/ccf-eui/src/app/app.module.ts
+++ projects/ccf-eui/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BodyModule } from './modules/body/body.module';
 import { FiltersPopoverModule } from './modules/filters/filters-popover/filters-popover.module';
-import { InfoDialogModule } from './modules/info/info-dialog/info-dialog.module';
+import { InfoDialogModule } from 'ccf-shared';
 import { OntologyExplorationModule } from './modules/ontology-exploration/ontology-exploration.module';
 import { DrawerModule } from './shared/components/drawer/drawer.module';
 import { DualSliderModule } from './shared/components/dual-slider/dual-slider.module';
