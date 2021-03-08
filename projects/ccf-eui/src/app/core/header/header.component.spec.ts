69431b09d55376985b39bf8cfef95c820425ed8f
Hrishikesh Paul <hrishikeshpaul@users.noreply.github.com>
2021-03-08 12:17:37 -0500

    Styling updates (#517)
    * chore: added shadow to header
    
    * chore: improved search styles
    
    * fix: removed code smell
    
    * chore: refactored buttons from header
    
    * chore: rebased with develop
    
    * chore: major changes to the styling
    
    * chore: added logo type
    
    * chore: removed margin from top
    
    * chore: improved filter popup styles
    
    * chore: fixed styling on filters
    
    * fix: styles of organ selector when hovering in dark mode
    
    * chore: fixed test cases
    
    * chore: fixed code smell
    
    * chore: fixed carousel

--- projects/ccf-eui/src/app/core/header/header.component.spec.ts
+++ projects/ccf-eui/src/app/core/header/header.component.spec.ts
@@ -4,44 +4,8 @@ import { HeaderComponent } from './header.component';
 import { HeaderModule } from './header.module';
 
 describe('HeaderComponent', () => {
-  let shallow: Shallow<HeaderComponent>;
-  const testFilter = { sex: 'Both', ageRange: [5, 99], bmiRange: [30, 80] };
 
   beforeEach(() => {
-    shallow = new Shallow(HeaderComponent, HeaderModule);
   });
 
-  it('should emit downloadClicked when download button is clicked', async () => {
-    const { find, outputs } = await shallow.render();
-    find('.download-icon').triggerEventHandler('click', {});
-    expect(outputs.downloadClicked.emit).toHaveBeenCalled();
-  });
-
-  it('should emit refreshClicked when refresh button is clicked', async () => {
-    const { find, outputs } = await shallow.render();
-    find('.refresh').triggerEventHandler('click', {});
-    expect(outputs.refreshClicked.emit).toHaveBeenCalled();
-  });
-
-  async function testFilterLabel(
-    index: number,
-    data: Record<string, unknown[] | unknown>,
-    result: string
-  ): Promise<void> {
-    const { find } = await shallow.render({ bind: { filters: data } });
-    const label = find(`.filter-labels div:nth-child(${index})`).nativeElement as HTMLElement;
-    expect(label.textContent).toBe(result);
-  }
-
-  it('should display the current sex', async () => {
-    await testFilterLabel(1, testFilter, 'Sex: Both');
-  });
-
-  it('should display the current age range', async () => {
-    await testFilterLabel(2, testFilter, 'Age: 5-99');
-  });
-
-  it('should display the current BMI range', async () => {
-    await testFilterLabel(3, testFilter, 'BMI: 30-80');
-  });
 });
