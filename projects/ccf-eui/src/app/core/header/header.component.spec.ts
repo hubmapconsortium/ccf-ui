ce10a5fae9354242888982360cc9ed5e4cfb49c8
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-08 11:44:49 -0500

    chore: fixed code smell

--- projects/ccf-eui/src/app/core/header/header.component.spec.ts
+++ projects/ccf-eui/src/app/core/header/header.component.spec.ts
@@ -4,11 +4,8 @@ import { HeaderComponent } from './header.component';
 import { HeaderModule } from './header.module';
 
 describe('HeaderComponent', () => {
-  let shallow: Shallow<HeaderComponent>;
-  const testFilter = { sex: 'Both', ageRange: [5, 99], bmiRange: [30, 80] };
 
   beforeEach(() => {
-    shallow = new Shallow(HeaderComponent, HeaderModule);
   });
 
 });
