7ff6822a528e90ccbf06234754ad31668404cf77
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-09 13:31:30 -0500

    chore: removed code smell

--- projects/ccf-eui/src/app/core/header/header.component.spec.ts
+++ projects/ccf-eui/src/app/core/header/header.component.spec.ts
@@ -1,8 +1,3 @@
-import { Shallow } from 'shallow-render';
-
-import { HeaderComponent } from './header.component';
-import { HeaderModule } from './header.module';
-
 describe('HeaderComponent', () => {
 
   beforeEach(() => {
