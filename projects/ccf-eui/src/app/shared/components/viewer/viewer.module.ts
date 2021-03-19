ecaf4549e410cf60a7adcdf9e96e603c1c9603a2
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-19 13:44:01 -0400

    chore: loading screen and test cases

--- projects/ccf-eui/src/app/shared/components/viewer/viewer.module.ts
+++ projects/ccf-eui/src/app/shared/components/viewer/viewer.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ViewerComponent } from './viewer.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 
 @NgModule({
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, MatProgressSpinnerModule],
   declarations: [ViewerComponent],
   exports: [ViewerComponent]
 })
