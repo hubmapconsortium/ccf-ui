d9034d04c47ab92b102ec5b25f86bd27bd4c8f4e
axdanbol <daniel_bolin@yahoo.com>
2021-03-24 01:00:41 -0400

    Added tests

--- projects/ccf-eui/src/app/shared/components/thumbnail-carousel/thumbnail-carousel.component.spec.ts
+++ projects/ccf-eui/src/app/shared/components/thumbnail-carousel/thumbnail-carousel.component.spec.ts
@@ -1,7 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DatasetResult } from 'ccf-database';
+import { Shallow } from 'shallow-render';
+import { Swiper } from 'swiper';
 
 import { ThumbnailCarouselComponent } from './thumbnail-carousel.component';
+import { ThumbnailCarouselModule } from './thumbnail-carousel.module';
+
+
+function castPartial<T>(partial: Partial<T>): T {
+  return partial as T;
+}
 
 
 describe('ThumbnailCarouselComponent', () => {
+  let shallow: Shallow<ThumbnailCarouselComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(ThumbnailCarouselComponent, ThumbnailCarouselModule);
+  });
+
+  describe('itemId(index, item)', () => {
+    it('returns an identifier', async () => {
+      const { instance } = await shallow.render();
+      const result = instance.itemId(0, castPartial<DatasetResult>({ thumbnail: 'abc' }));
+      expect(result).toEqual('abc');
+    });
+  });
 });
