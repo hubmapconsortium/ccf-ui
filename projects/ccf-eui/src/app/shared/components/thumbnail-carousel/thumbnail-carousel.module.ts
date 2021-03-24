83a8e61161f8d76742e7e502396b1f87d9aedd89
axdanbol <daniel_bolin@yahoo.com>
2021-03-24 00:59:41 -0400

    Added dependency modules

--- projects/ccf-eui/src/app/shared/components/thumbnail-carousel/thumbnail-carousel.module.ts
+++ projects/ccf-eui/src/app/shared/components/thumbnail-carousel/thumbnail-carousel.module.ts
@@ -1,22 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { SwiperModule } from 'swiper/angular';
 import { Navigation, Swiper } from 'swiper/core';
 
 import { ThumbnailCarouselComponent } from './thumbnail-carousel.component';
 
 
-// Install swiper modules
-Swiper.use([Navigation]);
-
-
 @NgModule({
   imports: [
     CommonModule,
+
+    MatButtonModule,
+    MatIconModule,
 
     SwiperModule
   ],
   declarations: [ThumbnailCarouselComponent],
   exports: [ThumbnailCarouselComponent]
 })
-export class ThumbnailCarouselModule { }
+export class ThumbnailCarouselModule {
+  constructor() {
+    // Install swiper modules
+    Swiper.use([Navigation]);
+  }
+}
