fda2705877f3c62c2d6ab0f0456d95a482cef820
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-05-20 15:36:21 -0400

    Test

--- projects/ccf-shared/src/lib/components/info/info-dialog/info-dialog.component.spec.ts
+++ projects/ccf-shared/src/lib/components/info/info-dialog/info-dialog.component.spec.ts
@@ -18,6 +18,17 @@ describe('InfoDialogComponent', () => {
       .provide({ provide: MAT_DIALOG_DATA, useValue: [ ] });
   });
 
+  it('should call the close() method when the close button is pressed', async () => {
+    const { find, instance } = await shallow
+      .mock(MAT_DIALOG_DATA, [])
+      .render();
+    const spy = spyOn(instance, 'close');
+    const closeButton = find('.close-icon');
+
+    closeButton.triggerEventHandler('click', {});
+    expect(spy).toHaveBeenCalled();
+  });
+
   it('should close the dialog when the close() method is called', async () => {
     const { instance, get } = await shallow
       .mock(MatDialogRef, { close(): void { } })
