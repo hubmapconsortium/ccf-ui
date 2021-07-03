09596a4031d6a90d0bbfb3884a9f7016f7204eaf
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-07-03 07:32:11 -0400

    Tests

--- projects/ccf-shared/src/lib/components/tracking-popup/tracking-popup.component.spec.ts
+++ projects/ccf-shared/src/lib/components/tracking-popup/tracking-popup.component.spec.ts
@@ -15,16 +15,62 @@ describe('TrackingPopupComponent', () => {
     const mockTrackingState = jasmine.createSpyObj<TrackingState>(['setAllowTelemetry']);
     shallow = new Shallow(TrackingPopupComponent, TrackingPopupModule)
       .provide({ provide: ElementRef, useValue: {} })
-      .provide({ provide: MAT_SNACK_BAR_DATA, useValue: {} })
+      .provide({ provide: MAT_SNACK_BAR_DATA, useValue: {preClose: () => undefined} })
       .provide({ provide: TrackingState, useValue: {} })
       .mock(TrackingState, {
         ...mockTrackingState,
         snapshot: {allowTelemetry: undefined}
-      });
+      })
+      .mock(MAT_SNACK_BAR_DATA, {preClose: () => undefined});
   });
 
-  it('makes the instance', async () => {
+  it('dismisses the popup', async () => {
     const { instance } = await shallow.render();
-    expect(instance).toBeDefined();
+    const spy = spyOn(instance.data, 'preClose');
+    instance.dismiss();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('shows the button if allowTelemetry is undefined', async () => {
+    const { instance } = await shallow.render();
+    instance.showButton('opt-in');
+    expect(instance.showButton('opt-in')).toBeTrue();
+  });
+
+  it('shows the button if allowTelemetry is true', async () => {
+    const mockTrackingState = jasmine.createSpyObj<TrackingState>(['setAllowTelemetry']);
+    const { instance } = await shallow.mock(TrackingState, {
+        ...mockTrackingState,
+        snapshot: {allowTelemetry: true}}).render();
+    instance.showButton('opt-in');
+    expect(instance.showButton('opt-in')).toBeFalse();
+  });
+
+  it('submits the selection', async () => {
+    const mockTrackingState = jasmine.createSpyObj<TrackingState>(['setAllowTelemetry']);
+    const { instance } = await shallow.mock(TrackingState, {
+        ...mockTrackingState,
+        snapshot: {allowTelemetry: false}}).render();
+    const spy = spyOn(instance, 'dismiss');
+    instance.submit(true);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('hides the opt-in button if allowTelemetry is true', async () => {
+    const mockTrackingState = jasmine.createSpyObj<TrackingState>(['setAllowTelemetry']);
+    const { instance } = await shallow.mock(TrackingState, {
+        ...mockTrackingState,
+        snapshot: {allowTelemetry: true}}).render();
+    instance.showButton('opt-in');
+    expect(instance.showButton('opt-in')).toBeFalse();
+  });
+
+  it('shows the opt-out button if allowTelemetry is false', async () => {
+    const mockTrackingState = jasmine.createSpyObj<TrackingState>(['setAllowTelemetry']);
+    const { instance } = await shallow.mock(TrackingState, {
+        ...mockTrackingState,
+        snapshot: {allowTelemetry: false}}).render();
+    instance.showButton('opt-out');
+    expect(instance.showButton('opt-in')).toBeTrue();
   });
 });
