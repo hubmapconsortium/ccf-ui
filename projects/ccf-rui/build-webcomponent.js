3aae75268c4335a8be46b15b2700a9ce271cea8b
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-07-20 11:27:49 -0400

    updates to eui / rui web components

--- projects/ccf-rui/build-webcomponent.js
+++ projects/ccf-rui/build-webcomponent.js
@@ -6,8 +6,7 @@ const concat = require('concat');
     './dist/ccf-rui/runtime.js',
     './dist/ccf-rui/polyfills.js',
     './dist/ccf-rui/scripts.js',
-    './dist/ccf-rui/main.js',
-    './dist/ccf-rui/styles.js'
+    './dist/ccf-rui/main.js'
   ];
   await concat(files, './dist/ccf-rui/wc.js');
   await fs.copy('./projects/ccf-rui/src/webcomponent-example.html', 'dist/ccf-rui/webcomponent-example.html');
