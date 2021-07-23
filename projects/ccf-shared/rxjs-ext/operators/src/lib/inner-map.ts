09bc4a550972021df3d3c60984b847f4b9b74c63
axdanbol <daniel_bolin@yahoo.com>
2021-07-23 17:33:26 -0400

    Fixed a typing error in innerMap operator

--- projects/ccf-shared/rxjs-ext/operators/src/lib/inner-map.ts
+++ projects/ccf-shared/rxjs-ext/operators/src/lib/inner-map.ts
@@ -2,9 +2,9 @@ import { OperatorFunction } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 
-export function innerMap<T>(
-  project: (value: T, index: number, outerIndex: number) => T
-): OperatorFunction<readonly T[], T[]> {
+export function innerMap<T, R>(
+  project: (value: T, index: number, outerIndex: number) => R
+): OperatorFunction<readonly T[], R[]> {
   return map(
     (array, outerIndex) => array.map(
       (value, index) => project(value, index, outerIndex)
