ffcc1d32b7845d40c45dfbaa0a37e96a02bfb8bd
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-07-26 14:04:36 -0400

    Load default organ in embedded mode

--- projects/ccf-rui/src/app/core/store/model/model.state.ts
+++ projects/ccf-rui/src/app/core/store/model/model.state.ts
@@ -163,12 +163,8 @@ export class ModelState extends NgxsImmutableDataRepository<ModelStateModel> {
     super.ngxsOnInit();
 
     this.referenceData = this.injector.get(ReferenceDataState);
-  }
 
-  ngxsAfterBootstrap(): void {
-    super.ngxsAfterBootstrap();
-
-    if (this.globalConfig.organ) {
+    if (this.globalConfig.organ && this.globalConfig.embedded) {
       const organConfig = this.globalConfig.organ;
       const organName = organConfig.name.toLowerCase();
       const organSide = organConfig.side;
