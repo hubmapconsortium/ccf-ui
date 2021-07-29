31daa83dbd762f4b664b29fe9cdb8a40042eba81
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-07-29 10:51:16 -0400

    Refactoring dialog logic to fix reload bug

--- projects/ccf-shared/src/lib/analytics/tracking.state.ts
+++ projects/ccf-shared/src/lib/analytics/tracking.state.ts
@@ -9,8 +9,12 @@ export interface TrackingStateModel {
 }
 
 export const LOCAL_STORAGE_ALLOW_TELEMETRY_KEY = 'ALLOW_TELEMETRY';
-export const INITIAL_TELEMETRY_SETTING  = localStorage.getItem(LOCAL_STORAGE_ALLOW_TELEMETRY_KEY) === null ? undefined
-  : localStorage.getItem(LOCAL_STORAGE_ALLOW_TELEMETRY_KEY)?.toLowerCase() === 'true';
+export const INITIAL_TELEMETRY_SETTING  = getTelemetryStorageSetting();
+
+function getTelemetryStorageSetting(): boolean | undefined {
+  const value = localStorage.getItem(LOCAL_STORAGE_ALLOW_TELEMETRY_KEY);
+  return value === null ? undefined : value.toLowerCase() === 'true';
+}
 
 @StateRepository()
 @State<TrackingStateModel>({
@@ -21,13 +25,13 @@ export const INITIAL_TELEMETRY_SETTING  = localStorage.getItem(LOCAL_STORAGE_ALL
 })
 @Injectable()
 export class TrackingState extends NgxsImmutableDataRepository<TrackingStateModel> {
-
   @DataAction()
   setAllowTelemetry(allowTelemetry: boolean): void {
-    const oldValue = localStorage.getItem(LOCAL_STORAGE_ALLOW_TELEMETRY_KEY) ?? null;
+    const oldValue = getTelemetryStorageSetting();
     localStorage.setItem(LOCAL_STORAGE_ALLOW_TELEMETRY_KEY, allowTelemetry.toString());
     this.ctx.patchState({ allowTelemetry });
-    if ((oldValue === null && !allowTelemetry) || (oldValue !== allowTelemetry.toString())) {
+  
+    if (oldValue !== undefined || allowTelemetry === false) {
       // This ensures that if telemetry is disabled that it _WONT_ send anything to Google Analytics
       location.reload();
     }
