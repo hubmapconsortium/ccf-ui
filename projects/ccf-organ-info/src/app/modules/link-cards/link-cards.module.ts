2b20032427a25adaf73f0601c59a42960f6ea22c
Adam Phillips <adphillips2017@users.noreply.github.com>
2021-09-02 12:32:06 -0400

    Pbi 657 link cards (#657 #660)
    * creating base ccf-organ-info project
    
    * link-cards component set up
    
    * changed eui link
    
    * fixing errors
    
    * fixed root tag

---
+++ projects/ccf-organ-info/src/app/modules/link-cards/link-cards.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LinkCardsComponent } from './link-cards.component';
+
+
+
+@NgModule({
+  declarations: [
+    LinkCardsComponent
+  ],
+  imports: [CommonModule],
+  exports: [LinkCardsComponent]
+})
+export class LinkCardsModule { }
