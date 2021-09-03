b2d4135c1182b0158cdf9c9ad4c55b897d18ef8a
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-09-03 10:10:41 -0400

    stats list styling

--- projects/ccf-organ-info/src/app/modules/stats-list/stats-list.component.ts
+++ projects/ccf-organ-info/src/app/modules/stats-list/stats-list.component.ts
@@ -1,8 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ccf-stats-list',
   templateUrl: './stats-list.component.html',
   styleUrls: ['./stats-list.component.scss']
 })
-export class StatsListComponent { }
+export class StatsListComponent {
+  organInfo = 'male, kidney, left';
+  stats = [
+    {
+      stat: 5,
+      label: 'Tissue Data Providers'
+    },
+    {
+      stat: 60,
+      label: 'Donors'
+    },
+    {
+      stat: 182,
+      label: 'Tissue Blocks'
+    }
+  ];
+}
