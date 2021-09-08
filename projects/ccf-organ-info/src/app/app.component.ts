d20d4b9e09b941cd664fd163af649235e9ab8c4d
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-09-08 11:03:47 -0400

    Fix label issues

--- projects/ccf-organ-info/src/app/app.component.ts
+++ projects/ccf-organ-info/src/app/app.component.ts
@@ -11,17 +11,16 @@ export class AppComponent {
   title = 'ccf-organ-info';
   @Input() organ = 'Kidney';
   @Input() sex: 'Both' | 'Male' | 'Female' = 'Female';
-  @Input() side?: 'Left' | 'Right' | undefined = 'Right';
-
-  organIri: string | undefined;
+  @Input() side?: 'left' | 'right' | undefined = 'right';
+  @Input() organIri: string | undefined;
 
   constructor() {
     this.organIri = this.getCurrentOrgan()?.id;
     this.side = this.getCurrentOrgan()?.side;
   }
 
-  sideChange(selection: 'Left' | 'Right'): void {
-    this.side = selection === 'Left' ? 'Left' : 'Right';
+  sideChange(selection: 'left' | 'right'): void {
+    this.side = selection === 'left' ? 'left' : 'right';
     this.organIri = this.getCurrentOrgan()?.id;
   }
 
