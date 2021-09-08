556a536eb111fe6cc11242dd03627bc3142a4881
axdanbol <axbolin@iu.edu>
2021-09-08 13:30:08 -0400

    Feature: organ-info google analytics (#667 #668)
    * Added analytics entry point and module to ccf-shared
    
    * Added analytics events to organ-info link-cards
    
    * Removed strict template option (accidental committed change)

--- projects/ccf-organ-info/src/environments/environment.ts
+++ projects/ccf-organ-info/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  googleAnalyticsToken: 'G-B3DT7XPMRT'
 };
 
 /*
@@ -13,4 +15,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import 'zone.js/plugins/zone-error'; // Included with Angular CLI.
