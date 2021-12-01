ae96a71ccc608c7ba6b1a230bbb364c4f780d9f3
axdanbol <axbolin@iu.edu>
2021-12-01 14:52:48 -0500

    Updated configuration

--- projects/ccf-api/karma.conf.js
+++ projects/ccf-api/karma.conf.js
@@ -36,9 +36,14 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+
+    customLaunchers: {
+      ChromeHeadlessWSL: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-features=VizDisplayCompositor']
+      }
+    }
   });
 };
