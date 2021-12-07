4a8d103bc60509d4fef86f6660c8629e710856a2
axdanbol <axbolin@iu.edu>
2021-12-07 14:05:05 -0500

    Added middleware and routes to main app

--- projects/ccf-api/src/lib/app.ts
+++ projects/ccf-api/src/lib/app.ts
@@ -1,18 +1,30 @@
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
 import express, { Express } from 'express';
 import helmet from 'helmet';
 
+import { databaseLoader, DatabaseLoaderOptions } from './middleware/database-loader';
+import { routes as v1Routes } from './routes/v1';
 
-export interface AppOptions {
+
+export interface AppOptions extends DatabaseLoaderOptions {
   placeholder?: never;
 }
 
 
-export function createApp(_options: AppOptions = {}): Express {
+export function createApp(options: AppOptions): Express {
   const app = express();
 
   // http://expressjs.com/en/advanced/best-practice-security.html
   app.use(helmet());
-  // Maybe TODO: Cookie configuration?
+  app.use(cors());
+
+  app.use(urlencoded());
+  app.use(json());
+
+  app.use(databaseLoader(options));
+
+  app.get('/v1', v1Routes);
 
   return app;
 }
