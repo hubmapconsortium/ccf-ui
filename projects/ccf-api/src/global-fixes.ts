e6800bf4cf2f449251c728fc9e5de6a568b0e767
axdanbol <axbolin@iu.edu>
2021-12-08 13:38:27 -0500

    fetch polyfill fixes

--- projects/ccf-api/src/global-fixes.ts
+++ projects/ccf-api/src/global-fixes.ts
@@ -1,7 +1,17 @@
-import fetch from 'node-fetch';
+import fetch, { Request, Response, Headers } from 'node-fetch';
+
+// Simple helper to make typescript happy
+function castToGlobalType<K extends keyof typeof globalThis>(value: unknown): (typeof globalThis)[K] {
+  return value as (typeof globalThis)[K];
+}
 
 // Fix for improper references in triple-store-utils
 globalThis.self = globalThis as unknown as typeof globalThis.window;
 
 // Fix ccf-database usage of fetch
-globalThis.fetch = fetch as unknown as typeof globalThis.fetch;
+if (typeof globalThis.fetch !== 'function') {
+  globalThis.fetch = castToGlobalType<'fetch'>(fetch);
+  globalThis.Headers = castToGlobalType<'Headers'>(Headers);
+  globalThis.Request = castToGlobalType<'Request'>(Request);
+  globalThis.Response = castToGlobalType<'Response'>(Response);
+}
