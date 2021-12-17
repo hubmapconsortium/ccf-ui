6ea99a84efc7b87701ed55682dc60562b5f63326
Bruce W. Herr II <bherr@indiana.edu>
2021-12-17 07:39:25 -0500

    Update prod and staging environments in the EUI

--- projects/ccf-eui/src/environments/environment.ts
+++ projects/ccf-eui/src/environments/environment.ts
@@ -11,22 +11,6 @@ export const environment = {
     'https://portal.hubmapconsortium.org',
     'https://portal.test.hubmapconsortium.org'
   ],
-  organNodes2: [
-    'http://purl.obolibrary.org/obo/UBERON_0002097', // Skin
-    'http://purl.obolibrary.org/obo/UBERON_0000059', // Large Intestine
-    'http://purl.obolibrary.org/obo/UBERON_0000948', // Heart
-    'http://purl.obolibrary.org/obo/UBERON_0002113', // Kidney
-    // 'http://purl.obolibrary.org/obo/UBERON_0004538', // Kidney, L
-    // 'http://purl.obolibrary.org/obo/UBERON_0004539', // Kidney, R
-    'http://purl.obolibrary.org/obo/UBERON_0002106', // Spleen
-    'http://purl.obolibrary.org/obo/UBERON_0000955', // Allen Brain
-    'http://purl.obolibrary.org/obo/UBERON_0002048', // Lungs
-    'http://purl.obolibrary.org/obo/UBERON_0000029', // Lymph Node, L
-    // 'http://purl.obolibrary.org/obo/UBERON_0000029', // Lymph Node, R
-    'http://purl.obolibrary.org/obo/UBERON_0001270', // Pelvis
-    'http://purl.obolibrary.org/obo/UBERON_0002370', // Thymus
-    'http://purl.obolibrary.org/obo/UBERON_0002049' // Vasculature
-  ],
   organNodes: [
     'http://purl.obolibrary.org/obo/UBERON_0000955', // Brain
     'http://purl.obolibrary.org/obo/UBERON_0000029', // Lymph Node
