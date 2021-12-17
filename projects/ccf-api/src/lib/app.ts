013729c9f80fc65530af00b707e1bc4158c515f5
Bruce W. Herr II <bherr@indiana.edu>
2021-12-17 12:10:39 -0500

    Feature: CCF-API spec and implementation (#724)
    * Initial generated ng library
    
    * Removed default generated code
    
    * Updated configuration
    
    * Installed express
    
    * Express server skeleton
    
    * Npm commands for ccf-api
    
    * Added helmet for security
    
    * Installed middleware and utilities
    
    * Created database middleware
    
    * Added qs for query param parsing
    
    * Created endpoints for database api
    
    * Added middleware and routes to main app
    
    * Load config from env in start script
    
    * ts-node doesn't understand type roots or something
    
    * Fixed build problems
    
    * Example environment file (more docs needed)
    
    * fetch polyfill fixes
    
    * Load local data sources without using fetch
    
    * Use non-browserified triple store
    
    * Fixed route bug
    
    * Add changes for deploying ccf-api to Heroku
    
    * Put heroku-postbuild in the right place
    
    * Fix for heroku?
    
    * Heroku removes dev dependencies, so keep ts-node for runtime for ccf-api
    
    * Move ts-node back to dev dependencies
    
    * Added the open api spec and a browser that uses it to the ccf-api server
    
    * Finished adding all metadata fields used for the routes advertised.
    
    * CSP updates to support Stoplight open api browser
    
    * Default to undefined in filter for age/bmi
    
    * Utility lru cache that automatically prunes outdated items
    
    * Expose hubmap search functionality
    
    * Helper class that caches requests
    
    * Expose lazy getDatabase method instead of eagerly loading the database
    
    * Added hubmap rui locations route
    
    * Set default env values
    
    * RequestCache: additional argument to callback
    
    * Parse and use token when loading database
    
    * Moved /hubmap routes under /v1
    
    * Fixed empty query bug
    
    * AutoPruneLRUCache: Use composition instead of inheritance
    
    Workaround for bug where downgraded class tries to inherit a regularly defined class
    
    * Moved server code into the library and exposed as part of the api
    
    * Updated api server command to run compiled code without ts-node
    
    * Update server-libs build to include ccf-api (for prod builds)
    
    * Add hubmap route to ccf-api
    
    * Catch and log errors when starting the ccf-api server
    
    * Slightly different error message
    
    * Add node-fetch to deps
    
    * Use default env
    
    * Do not rely on dotenv having a .env file, instead use the process.env which dotenv also populates (in production we use environment variables not a .env file)
    
    * Fix lint
    
    * Make production server the default in staging
    
    * Add dummy test
    
    * Increased heap size during linting
    
    Co-authored-by: axdanbol <axbolin@iu.edu>

---
+++ projects/ccf-api/src/lib/app.ts
@@ -0,0 +1,42 @@
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import express, { Express } from 'express';
+import helmet from 'helmet';
+
+import { databaseLoader, DatabaseLoaderOptions } from './middleware/database-loader';
+import { routes as browserRoutes } from './routes/browser';
+import { routes as v1Routes } from './routes/v1';
+
+
+export interface AppOptions extends DatabaseLoaderOptions {
+  placeholder?: never;
+}
+
+
+export function createApp(options: AppOptions): Express {
+  const app = express();
+
+  app.set('database-options', options.database);
+
+  // http://expressjs.com/en/advanced/best-practice-security.html
+  app.use(helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        'script-src': ['\'self\'', '\'unsafe-eval\'', 'cdn.jsdelivr.net'],
+        'connect-src': ['*'],
+      }
+    }
+  }));
+  app.use(cors());
+
+  app.use(urlencoded());
+  app.use(json());
+
+  app.use(databaseLoader(options));
+
+  app.use('/', browserRoutes);
+  app.use('/v1', v1Routes);
+
+  return app;
+}
---
+++ projects/ccf-api/src/lib/routes/browser.ts
@@ -0,0 +1,30 @@
+import { RequestHandler, Router } from 'express';
+import { resolve } from 'path';
+
+export const browserRoute: RequestHandler = (_req, res, _next) => {
+
+  res.send(`<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CCF-API Browser</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@stoplight/elements/web-components.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@stoplight/elements/styles.min.css">
+  </head>
+  <body>
+    <elements-api apiDescriptionUrl="ccf-api-spec.yaml" router="hash" />
+  </body>
+</html>`);
+
+};
+
+export const openApiRoute: RequestHandler = (_req, res, _next) => {
+  const apiFile = resolve('ccf-api-spec.yaml');
+  res.sendFile(apiFile);
+};
+
+export const routes = Router()
+  .get('/', browserRoute)
+  .get('/ccf-api-spec.yaml', openApiRoute);
