7f188620607b068423290b95571f377ae17599c6
axdanbol <axbolin@iu.edu>
2022-01-25 16:14:45 -0500

    Replaced data source in organ info using shared code

--- projects/ccf-organ-info/src/app/core/services/organ-lookup/organ-lookup.service.ts
+++ projects/ccf-organ-info/src/app/core/services/organ-lookup/organ-lookup.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AggregateResult, Filter, SpatialEntity, SpatialSceneNode, TissueBlockResult } from 'ccf-database';
-import { ALL_POSSIBLE_ORGANS, OrganInfo } from 'ccf-shared';
+import { ALL_POSSIBLE_ORGANS, DataSourceService, OrganInfo } from 'ccf-shared';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-import { DataSourceService } from '../data-source/data-source.service';
 
 
 @Injectable({
