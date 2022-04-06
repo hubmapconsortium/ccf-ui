5d8ece0a21db194b1548543e196bc9c6256d5216
Bruce W. Herr II <bherr@indiana.edu>
2022-04-06 14:36:43 -0400

    Dispose of workers when needed

--- projects/ccf-api/src/lib/middleware/database-loader.ts
+++ projects/ccf-api/src/lib/middleware/database-loader.ts
@@ -1,8 +1,8 @@
-import { CCFDatabase, CCFDatabaseOptions } from 'ccf-database';
+import { CCFDatabaseOptions } from 'ccf-database';
 import { Request, RequestHandler } from 'express';
 
 import { AutoPruneLRUCache } from '../utils/auto-prune-lru-cache';
-import { createCCFDatabaseWorker } from '../utils/ccf-database-worker';
+import { CCFDatabaseInstance, createCCFDatabaseWorker } from '../utils/ccf-database-worker';
 import { RequestCache } from '../utils/request-cache';
 
 
@@ -29,33 +29,32 @@ function selectToken(token: string | undefined, req: Request): string {
   return '';
 }
 
-async function createDatabase(token: string, options: CCFDatabaseOptions): Promise<CCFDatabase> {
+function createDatabase(token: string, options: CCFDatabaseOptions): Promise<CCFDatabaseInstance> {
   // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
   const hubmapToken = token || options.hubmapToken || undefined;
-  const database = createCCFDatabaseWorker({
+
+  return createCCFDatabaseWorker({
     ...options,
     hubmapDataUrl: '', // Do not use deprecated internal hubmap data loading
     dataSources: options.dataSources.map(s =>
       hubmapToken && typeof s === 'string' && s.endsWith('hubmap/rui_locations.jsonld') ? `${s}?token=${hubmapToken}` : s
     )
   });
-  await database.connect();
-  return database;
 }
 
-
 export function databaseLoader(options: DatabaseLoaderOptions): RequestHandler {
-  const cache = new RequestCache<string, CCFDatabase>(
+  const cache = new RequestCache<string, CCFDatabaseInstance>(
     new AutoPruneLRUCache({
       max: 10,
       maxAge: 60 * 60 * 1000,
+      dispose: (_key, instance) => instance.then((r) => r.dispose()),
       ...options.cache
     }),
     token => createDatabase(token, options.database)
   );
 
   return (req, _res, next) => {
-    req['getDatabase'] = (token?: string) => cache.get(selectToken(token, req));
+    req['getDatabase'] = (token?: string) => cache.get(selectToken(token, req))?.then((instance) => instance.database);
     next();
   };
 }
--- projects/ccf-api/src/lib/utils/ccf-database-worker.ts
+++ projects/ccf-api/src/lib/utils/ccf-database-worker.ts
@@ -1,11 +1,25 @@
 import { CCFDatabase, CCFDatabaseOptions } from 'ccf-database';
-import { wrap } from 'comlink';
+import { releaseProxy, wrap } from 'comlink';
 import nodeEndpoint from 'comlink/dist/umd/node-adapter';
 import { Worker } from 'worker_threads';
 
 
-export function createCCFDatabaseWorker(options: CCFDatabaseOptions): CCFDatabase {
+export interface CCFDatabaseInstance {
+  database: CCFDatabase;
+  dispose: () => Promise<void>;
+}
+
+export async function createCCFDatabaseWorker(options: CCFDatabaseOptions): Promise<CCFDatabaseInstance> {
   const worker = new Worker('./projects/ccf-api/ccf-database.worker.js', { workerData: { options } });
   const database = wrap<CCFDatabase>(nodeEndpoint(worker));
-  return database as unknown as CCFDatabase;
+  await database.connect();
+  const dispose = async () => {
+    database[releaseProxy]();
+    await worker.terminate();
+  };
+  return {
+    // NOTE: this is safe as all ccf operations are proxied and used via async/await
+    database: database as unknown as CCFDatabase,
+    dispose
+  };
 }
