189f20eb54b95a1e101290bdf3e4ac120a45fbe9
axdanbol <axbolin@iu.edu>
2022-05-25 17:30:50 -0400

    Basic functionality

--- projects/ccf-shared/src/lib/components/spatial-search-list/spatial-search-list.component.ts
+++ projects/ccf-shared/src/lib/components/spatial-search-list/spatial-search-list.component.ts
@@ -1,23 +1,45 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 
 
+export interface SpatialSearchListItem {
+  selected: boolean;
+  description: string;
+}
+
+
 @Component({
   selector: 'ccf-spatial-search-list',
   templateUrl: './spatial-search-list.component.html',
   styleUrls: ['./spatial-search-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SpatialSearchListComponent {
+export class SpatialSearchListComponent<T extends SpatialSearchListItem> {
   /** HTML class */
   @HostBinding('class') readonly clsName = 'ccf-spatial-search-list';
 
   @Input() label: string;
 
-  @Input() items: unknown[]; // TODO correct type
+  @Input() items: T[];
 
-  @Output() readonly itemSelected = new EventEmitter<unknown>(); // TODO correct type
+  @Output() readonly selectionChanged = new EventEmitter<T[]>();
 
-  @Output() readonly itemRemoved = new EventEmitter<unknown>(); // TODO correct type
+  @Output() readonly itemRemoved = new EventEmitter<T>();
 
-  // TODO
+  itemId(_index: number, item: T): string {
+    return item.description;
+  }
+
+  updateItemSelection(index: number, selected: boolean): void {
+    const newItems = this.items = [...this.items];
+    newItems[index] = { ...newItems[index], selected };
+
+    const selectedItems = newItems.filter(item => item.selected);
+    this.selectionChanged.emit(selectedItems);
+  }
+
+  removeItem(index: number): void {
+    const newItems = this.items = [...this.items];
+    const [item] = newItems.splice(index, 1);
+    this.itemRemoved.emit(item);
+  }
 }
