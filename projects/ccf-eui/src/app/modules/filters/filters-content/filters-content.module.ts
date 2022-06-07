36ffbcb344dd314f101e4bd21d88be9d280292af
Edward Lu <36934022+edlu77@users.noreply.github.com>
2022-06-07 14:57:24 -0400

    Add run spatial search button

--- projects/ccf-eui/src/app/modules/filters/filters-content/filters-content.module.ts
+++ projects/ccf-eui/src/app/modules/filters/filters-content/filters-content.module.ts
@@ -5,11 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { CheckboxModule } from '../../../shared/components/checkbox/checkbox.module';
 import { DropdownModule } from '../../../shared/components/dropdown/dropdown.module';
 import { DualSliderModule } from '../../../shared/components/dual-slider/dual-slider.module';
+import { RunSpatialSearchModule } from '../../../shared/components/run-spatial-search/run-spatial-search.module';
 import { FiltersContentComponent } from './filters-content.component';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, CheckboxModule, DropdownModule, DualSliderModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, CheckboxModule, DropdownModule, DualSliderModule, RunSpatialSearchModule, MatButtonModule, MatIconModule],
   declarations: [FiltersContentComponent],
   exports: [FiltersContentComponent]
 })
