5f5cee648ba995222d0b8db3eec3fcf244cb3801
Bryce Greene <sirbrucian@gmail.com>
2022-06-09 11:38:34 -0400

    lint fix II

--- projects/ccf-shared/src/lib/components/call-to-action/call-to-action.component.spec.ts
+++ projects/ccf-shared/src/lib/components/call-to-action/call-to-action.component.spec.ts
@@ -1,3 +1,4 @@
+import { CallToActionModule } from 'ccf-shared';
 import { Shallow } from 'shallow-render';
 import { CallToActionComponent } from './call-to-action.component';
 
@@ -5,11 +6,17 @@ import { CallToActionComponent } from './call-to-action.component';
 describe('CallToActionComponent', () => {
   let shallow: Shallow<CallToActionComponent>;
 
+  beforeEach(() => {
+    shallow = new Shallow(CallToActionComponent, CallToActionModule);
+  });
+
   it('should emit on call to action click', async () => {
     const { instance } = await shallow.render();
     instance.onDialogButtonClick();
     expect(instance.callToActionClicked.emit).toHaveBeenCalledWith();
   });
+
+
 
   it('should emit on close click', async () => {
     const { instance } = await shallow.render();
