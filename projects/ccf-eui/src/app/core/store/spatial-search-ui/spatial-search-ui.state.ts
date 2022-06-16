d9f51435d4d86794ab63579847c914373311dab5
Edward Lu <36934022+edlu77@users.noreply.github.com>
2022-06-16 11:54:15 -0400

    Fix circular dependency

--- projects/ccf-eui/src/app/core/store/spatial-search-ui/spatial-search-ui.state.ts
+++ projects/ccf-eui/src/app/core/store/spatial-search-ui/spatial-search-ui.state.ts
@@ -5,9 +5,10 @@ import { GoogleAnalyticsService } from 'ngx-google-analytics';
 import { Sex } from '../../../shared/components/spatial-search-config/spatial-search-config.component';
 import { SceneState } from '../scene/scene.state';
 import { SetOrgan, SetSex } from './spatial-search-ui.actions';
-import { SpatialSearchUiSelectors } from './spatial-search-ui.selectors';
 
-
+/**
+ * Contains sex and organId for the Spatial Search
+ */
 export interface SpatialSearchUiModel {
   sex: Sex;
   organId?: string;
@@ -28,25 +29,37 @@ export class SpatialSearchUiState {
     private readonly ga: GoogleAnalyticsService
   ) { }
 
+  /**
+   * Updates sex in the SpatialSearchUI and sets selected organ to undefined if not valid for the sex
+   */
   @Action(SetSex)
   setSex(ctx: StateContext<SpatialSearchUiModel>, { sex }: SetSex): void {
-    const organs = this.store.selectSnapshot(SceneState.referenceOrgans);
     const { organId } = ctx.getState();
-    const organ = SpatialSearchUiSelectors.organ(organId, organs);
-    const organValidForSex = organ ? SpatialSearchUiSelectors.organMatchesSex(organ, sex) : true;
-    const newOrganId = organValidForSex ? organId : undefined;
 
     ctx.patchState({ sex });
-    if (newOrganId !== organId) {
-      ctx.dispatch(new SetOrgan(newOrganId));
+    if (organId !== undefined && !this.organValidForSex(organId, sex)) {
+      ctx.dispatch(new SetOrgan(undefined));
     }
 
     this.ga.event('set_sex', 'spatial_search_ui', sex);
   }
 
+  /**
+   * Updates organId in the SpatialSearchUI
+   */
   @Action(SetOrgan)
   setOrgan(ctx: StateContext<SpatialSearchUiModel>, { organId }: SetOrgan): void {
     ctx.patchState({ organId });
     this.ga.event('set_organ', 'spatial_search_ui', organId);
+  }
+
+  /**
+   * Used to determine if an organ should be listed if a certain sex is selected
+   */
+  private organValidForSex(organId: string, sex: Sex): boolean {
+    const organs = this.store.selectSnapshot(SceneState.referenceOrgans);
+    const organ = organs.find(o => o.id === organId)!;
+    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+    return organ.hasSex || organ.sex === sex;
   }
 }
