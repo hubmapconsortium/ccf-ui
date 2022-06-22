d2574bec6755a34831105560bbb46edc46f769af
Bruce W. Herr II <bherr@indiana.edu>
2022-06-22 08:08:49 -0400

    Documentation, code cleanup, code smell fixes

--- projects/ccf-database/src/lib/util/enrich-rui-locations.ts
+++ projects/ccf-database/src/lib/util/enrich-rui-locations.ts
@@ -3,10 +3,17 @@ import { getAnatomicalStructureTreeModel } from '../queries/ontology-tree-n3';
 import { ccf, entity } from './prefixes';
 
 
+/**
+ * Function to add additional ccf_annotations to rui locations based on the
+ * reference organ it was placed relative to.
+ *
+ * @param store the triple store holding the CCF.OWL data
+ */
 export function enrichRuiLocations(store: Store): void {
   const tree = getAnatomicalStructureTreeModel(store);
-  const refOrganMap: Map<string, NamedNode<string>[]> = new Map();
+  const refOrganMap: Map<string, NamedNode[]> = new Map();
 
+  // Build a map from reference organ to ccf annotations via representation_of and the AS partonomy
   for (const { subject: organ, object: term } of readQuads(store, null, ccf.spatialEntity.representation_of, null, null)) {
     const annotations = [term.id];
     let parent = tree.nodes[term.id]?.parent;
@@ -17,19 +24,14 @@ export function enrichRuiLocations(store: Store): void {
     refOrganMap.set(organ.id, annotations.map(s => DataFactory.namedNode(s)));
   }
 
+  // Add AS terms for rui locations based on the reference organs they are placed relative to
   for (const { object: ruiLocation } of readQuads(store, null, entity.spatialEntity, null, null)) {
-    let annotations: NamedNode<string>[] = [];
     for (const { subject: placement } of readQuads(store, null, ccf.spatialPlacement.source, ruiLocation, null)) {
       for (const { object: organ } of readQuads(store, placement, ccf.spatialPlacement.target, null, null)) {
-        if (refOrganMap.has(organ.id)) {
-          annotations = refOrganMap.get(organ.id)!;
-        } else {
-          console.log(organ.id);
+        for (const term of refOrganMap.get(organ.id) ?? []) {
+          store.addQuad(DataFactory.namedNode(ruiLocation.id), ccf.spatialEntity.ccf_annotations, term);
         }
       }
-    }
-    for (const term of annotations) {
-      store.addQuad(DataFactory.namedNode(ruiLocation.id), ccf.spatialEntity.ccf_annotations, term, undefined);
     }
   }
 }
