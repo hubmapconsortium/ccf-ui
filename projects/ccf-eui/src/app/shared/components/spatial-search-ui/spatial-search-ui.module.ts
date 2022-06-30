4d5c445fbbcf874e3000b480d0bd337d2618642f
Edward Lu <36934022+edlu77@users.noreply.github.com>
2022-06-30 15:58:37 -0400

    Styling improvements (#914)
    * Sizing issues
    
    * Styling for spatial search config
    
    * Add min-width and min-height
    
    * Styling improvements to main ui
    
    * Info button color

--- projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
+++ projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
+
 import { BodyUiModule, InfoButtonModule, XYZPositionModule } from 'ccf-shared';
 import { SpatialSearchKeyboardUIBehaviorModule } from '../spatial-search-keyboard-ui-behavior/spatial-search-keyboard-ui-behavior.module';
-
 import { TissueBlockListModule } from '../tissue-block-list/tissue-block-list.module';
 import { SpatialSearchUiComponent } from './spatial-search-ui.component';
 
@@ -18,6 +19,7 @@ import { SpatialSearchUiComponent } from './spatial-search-ui.component';
     SpatialSearchKeyboardUIBehaviorModule,
     MatIconModule,
     MatSliderModule,
+    MatDividerModule,
     MatButtonModule,
     InfoButtonModule
   ],
