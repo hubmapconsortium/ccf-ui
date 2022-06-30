39d56ea402c5c18c8a24afdd14b43c5da7377d5f
Daniel Bolin <axbolin@iu.edu>
2022-06-29 21:36:44 -0400

    xyz-position component (#904)
    Co-authored-by: Bruce W. Herr II <bherr@indiana.edu>

--- projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
+++ projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
@@ -1,24 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InfoButtonModule, XYZPositionModule } from 'ccf-shared';
 import { SpatialSearchKeyboardUIBehaviorModule } from '../spatial-search-keyboard-ui-behavior/spatial-search-keyboard-ui-behavior.module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
-import { InfoButtonModule } from 'ccf-shared';
-import { TissueBlockListModule } from '../tissue-block-list/tissue-block-list.module';
 
+import { TissueBlockListModule } from '../tissue-block-list/tissue-block-list.module';
 import { SpatialSearchUiComponent } from './spatial-search-ui.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    XYZPositionModule,
     TissueBlockListModule,
     SpatialSearchKeyboardUIBehaviorModule,
     MatIconModule,
     MatSliderModule,
     MatButtonModule,
-    InfoButtonModule,
-    TissueBlockListModule
+    InfoButtonModule
   ],
   declarations: [SpatialSearchUiComponent],
   exports: [SpatialSearchUiComponent]
