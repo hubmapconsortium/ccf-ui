c34f0f5ff84b89f46e77f5f8242353934064035f
edlu77 <eddd.lu@gmail.com>
2023-09-06 02:09:36 -0400

    Revert slider back to legacy for now

--- projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
+++ projects/ccf-eui/src/app/shared/components/spatial-search-ui/spatial-search-ui.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider';
 
 import { BodyUiModule, InfoButtonModule, XYZPositionModule } from 'ccf-shared';
 import { SpatialSearchKeyboardUIBehaviorModule } from '../spatial-search-keyboard-ui-behavior/spatial-search-keyboard-ui-behavior.module';
 import { TermOccurrenceListModule } from '../term-occurence-list/term-occurrence.module';
 import { TissueBlockListModule } from '../tissue-block-list/tissue-block-list.module';
 import { SpatialSearchUiComponent } from './spatial-search-ui.component';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [
@@ -25,8 +24,7 @@ import { MatInputModule } from '@angular/material/input';
     MatDividerModule,
     MatButtonModule,
     InfoButtonModule,
-    TermOccurrenceListModule,
-    MatInputModule
+    TermOccurrenceListModule
   ],
   declarations: [SpatialSearchUiComponent],
   exports: [SpatialSearchUiComponent]
