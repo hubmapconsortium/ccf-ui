9a7632ec4ecc4415bab653372fac940e1ce068df
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-09-11 16:02:46 -0400

    adds test

--- projects/ccf-organ-info/src/app/modules/link-cards/link-cards.component.spec.ts
+++ projects/ccf-organ-info/src/app/modules/link-cards/link-cards.component.spec.ts
@@ -1,3 +1,15 @@
+import { Shallow } from 'shallow-render';
+import { LinkCardsComponent } from './link-cards.component';
+import { LinkCardsModule } from './link-cards.module';
 describe('LinkCardsComponent', () => {
-  // Add tests
+  let shallow: Shallow<LinkCardsComponent>;
+  beforeEach(() => {
+    shallow = new Shallow(LinkCardsComponent, LinkCardsModule);
+  });
+  it('should call window open function on click', async () => {
+    const { instance } = await shallow.render();
+    spyOn(window, 'open').and.callThrough();
+    instance.goToURL('http://test.com');
+    expect(window.open).toHaveBeenCalled();
+  });
 });
