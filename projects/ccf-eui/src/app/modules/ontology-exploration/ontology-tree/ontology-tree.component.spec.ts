fa130c636154f89022d77636d3535e69a0a6ec22
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-09-12 15:45:57 -0400

    improves coverage

--- projects/ccf-eui/src/app/modules/ontology-exploration/ontology-tree/ontology-tree.component.spec.ts
+++ projects/ccf-eui/src/app/modules/ontology-exploration/ontology-tree/ontology-tree.component.spec.ts
@@ -1,6 +1,7 @@
 import { OntologyTreeNode } from 'ccf-database';
 import { RecursivePartial, Shallow } from 'shallow-render';
 
+import { SimpleChanges } from '@angular/core';
 import { FlatNode } from '../../../core/models/flat-node';
 import { OntologyTreeComponent } from './ontology-tree.component';
 import { OntologyTreeModule } from './ontology-tree.module';
@@ -12,8 +13,8 @@ function fromPartial<T>(partial: RecursivePartial<T>): T {
 
 
 describe('OntologyTreeComponent', () => {
-  const node1 = fromPartial<OntologyTreeNode>({ label: 'label', children: ['child1', 'child2'] });
-  const node2 = fromPartial<OntologyTreeNode>({ label: 'label2', children: [] });
+  const node1 = fromPartial<OntologyTreeNode>({ id:'1', parent:'2', label: 'label', children: ['child1', 'child2'] });
+  const node2 = fromPartial<OntologyTreeNode>({ id: '2', label: 'label2', children: [] });
   const flatNode1 = new FlatNode(node1, 1);
   const flatNode2 = new FlatNode(node2, 1);
 
@@ -29,10 +30,59 @@ describe('OntologyTreeComponent', () => {
     expect(instance.isSelected(flatNode1)).toBeTrue();
   });
 
+  it('should call set nodes', async () => {
+    const { instance } = await shallow.render();
+    instance.nodes = [node1, node2];
+  });
+
+  it('should return nodes', async () => {
+    const { instance } = await shallow.render({ bind: { nodes: [node1, node2] } });
+    expect(instance.nodes).toEqual([node1, node2]);
+  });
+
+  it('should return left indent', async () => {
+    const { instance } = await shallow.render();
+    expect(instance.getLeftIndent(1)).toEqual('-1.5rem');
+  });
+
+  it('should set and get occurenceData', async () => {
+    const { instance } = await shallow.render({});
+    instance.occurenceData = { 'node': 1 };
+    expect(instance.occurenceData).toEqual({ 'node': 1 });
+  });
+
+  it('should call selectByIDs when rootNode changes', async () => {
+    const { instance } = await shallow.render({ bind: { rootNode: '1', nodes: [node1, node2] } });
+    spyOn(instance, 'selectByIDs').and.callThrough();
+    const changes: SimpleChanges = { 'rootNode': { currentValue: '2', previousValue: '1', isFirstChange: () => true, firstChange: true } };
+    instance.ngOnChanges(changes);
+    expect(instance.selectByIDs).toHaveBeenCalledWith(['2']);
+  });
+
+  it('should call selectByIDs when ontologyFilter changes', async () => {
+    const { instance } = await shallow.render({ bind: { ontologyFilter:['1'] ,rootNode: '1', nodes: [node1, node2] } });
+    spyOn(instance, 'selectByIDs').and.callThrough();
+    const changes: SimpleChanges = { 'ontologyFilter': { currentValue: ['2'], previousValue: '1', isFirstChange: () => true, firstChange: true } };
+    instance.ngOnChanges(changes);
+    expect(instance.selectByIDs).toHaveBeenCalledWith(['2']);
+  });
+
+  it('should call onScroll when mat-tree emits scroll', async () => {
+    const { instance, find } = await shallow.render();
+    spyOn(instance, 'onScroll').and.callThrough();
+    find('mat-tree')[0].triggerEventHandler('scroll', new UIEvent('scroll'));
+    expect(instance.onScroll).toHaveBeenCalled();
+  });
+
   it('should check if the root node is selected too or not', async () => {
     const { instance } = await shallow.render();
     instance.selectedNodes = [flatNode2];
-    expect(instance.isSelected(flatNode1)).toBeTrue();
+    expect(instance.isSelected(flatNode1)).toBeFalse();
+  });
+  it('should check if  node is selected too or not', async () => {
+    const { instance } = await shallow.render();
+    instance.selectedNodes = [flatNode2];
+    expect(instance.isSelected(flatNode2)).toBeTrue();
   });
 
   it('should set the current selection when a different node is selected', async () => {
