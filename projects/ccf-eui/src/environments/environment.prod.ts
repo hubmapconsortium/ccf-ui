99f7ff891b3b70299c68d92410fb337603234912
Sai Kiran Jella <jsaikiran19@gmail.com>
2023-09-19 15:16:28 -0400

    updates selected organs to uberons

--- projects/ccf-eui/src/environments/environment.prod.ts
+++ projects/ccf-eui/src/environments/environment.prod.ts
@@ -30,9 +30,9 @@ export const environment = {
     header: true,
     homeUrl: 'https://portal.hubmapconsortium.org/',
     selectedOrgans: [
-      'Heart',
-      'Kidney',
-      'Skin'
+      'http://purl.obolibrary.org/obo/UBERON_0000948',
+      'http://purl.obolibrary.org/obo/UBERON_0004538',
+      'http://purl.obolibrary.org/obo/UBERON_0002097'
     ],
     logoTooltip: 'Human BioMolecular Atlas Project'
   },
