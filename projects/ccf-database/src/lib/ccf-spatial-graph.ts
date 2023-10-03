3b8056de6195da314c5e5212bbcd0dafa6a285d6
Bruce W. Herr II <bherr@indiana.edu>
2023-10-03 13:27:47 -0400

    Trying a different workaround for DirectedGraph

--- projects/ccf-database/src/lib/ccf-spatial-graph.ts
+++ projects/ccf-database/src/lib/ccf-spatial-graph.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Euler, Matrix4, toDegrees, toRadians } from '@math.gl/core';
-import graphology from 'graphology';
+import * as graphology from 'graphology';
 import shortestPath from 'graphology-shortest-path/unweighted';
 import get from 'lodash/get';
 import { readQuads } from 'triple-store-utils';
@@ -13,9 +13,9 @@ import { getSpatialPlacement } from './queries/spatial-result-n3';
 import { FlatSpatialPlacement, SpatialEntity, SpatialPlacement } from './spatial-types';
 import { ccf, rdf } from './util/prefixes';
 
-// Including TS ignore because of bad typings and node 18 support.
-// @ts-ignore
-const { DirectedGraph } = graphology;
+// Ugly workaround to make ccf database work both on the browser and node.js 18+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const DirectedGraph = ((graphology.default || graphology) as any).DirectedGraph;
 
 export function applySpatialPlacement(tx: Matrix4, placement: SpatialPlacement): Matrix4 {
   const p = placement;
