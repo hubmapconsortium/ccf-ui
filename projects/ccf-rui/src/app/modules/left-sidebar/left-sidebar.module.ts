f31efde096e4dfdcdb5e82f67be7a0cc7d335db9
edlu77 <eddd.lu@gmail.com>
2023-11-08 18:52:01 -0500

    Remove organ selector and sex toggle from RUI

--- projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
+++ projects/ccf-rui/src/app/modules/left-sidebar/left-sidebar.module.ts
@@ -6,9 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { OpacitySliderModule } from 'ccf-shared';
 
-import { DetailsLabelModule } from '../../shared/components/details-label/details-label.module';
 import { ExtractionSetDropdownModule } from '../../shared/components/extraction-set-dropdown/extraction-set-dropdown.module';
-import { LabeledSlideToggleModule } from '../../shared/components/labeled-slide-toggle/labeled-slide-toggle.module';
 import { NameInputModule } from '../../shared/components/name-input/name-input.module';
 import { VisibilityMenuModule } from '../../shared/components/visibility-menu/visibility-menu.module';
 import { VisibilityToggleModule } from '../../shared/components/visibility-toggle/visibility-toggle.module';
@@ -24,8 +22,6 @@ import { LeftSidebarComponent } from './left-sidebar.component';
     MatTooltipModule,
 
     MatDividerModule,
-    DetailsLabelModule,
-    LabeledSlideToggleModule,
     NameInputModule,
     VisibilityToggleModule,
     ExtractionSetDropdownModule,
--- projects/ccf-rui/src/app/modules/registration-modal/registration-content/registration-content.module.ts
+++ projects/ccf-rui/src/app/modules/registration-modal/registration-content/registration-content.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { RegistrationMetadataModule } from '../../../shared/components/registration-metadata/registration-metadata.module';
 import { RegistrationContentComponent } from './registration-content.component';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [RegistrationContentComponent],
-  imports: [CommonModule, MatTooltipModule, RegistrationMetadataModule],
+  imports: [CommonModule, MatTooltipModule, RegistrationMetadataModule, MatButtonModule],
   exports: [RegistrationContentComponent]
 })
 export class RegistrationContentModule { }
