193719f748afb51192a34b947066d84a8b562322
Daniel Bolin <axbolin@iu.edu>
2024-02-02 17:44:18 -0500

    Fix ccf-body-ui-wc webcomponent build

---
+++ tools/scripts/build-webcomponent.mjs
@@ -0,0 +1,35 @@
+import { copyFile } from 'node:fs/promises';
+import concat from 'concat';
+import { argv, exit } from 'node:process';
+import { glob } from 'glob';
+
+async function build(app_name) {
+  const appDir = `./apps/${app_name}`;
+  const distDir = `./dist/apps/${app_name}`;
+  const jsFiles = await glob([
+    `${distDir}/runtime.*.js`,
+    `${distDir}/polyfills.*.js`,
+    `${distDir}/scripts.*.js`,
+    `${distDir}/main.*.js`,
+  ]);
+
+  if (jsFiles.length !== 4) {
+    console.log('Failed to find all application js files');
+    exit(1);
+  }
+
+  await concat(jsFiles, `${distDir}/wc.js`);
+  await copyFile(`${appDir}/src/webcomponent-example.html`, `${distDir}/webcomponent-example.html`);
+}
+
+async function main() {
+  const userArguments = argv.slice(2);
+  if (userArguments.length !== 1) {
+    console.error('Expected the application directory name as the only argument');
+    exit(1);
+  }
+
+  await build(userArguments[0]);
+}
+
+await main();
